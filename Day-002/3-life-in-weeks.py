79af5cc101821af40c717cf1b157377df94e2796
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-13 15:26:33 -0700

    Calcualte years remaining assuming 90 year lifespan, then calculate months, weeks, and days

--- Day-002/3-life-in-weeks.py
+++ Day-002/3-life-in-weeks.py
@@ -4,3 +4,7 @@ age = input("What is your current age? ")
 
 # Write your code below this line 👇
 age_int = int(age)
+years_remaining = 90 - age_int
+months_remaining = 12 * years_remaining
+weeks_remaining = 52 * years_remaining
+days_remaining = 365 * years_remaining
---
+++ Day-002/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+print(
+    f"YOu have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+)
