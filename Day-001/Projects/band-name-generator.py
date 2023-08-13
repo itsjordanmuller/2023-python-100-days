63c282700f9ab98006a5d977cd80fffbcb7a5473
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-12 21:01:32 -0700

    Store user's hometown in variable named city

--- Day-001/Projects/band-name-generator.py
+++ Day-001/Projects/band-name-generator.py
@@ -6,6 +6,8 @@ print(
 
 # 2. Ask the user for the city that they grew up in.
 
+city = input("What city/town did you grow up in?:\n")
+
 # 3. Ask the user for the name of a pet.
 
 # 4. Combine the name of their city and pet and show them their band name.
