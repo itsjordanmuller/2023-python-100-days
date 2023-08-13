e7475911a71bac297bf009c6cba68a8b4a2c4eac
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-13 15:27:49 -0700

    Fix a typo, capitalization where there should have been none

--- Day-002/3-life-in-weeks.py
+++ Day-002/3-life-in-weeks.py
@@ -10,5 +10,5 @@ weeks_remaining = 52 * years_remaining
 days_remaining = 365 * years_remaining
 
 print(
-    f"YOu have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+    f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
 )
