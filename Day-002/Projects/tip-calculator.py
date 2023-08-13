93ecc605a442cd7bfef51b8d45c191722c27eccd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-13 15:30:43 -0700

    Get the total bill as a float input from the user

--- Day-002/Projects/tip-calculator.py
+++ Day-002/Projects/tip-calculator.py
@@ -6,3 +6,5 @@
 # Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 # Write your code below this line 👇
+
+total_bill = float(input("What was the total bill? $"))
