4b6b1ded85ef15e6de9b0b1da13d5b7288a6f39f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-13 15:13:07 -0700

    Calculate BMI, and then print it as an integer

--- Day-002/2-bmi-calculator.py
+++ Day-002/2-bmi-calculator.py
@@ -6,3 +6,5 @@ weight = input("enter your weight in kg: ")
 # Write your code below this line 👇
 height_float = float(height)
 weight_float = float(weight)
+bmi = (weight_float) / (height_float**2)
+print(int(bmi))
