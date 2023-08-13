5d6603af876b17a85a4a0cc123ab43c9bbc59cb9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-12 21:22:59 -0700

    Print out a full sentence with the complete band name using variables and print

--- Day-001/Projects/band-name-generator.py
+++ Day-001/Projects/band-name-generator.py
@@ -10,7 +10,11 @@ city = input("What city/town did you grow up in?:\n")
 
 # 3. Ask the user for the name of a pet.
 
+pet = input("What is the name of a beloved pet?\n")
+
 # 4. Combine the name of their city and pet and show them their band name.
+
+print("A good band name might be: " + city + " " + pet)
 
 # 5. Make sure the input cursor shows on a new line:
 
