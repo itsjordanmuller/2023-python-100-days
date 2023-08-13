ca1321464dc860644ce6d02d70c557c6c4c35f84
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-13 15:31:44 -0700

    Allow the user to input the percentage tip that they would like to use for this meal

--- Day-002/Projects/tip-calculator.py
+++ Day-002/Projects/tip-calculator.py
@@ -9,3 +9,7 @@
 
 total_bill = float(input("What was the total bill? $"))
 print(total_bill)
+tip_percent = (
+    float("1" + input("What percentage tip would you like to give? 10, 12, or 15? "))
+    / 100
+)
