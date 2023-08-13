b0acc63bf0cfa0a91bf6aecf761627c8a421b4c9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-13 15:22:15 -0700

    Use an f string to bring multiple data types into a string easily

--- Day-002/0-primitive-data-types.py
+++ Day-002/0-primitive-data-types.py
@@ -48,3 +48,9 @@ print(result)
 score = 0
 score += 1
 print(score)
+height = 1.8
+isWinning = True
+
+print(
+    f"Your score is {score}, your height is {height}, and you are winning is: {isWinning}"
+)
