bc8133c57ddc6d521131343af4ed8050249ab6e7
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-13 15:27:05 -0700

    Use an f-string to print the remaining time

--- Day-002/3-life-in-weeks.py
+++ Day-002/3-life-in-weeks.py
@@ -8,3 +8,7 @@ years_remaining = 90 - age_int
 months_remaining = 12 * years_remaining
 weeks_remaining = 52 * years_remaining
 days_remaining = 365 * years_remaining
+
+print(
+    f"YOu have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+)
