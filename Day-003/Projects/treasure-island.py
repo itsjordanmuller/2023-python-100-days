6ef8bb2c99ecad2f57b04f67a55d47998750a84c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-14 10:50:57 -0700

    Ask the user to input the direction they would like to pick

--- Day-003/Projects/treasure-island.py
+++ Day-003/Projects/treasure-island.py
@@ -26,3 +26,4 @@ print("Your mission is to find the treasure.")
 # https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 # Write your code below this line 👇
+direction = input("Would you like to go left or right? ").lower()
