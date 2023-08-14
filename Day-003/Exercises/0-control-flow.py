efbe2f86066e55ef449c9dc8bf5fedc789e6a111
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-14 10:14:58 -0700

    Nest another if else statement inside of height check conditional statement

--- Day-003/Exercises/0-control-flow.py
+++ Day-003/Exercises/0-control-flow.py
@@ -3,5 +3,10 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
 else:
     print("You are not tall enough to ride the rollercoaster yet, try again next time!")
