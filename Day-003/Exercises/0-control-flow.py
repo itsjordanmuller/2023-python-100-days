9bcf5b80d0f5ea5f2cbe7ac02f032895ed75b435
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-14 10:30:53 -0700

    Set bill value based on age, and then add  if the user wants a photo

--- Day-003/Exercises/0-control-flow.py
+++ Day-003/Exercises/0-control-flow.py
@@ -1,19 +1,27 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
+        bill = 5
         print("Child tickets are $5")
     elif age <= 18 >= age:
+        bill = 7
         print("Youth tickets are $7")
     else:
+        bill = 12
         print("Adult tickets are $12")
 
     wants_photo = input("Do you want a photo taken? Y for Yes, N for No ")
     if wants_photo == "Y":
+        bill += 3
         print("Photos are an additional $3")
+
+    print(f"Your total bill is ${bill}")
+
 else:
     print("You are not tall enough to ride the rollercoaster yet, try again next time!")
