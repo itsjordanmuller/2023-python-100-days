177dcef3547323a9b82a1d9ed51772b8bac0e3f7
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-14 10:41:29 -0700

    Fix check for between 12 and 18, add a new check for mid-life discount

--- Day-003/Exercises/0-control-flow.py
+++ Day-003/Exercises/0-control-flow.py
@@ -9,9 +9,12 @@ if height >= 120:
     if age < 12:
         bill = 5
         print("Child tickets are $5")
-    elif age <= 18 >= age:
+    elif 12 <= age <= 18:
         bill = 7
         print("Youth tickets are $7")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("Have a free ride on us!")
     else:
         bill = 12
         print("Adult tickets are $12")
