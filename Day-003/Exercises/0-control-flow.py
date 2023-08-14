f9f53bfd4f07960da1828fd75a3d0864166c5daa
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-14 10:17:22 -0700

    Modify nested if statement to include an elif condition

--- Day-003/Exercises/0-control-flow.py
+++ Day-003/Exercises/0-control-flow.py
@@ -4,7 +4,9 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
-    if age <= 18:
+    if age < 12:
+        print("Please pay $5")
+    elif age <= 18 >= age:
         print("Please pay $7")
     else:
         print("Please pay $12")
