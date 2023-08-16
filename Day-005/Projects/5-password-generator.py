98a9061b372cb6b70de28365f51127c1cd65b7ce
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-16 10:34:32 -0700

    Import random module

---
+++ Day-005/Projects/5-password-generator.py
@@ -0,0 +1,6 @@
+import random
+
+print("Welcome to the PyPassword Generator!")
+num_letters = input("How many letters would you like in your password?\n")
+num_symbols = input("How many symbols would you like?\n")
+num_numbers = input("How many numbers would you like?\n")
