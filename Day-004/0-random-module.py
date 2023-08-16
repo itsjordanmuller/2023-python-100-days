0d43383ac3d8a1bafb46ccc60cf18a6132912b38
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-15 18:05:04 -0700

    Comment out the change in the second state, then append a new state to the end

--- Day-004/0-random-module.py
+++ Day-004/0-random-module.py
@@ -69,5 +69,6 @@ states_of_america = [
 print(states_of_america[0])
 print(states_of_america[-1])
 
-states_of_america[1] = "Pencilvania"
+# states_of_america[1] = "Pencilvania"
+states_of_america.append("Jordanland")
 print(states_of_america)
