a430738c6d7509eda24de7efbab5cbb1f941e213
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-16 10:26:31 -0700

    Calculate the total of the sum of numbers between 1 and 100

--- Day-005/Exercises/0-basic-loops.py
+++ Day-005/Exercises/0-basic-loops.py
@@ -4,3 +4,10 @@ for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
 print(fruits)
+
+total_number = 0
+
+for number in range(1, 101):
+    total_number += number
+
+print(total_number)
