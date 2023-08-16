4cb2fd3c04f23117822c0d4c246adde389fe2c04
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-15 17:56:04 -0700

    Remove my_module.py and pycache completely, comment out relevant code

--- Day-004/0-random-module.py
+++ Day-004/0-random-module.py
@@ -1,10 +1,11 @@
 import random
-import my_module
+
+# import my_module
 
 random_integer = random.randint(1, 10)
 print(random_integer)
 
-print(my_module.pi)
+# print(my_module.pi)
 
 random_float = random.random() * 5
 print(random_float)
