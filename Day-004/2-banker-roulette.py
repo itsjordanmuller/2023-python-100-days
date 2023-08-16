ffc1415dee0a25c61bed90ec462bfcc81dd6287e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-15 18:08:57 -0700

    Calculate the length of the list of names to create a random integer within that range
    The integer that is picked at random will correspond with the person who should pay the bill

--- Day-004/2-banker-roulette.py
+++ Day-004/2-banker-roulette.py
@@ -7,3 +7,5 @@ names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
+name_len = len(names) - 1
+print(f"{names[random.randint(0, name_len)]} is going to buy the meal today!")
