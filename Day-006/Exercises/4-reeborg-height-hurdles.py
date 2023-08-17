688be481442956973ebfbcfec44ce330487fb81a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-17 14:09:34 -0700

    Add completed solution for Reeborg to jump over variable height hurdles

---
+++ Day-006/Exercises/4-reeborg-height-hurdles.py
@@ -0,0 +1,25 @@
+# Variable Height Hurdles
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+# Use the below code to help Reeborg jump over hurdles of random height
+
+# def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+
+# def move_up():
+#   turn_left()
+#   while wall_on_right():
+#       move()
+#   turn_right()
+#   move()
+#   turn_right()
+#   while front_is_clear():
+#       move()
+#   turn_left()
+
+# while not at_goal():
+#   if wall_in_front():
+#       move_up()
+#   else:
+#       move()
