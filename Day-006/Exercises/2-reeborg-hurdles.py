47f22ba0e9c35a567ca3df97814534a84127db27
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-17 14:04:44 -0700

    Add completed solution for Reeborg to jump over 6 hurdles

---
+++ Day-006/Exercises/2-reeborg-hurdles.py
@@ -0,0 +1,22 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# Use the below code to help Reeborg get to the end of the 6 hurdles
+
+# def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+
+# def jump():
+#   move()
+#   turn_left()
+#   move()
+#   turn_right()
+#   move()
+#   turn_right()
+#   move()
+#   turn_left()
+
+# i = 0
+# while i < 6:
+#   jump()
+#   i += 1
