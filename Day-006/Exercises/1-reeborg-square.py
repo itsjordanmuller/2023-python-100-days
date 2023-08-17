2c795973d0dddc4e2a2e167ac19bf5d741c8d1d2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-17 14:02:49 -0700

    Add completed solution for Reeborg to walk in a square

---
+++ Day-006/Exercises/1-reeborg-square.py
@@ -0,0 +1,26 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+# Use the below code to make Reeborg the Robot walk in a square
+
+# def turn_around():
+#   turn_left()
+#   turn_left()
+
+# def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+
+# def move_three():
+#   move()
+#   move()
+#   move()
+
+# turn_left()
+# move_three()
+# turn_right()
+# move_three()
+# turn_right()
+# move_three()
+# turn_right()
+# move_three()
+# turn_right()
