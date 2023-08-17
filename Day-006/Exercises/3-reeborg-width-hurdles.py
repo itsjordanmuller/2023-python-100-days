ba63ea36acb826a68fb75e815c0dde8dfa402d9c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-17 14:08:04 -0700

    Add completed solution for Reeborg to jump over variable width hurdles

---
+++ Day-006/Exercises/3-reeborg-width-hurdles.py
@@ -0,0 +1,23 @@
+# Variable Width Hurdles
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# Use the below code to help Reeborg jump and run over any amount of hurdles spaced apart at random
+
+# def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+
+# def jump():
+#   turn_left()
+#   move()
+#   turn_right()
+#   move()
+#   turn_right()
+#   move()
+#   turn_left()
+
+# while not at_goal():
+#   if wall_in_front():
+#       jump()
+#   else:
+#       move()
