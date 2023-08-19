031f9417e717da558888194f5e53ec3602fd5849
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-19 15:34:26 -0700

    Define a function that will encrypt text

--- Day-008/Exercises/4-caesar-cipher1.py
+++ Day-008/Exercises/4-caesar-cipher1.py
@@ -57,3 +57,13 @@ text = input("Type your message:\n").lower()
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text, shift):
+    cipher_text = ""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is: {cipher_text}")
