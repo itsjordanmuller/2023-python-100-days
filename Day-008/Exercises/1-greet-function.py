ed7b805f46e4a17088c3af8522832f12aa11edc6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-19 15:26:57 -0700

    Define and call greet function to take name and location and form a printed greeting

--- Day-008/Exercises/1-greet-function.py
+++ Day-008/Exercises/1-greet-function.py
@@ -1,2 +1,11 @@
 name = input("What is the name of the person you are greeting? ")
 location = input("Where do they live? ")
+
+
+def greet(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+    print(f"Isn't it lovely out today, {name}?")
+
+
+greet(name, location)
