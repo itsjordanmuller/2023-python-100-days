08e985d3bc6670c84d48e0b8b6745b4d903e73f6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-19 15:28:28 -0700

    Define a function that calculates the amount of paint needed to cover a wall

--- Day-008/Exercises/2-paint-area-calculator.py
+++ Day-008/Exercises/2-paint-area-calculator.py
@@ -1,1 +1,7 @@
 import math
+
+
+def paint_calc(height, width, cover):
+    num_cans = (height * width) / cover
+    round_up_cans = math.ceil(num_cans)
+    print(f"You'll need {round_up_cans} cans of paint.")
