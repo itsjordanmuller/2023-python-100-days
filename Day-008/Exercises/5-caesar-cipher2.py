92f22f0c195884493a6d3fee1969aaac74971860
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-19 15:35:34 -0700

    Copy over the first bit of code for the caesar cipher project

---
+++ Day-008/Exercises/5-caesar-cipher2.py
@@ -0,0 +1,72 @@
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+text = input("Type your message:\n").lower()
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text, shift):
+    cipher_text = ""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is: {cipher_text}")
+
+
+encrypt(text, shift)
