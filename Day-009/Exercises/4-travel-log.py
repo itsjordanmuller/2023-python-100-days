356ea1eac1577bb8fceaa08ea2454608b442a98a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-20 14:29:33 -0700

    Create travel log lists with nested dictionaries

---
+++ Day-009/Exercises/4-travel-log.py
@@ -0,0 +1,4 @@
+travel_log = [
+    {"country": "France", "visits": 12, "cities": ["Paris", "Lille", "Dijon"]},
+    {"country": "Germany", "visits": 5, "cities": ["Berlin", "Hamburg", "Stuttgart"]},
+]
