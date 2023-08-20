a344fd0d0291e434fd27c8bf40da454709ad3a46
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-20 14:28:23 -0700

    Nest lists of cities that were visited inside of a travel log dictionary

--- Day-009/Exercises/3-nesting-dictionaries.py
+++ Day-009/Exercises/3-nesting-dictionaries.py
@@ -1,1 +1,9 @@
 capitals = {"France": "Paris", "Germany": "Berlin"}
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 12,
+    },
+}
