8c5ab6cb3586acf37ebab618a20143f8144bf366
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-20 14:28:51 -0700

    Nest dictionaries inside of a list

--- Day-009/Exercises/3-nesting-dictionaries.py
+++ Day-009/Exercises/3-nesting-dictionaries.py
@@ -7,3 +7,16 @@ travel_log = {
         "total_visits": 12,
     },
 }
+
+travel_log = {
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 12,
+    },
+}
