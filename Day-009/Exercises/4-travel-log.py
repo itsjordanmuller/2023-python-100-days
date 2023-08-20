b5bd95ad02259e0907e2d0b8e5796e2671c93f1b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-20 14:30:29 -0700

    Add Russia to the list of countries in the travel log using the new add country function

--- Day-009/Exercises/4-travel-log.py
+++ Day-009/Exercises/4-travel-log.py
@@ -10,3 +10,6 @@ def add_new_country(country_visited, times_visited, cities_visited):
     new_country["visits"] = times_visited
     new_country["cities"] = cities_visited
     travel_log.append(new_country)
+
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
