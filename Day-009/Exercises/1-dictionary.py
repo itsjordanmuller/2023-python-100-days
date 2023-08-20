4db48e231b21950fd1ef189de2bdabc4403a317f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-20 14:23:12 -0700

    Add a new term to the dictionary by definining it

--- Day-009/Exercises/1-dictionary.py
+++ Day-009/Exercises/1-dictionary.py
@@ -7,3 +7,5 @@ print(programming_dictionary["Bug"])
 print(programming_dictionary["Function"])
 
 print(programming_dictionary)
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
