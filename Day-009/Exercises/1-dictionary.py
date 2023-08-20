50a60beaf4a574c741a67adc38bb6b963657ea7b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-20 14:22:39 -0700

    Print out the dictionary terms using keys, and print the whole dictionary

--- Day-009/Exercises/1-dictionary.py
+++ Day-009/Exercises/1-dictionary.py
@@ -2,3 +2,8 @@ programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
 }
+
+print(programming_dictionary["Bug"])
+print(programming_dictionary["Function"])
+
+print(programming_dictionary)
