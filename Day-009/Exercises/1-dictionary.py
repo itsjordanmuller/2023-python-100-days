138b3cd43ab9be4a50cc823a5afe4ebf32a463ee
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-20 14:24:22 -0700

    Use a for loop to call out terms in the dictionary using keys

--- Day-009/Exercises/1-dictionary.py
+++ Day-009/Exercises/1-dictionary.py
@@ -12,3 +12,7 @@ programming_dictionary["Loop"] = "The action of doing something over and over ag
 
 print(programming_dictionary["Loop"])
 print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
