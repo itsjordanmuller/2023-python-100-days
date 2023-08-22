d13ed5a9a3da2af332d7dd9db8057d7d7ea8d6c5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-21 17:53:16 -0700

    Call the format name function to format the input first and last names

--- Day-010/Exercises/2-multiple-return-values.py
+++ Day-010/Exercises/2-multiple-return-values.py
@@ -4,3 +4,11 @@ def format_name(f_name, l_name):
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
     return f"{formatted_f_name} {formatted_l_name}"
+
+
+print(
+    format_name(
+        input("What is your first name? "),
+        input("What is your last name? "),
+    )
+)
