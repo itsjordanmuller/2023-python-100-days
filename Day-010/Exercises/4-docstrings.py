a01b2747f9984db2966c9b324058cbe2d75ba3ca
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-21 18:06:33 -0700

    Add a docstring to the format name function to give it a description

--- Day-010/Exercises/4-docstrings.py
+++ Day-010/Exercises/4-docstrings.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    """Take a first and last name and format it
+    to return the title case version of the name."""
     if f_name == "" or l_name == "":
         return "Empty Name, Please Provide a Valid Name"
     formatted_f_name = f_name.title()
