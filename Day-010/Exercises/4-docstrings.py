fe89da2f5c34d68469fa362aa7ae3a81251dac43
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-21 18:05:51 -0700

    Copy over format name function to demonstrate docstrings

---
+++ Day-010/Exercises/4-docstrings.py
@@ -0,0 +1,14 @@
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "Empty Name, Please Provide a Valid Name"
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+
+
+print(
+    format_name(
+        input("What is your first name? "),
+        input("What is your last name? "),
+    )
+)
