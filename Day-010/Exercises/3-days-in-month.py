7fd47b547532a6d19468b5c0274f84c4b31c369d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-21 17:54:59 -0700

    Get an input for the year and month, to determine the amount of days in that month keeping in mind leap years

--- Day-010/Exercises/3-days-in-month.py
+++ Day-010/Exercises/3-days-in-month.py
@@ -16,3 +16,9 @@ def days_in_month(year, month):
     if is_leap(year) and month == 2:
         return 29
     return month_days[month - 1]
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
