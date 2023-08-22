b55001bc0e0542f4b207abf22f8e2a46aa600271
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-21 17:51:14 -0700

    Define another function that will format an input name with correct capitalization

--- Day-010/Exercises/1-functions-with-outputs.py
+++ Day-010/Exercises/1-functions-with-outputs.py
@@ -4,3 +4,9 @@ def my_function():
 
 
 my_function()
+
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
