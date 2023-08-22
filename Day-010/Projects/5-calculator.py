5355047ea9f6be5390338e9b1ace51468d22ca0e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-21 18:12:56 -0700

    Continue to define the calculator function, setting up a while loop to pick numbers and see if the user wants to keep calculating more

--- Day-010/Projects/5-calculator.py
+++ Day-010/Projects/5-calculator.py
@@ -31,3 +31,17 @@ def calculator():
     for operator in operators:
         print(operator)
     calculating = True
+
+    while calculating:
+        operation_choice = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operators[operation_choice]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_choice} {num2} = {answer}")
+
+        if input("Do you want to continue? 'y'/'n' ") == "y":
+            num1 = answer
+        else:
+            calculating = False
+            calculator()
