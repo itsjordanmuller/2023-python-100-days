cda499ab88f3b567c70ec7cb0a8273ca8d5a65b3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-21 17:51:45 -0700

    Call the new formatted string function to format a name with incorrect capitalization

--- Day-010/Exercises/1-functions-with-outputs.py
+++ Day-010/Exercises/1-functions-with-outputs.py
@@ -10,3 +10,7 @@ def format_name(f_name, l_name):
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
     return f"{formatted_f_name} {formatted_l_name}"
+
+
+formatted_string = format_name("JoRdAn", "MULLER")
+print(formatted_string)
