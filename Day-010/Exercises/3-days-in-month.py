05f7b1979e8ff753224fdace3e96269b209e1ef6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-21 17:54:31 -0700

    Define another function to calculate the number of days in the provided month by referencing a list of day values, including leap scenario

--- Day-010/Exercises/3-days-in-month.py
+++ Day-010/Exercises/3-days-in-month.py
@@ -9,3 +9,10 @@ def is_leap(year):
             return True
     else:
         return False
+
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
