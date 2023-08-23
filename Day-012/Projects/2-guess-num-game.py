eac63d4718f0649830f47390746b9d22d0d33386
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-23 14:44:17 -0700

    Set the number of attempts depending on the input difficulty

--- Day-012/Projects/2-guess-num-game.py
+++ Day-012/Projects/2-guess-num-game.py
@@ -4,3 +4,12 @@ print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+if difficulty == "easy":
+    attempts = 10
+elif difficulty == "hard":
+    attempts = 5
+else:
+    print("Error, please input 'easy' or 'hard'")
+
+print(attempts)
