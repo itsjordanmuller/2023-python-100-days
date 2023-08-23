ca193c875fcae6afa6db0f402ac6c10f904aa4b4
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-22 17:26:55 -0700

    Define a function that calculates the score of the cards, checking if the total exceeds 21, and switching 11s to 1s

--- Day-011/Projects/1-blackjack.py
+++ Day-011/Projects/1-blackjack.py
@@ -6,3 +6,12 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
     return random.choice(cards)
+
+
+def calculate_score(cards):
+    score = sum(cards)
+    if 11 in cards and score > 21:
+        cards.remove(11)
+        cards.append(1)
+        score = sum(cards)
+    return score
