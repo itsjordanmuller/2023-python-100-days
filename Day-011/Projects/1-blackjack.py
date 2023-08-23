0d02b2a24c23172a5da8293c02b6d71900e75c23
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-22 17:27:35 -0700

    Define a function that compares the player and computer scores to determine a winner

--- Day-011/Projects/1-blackjack.py
+++ Day-011/Projects/1-blackjack.py
@@ -15,3 +15,20 @@ def calculate_score(cards):
         cards.append(1)
         score = sum(cards)
     return score
+
+
+def compare(player_score, computer_score):
+    if player_score > 21:
+        return "You lose."
+    elif computer_score > 21:
+        return "You win!"
+    elif player_score == computer_score:
+        return "It's a draw."
+    elif computer_score == 0:
+        return "You lose."
+    elif player_score == 0:
+        return "You win!"
+    elif player_score > computer_score:
+        return "You win!"
+    else:
+        return "You lose."
