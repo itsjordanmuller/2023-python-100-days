dc2defd7906cd95db643b5490b230022b12dd1cc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-23 14:23:43 -0700

    Show what happens when you create a variable inside of a function, yet again

--- Day-012/1-namespaces.py
+++ Day-012/1-namespaces.py
@@ -40,8 +40,13 @@
 
 
 game_level = 3
-enemies = ["Skeleton", "Zombie", "Alien"]
-if game_level < 5:
-    new_enemy = enemies[0]
 
-print(new_enemy)
+
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 5:
+        new_enemy = enemies[0]
+    print(new_enemy)
+
+
+# print(new_enemy)
