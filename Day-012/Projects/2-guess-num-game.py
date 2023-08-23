66eef4603d4d3d5a0e9148bdd5a7d05d9259aa2d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-23 14:41:58 -0700

    Take a user input to check if difficulty should be easy or hard

--- Day-012/Projects/2-guess-num-game.py
+++ Day-012/Projects/2-guess-num-game.py
@@ -3,3 +3,4 @@ from art import logo
 print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
