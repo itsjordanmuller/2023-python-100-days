d2602399ba13e66315ffea05242cc8fe8e3b0728
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-24 12:38:16 -0700

    Use a print statement to determine error location, word per page was culprit

--- Day-013/Exercises/1-beginner-debugging.py
+++ Day-013/Exercises/1-beginner-debugging.py
@@ -35,8 +35,10 @@
 pages = 0
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
+# print(pages)
+# print(word_per_page)
 print(total_words)
 
 # #Use a Debugger
