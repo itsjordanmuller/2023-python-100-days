ba7dedef2e16bea4492893ee78397d6d4aa6b86f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-24 12:38:33 -0700

    Move onto next section of code to debug, commenting out previous code

--- Day-013/Exercises/1-beginner-debugging.py
+++ Day-013/Exercises/1-beginner-debugging.py
@@ -32,21 +32,23 @@
 #     print(f"You can drive at age {age}.")
 
 # Print is Your Friend
-pages = 0
-word_per_page = 0
-pages = int(input("Number of pages: "))
-word_per_page = int(input("Number of words per page: "))
-total_words = pages * word_per_page
-# print(pages)
-# print(word_per_page)
-print(total_words)
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# # print(pages)
+# # print(word_per_page)
+# print(total_words)
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
 
-# mutate([1,2,3,5,8,13])
+# Use a Debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+    b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
