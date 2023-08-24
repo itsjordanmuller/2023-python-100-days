1a29aa2aa4f5a70431c08ca62ab56f8b32dfbffb
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-24 12:25:31 -0700

    Remove comments from first section to begin debugging

--- Day-013/Exercises/1-beginner-debugging.py
+++ Day-013/Exercises/1-beginner-debugging.py
@@ -1,11 +1,14 @@
 ############DEBUGGING#####################
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+
+# Describe Problem
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
 
 # # Reproduce the Bug
 # from random import randint
