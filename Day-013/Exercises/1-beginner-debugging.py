0b5d4c646f8528c954ec4bb3e4253386d39e5c06
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-24 12:32:12 -0700

    Move onto next section of code to debug, commenting out previous code

--- Day-013/Exercises/1-beginner-debugging.py
+++ Day-013/Exercises/1-beginner-debugging.py
@@ -12,19 +12,19 @@
 # my_function()
 
 # Reproduce the Bug
-from random import randint
+# from random import randint
 
-dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(0, 5)
-print(dice_imgs[dice_num])
-# print(dice_imgs[6])
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+# # print(dice_imgs[6])
 
-# # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+    print("You are a millenial.")
+elif year > 1994:
+    print("You are a Gen Z.")
 
 # # Fix the Errors
 # age = input("How old are you?")
