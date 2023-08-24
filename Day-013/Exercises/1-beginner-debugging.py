fe75d98ea61e64f128b82f2f9ac1ea37460961ad
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-24 12:27:57 -0700

    Check value of i, and fix the for loop range to allow print

--- Day-013/Exercises/1-beginner-debugging.py
+++ Day-013/Exercises/1-beginner-debugging.py
@@ -3,9 +3,10 @@
 
 # Describe Problem
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
+    # print(i)
 
 
 my_function()
