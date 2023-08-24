44eb0c830e96d4cd2d4dc37d0390aba03b2ceb75
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-24 12:48:05 -0700

    Convert input string to integer to correctly perform comparisons

--- Day-013/Exercises/3-debug-leap-year.py
+++ Day-013/Exercises/3-debug-leap-year.py
@@ -1,12 +1,12 @@
-# year = input("Which year do you want to check?")
+year = int(input("Which year do you want to check?"))
 
-# if year % 4 == 0:
-#   if year % 100 == 0:
-#     if year % 400 == 0:
-#       print("Leap year.")
-#     else:
-#       print("Not leap year.")
-#   else:
-#     print("Leap year.")
-# else:
-#   print("Not leap year.")
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
