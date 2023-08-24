be25e0f5bf5da5230c33a20b0fef68269030be3e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-24 12:35:43 -0700

    Fix multiple errors, create f-string, fix indentation, convert str to int

--- Day-013/Exercises/1-beginner-debugging.py
+++ Day-013/Exercises/1-beginner-debugging.py
@@ -27,9 +27,9 @@
 #     print("You are a Gen Z.")
 
 # Fix the Errors
-age = input("How old are you?")
+age = int(input("How old are you?"))
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
 
 # #Print is Your Friend
 # pages = 0
