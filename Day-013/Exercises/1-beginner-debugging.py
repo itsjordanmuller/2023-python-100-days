8fc9fd0511bde6ffaa07fe982f5edd66fca3afa1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-24 12:34:04 -0700

    Fix the comparison to provide a response in the case of 1994 input

--- Day-013/Exercises/1-beginner-debugging.py
+++ Day-013/Exercises/1-beginner-debugging.py
@@ -21,7 +21,7 @@
 
 # Play Computer
 year = int(input("What's your year of birth?"))
-if year > 1980 and year < 1994:
+if year > 1980 and year <= 1994:
     print("You are a millenial.")
 elif year > 1994:
     print("You are a Gen Z.")
