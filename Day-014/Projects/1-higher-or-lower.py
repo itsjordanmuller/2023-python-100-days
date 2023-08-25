5f24ca461570eef44faf850271cc9ee681ba003c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-25 15:36:37 -0700

    Define a function to check if the guess is correct to reduce code

--- Day-014/Projects/1-higher-or-lower.py
+++ Day-014/Projects/1-higher-or-lower.py
@@ -2,6 +2,18 @@ import random
 from art import logo, vs
 from game_data import data
 
+
+def is_guess_correct(guess, person_a_followers, person_b_followers):
+    """
+    Returns True if the guess is correct; otherwise, returns False.
+    """
+    if guess == "A" and person_a_followers > person_b_followers:
+        return True
+    elif guess == "B" and person_b_followers > person_a_followers:
+        return True
+    return False
+
+
 print(logo)
 print("Welcome to Higher or Lower!")
 print(
@@ -9,15 +21,13 @@ print(
 )
 print("Let's begin!\n")
 
-random_entry = random.choice(data)
+score = 0
+continue_game = True
 
 person_a = random.choice(data)
 person_b = random.choice(data)
 while person_b == person_a:
     person_b = random.choice(data)
-
-score = 0
-continue_game = 1
 
 while continue_game:
     person_a_name = person_a["name"]
@@ -26,29 +36,17 @@ while continue_game:
     person_b_followers = person_b["follower_count"]
 
     print(f"A: {person_a_name}{vs}\nB: {person_b_name}\n")
-    guess = input("Who has more followers? A or B?: ")
+    guess = input("Who has more followers? A or B?: ").upper()
 
-    if guess == "A":
-        if person_a_followers > person_b_followers:
-            score += 1
-            print(f"Correct! Your score is: {score}")
-            person_a = person_b
+    if is_guess_correct(guess, person_a_followers, person_b_followers):
+        score += 1
+        print(f"Correct! Your score is: {score}")
+        person_a = person_b
+        person_b = random.choice(data)
+        while person_b == person_a:
             person_b = random.choice(data)
-            while person_b == person_a:
-                person_b = random.choice(data)
-        else:
-            print(f"Sorry, that's wrong! Your final score is: {score}")
-            continue_game = 0
-    elif guess == "B":
-        if person_b_followers > person_a_followers:
-            score += 1
-            print(f"Correct! Your score is: {score}")
-            person_a = person_b
-            person_b = random.choice(data)
-            while person_b == person_a:
-                person_b = random.choice(data)
-        else:
-            print(f"Sorry, that's wrong! Your final score is: {score}")
-            continue_game = 0
+    elif guess in ["A", "B"]:
+        print(f"Sorry, that's wrong! Your final score is: {score}")
+        continue_game = False
     else:
         print("Input Error, please type 'A' or 'B'")
