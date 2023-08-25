49fcfa6a9e9f515636bd92624e928da8abc46b89
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-25 15:06:20 -0700

    Add some basic instructions for how the game is played and how the user should input

--- Day-014/Projects/1-higher-or-lower.py
+++ Day-014/Projects/1-higher-or-lower.py
@@ -2,4 +2,10 @@ import random
 from art import logo, vs
 
 print(logo)
-print(vs)
+print("Welcome to Higher or Lower!")
+print(
+    "You will guess which account has more followers, by entering either 'A' or 'B' to make a guess"
+)
+print("Let's begin!")
+
+# print(vs)
