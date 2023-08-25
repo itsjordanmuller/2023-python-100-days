2e0d03ce814d1fa888720af87c0fbf16a8d3a41a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-25 15:17:31 -0700

    Select a random person A and person B, storing the relevant data in variables for easy access

--- Day-014/Projects/1-higher-or-lower.py
+++ Day-014/Projects/1-higher-or-lower.py
@@ -1,11 +1,21 @@
 import random
 from art import logo, vs
+from game_data import data
 
 print(logo)
 print("Welcome to Higher or Lower!")
 print(
     "You will guess which account has more followers, by entering either 'A' or 'B' to make a guess"
 )
-print("Let's begin!")
+print("Let's begin!\n")
 
-# print(vs)
+random_entry = random.choice(data)
+
+person_a = random.choice(data)
+person_b = random.choice(data)
+person_a_name = person_a["name"]
+person_b_name = person_b["name"]
+person_a_followers = person_a["follower_count"]
+person_b_followers = person_b["follower_count"]
+
+print(f"A: {person_a_name}{vs}\nB: {person_b_name}")
