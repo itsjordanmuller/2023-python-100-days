fedc81debf668a604f3c0966a424f01ffa3fa406
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-25 15:38:39 -0700

    Continue to refactor by defining get new person function, using DRY principles

--- Day-014/Projects/1-higher-or-lower.py
+++ Day-014/Projects/1-higher-or-lower.py
@@ -7,11 +7,19 @@ def is_guess_correct(guess, person_a_followers, person_b_followers):
     """
     Returns True if the guess is correct; otherwise, returns False.
     """
-    if guess == "A" and person_a_followers > person_b_followers:
-        return True
-    elif guess == "B" and person_b_followers > person_a_followers:
-        return True
-    return False
+    return (guess == "A" and person_a_followers > person_b_followers) or (
+        guess == "B" and person_b_followers > person_a_followers
+    )
+
+
+def get_new_person(previous_person=None):
+    """
+    Returns a random person not equal to the previous person.
+    """
+    person = random.choice(data)
+    while person == previous_person:
+        person = random.choice(data)
+    return person
 
 
 print(logo)
@@ -24,16 +32,12 @@ print("Let's begin!\n")
 score = 0
 continue_game = True
 
-person_a = random.choice(data)
-person_b = random.choice(data)
-while person_b == person_a:
-    person_b = random.choice(data)
+person_a = get_new_person()
+person_b = get_new_person(person_a)
 
 while continue_game:
-    person_a_name = person_a["name"]
-    person_b_name = person_b["name"]
-    person_a_followers = person_a["follower_count"]
-    person_b_followers = person_b["follower_count"]
+    person_a_name, person_a_followers = person_a["name"], person_a["follower_count"]
+    person_b_name, person_b_followers = person_b["name"], person_b["follower_count"]
 
     print(f"A: {person_a_name}{vs}\nB: {person_b_name}\n")
     guess = input("Who has more followers? A or B?: ").upper()
@@ -41,10 +45,7 @@ while continue_game:
     if is_guess_correct(guess, person_a_followers, person_b_followers):
         score += 1
         print(f"Correct! Your score is: {score}")
-        person_a = person_b
-        person_b = random.choice(data)
-        while person_b == person_a:
-            person_b = random.choice(data)
+        person_a, person_b = person_b, get_new_person(person_b)
     elif guess in ["A", "B"]:
         print(f"Sorry, that's wrong! Your final score is: {score}")
         continue_game = False
