8a2ab5dae6c464fe11c4fbfb9305908307a57571
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-26 14:49:32 -0700

    Create project file, and take notes on the ingredients for each type of coffee

---
+++ Day-015/Projects/1-coffee-machine.py
@@ -0,0 +1,5 @@
+# Flavors: Espresso, Latte, Cappuccino
+
+# Espresso Recipe: 50ml Water, 18g Coffee, 0ml Milk
+# Latte Recipe: 200ml Water, 24g Coffee, 150ml Milk
+# Cappuccino Recipe: 250ml Water, 24g Coffee, 100ml Milk
