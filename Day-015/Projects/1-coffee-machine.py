953b249cebbf1f2489044baaf7f69809ee8db4c1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-26 14:59:44 -0700

    Copy the menu and resources from the Replit starting code

--- Day-015/Projects/1-coffee-machine.py
+++ Day-015/Projects/1-coffee-machine.py
@@ -20,3 +20,35 @@
 # Allow the user to keep ordering more, but if there's not enough of a certain resource, tell them which resource there is not enough of
 
 # Process the coins inserted into the machine, calculating the excess and returning any money not used for the coffee, checking to make sure the transaction was successful and the user paid enough. If the transaction is successful, deduct the money from the total input and return the change.
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    },
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
