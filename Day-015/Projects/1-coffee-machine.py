30b6d2bd4124516646cc4542bfd1782952b67f24
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-26 14:51:11 -0700

    Add a note containing the starting resources inside of the coffee machine

--- Day-015/Projects/1-coffee-machine.py
+++ Day-015/Projects/1-coffee-machine.py
@@ -3,3 +3,5 @@
 # Espresso $1.5 | Recipe: 50ml Water, 18g Coffee, 0ml Milk
 # Latte $2.5 | Recipe: 200ml Water, 24g Coffee, 150ml Milk
 # Cappuccino $3.0 | Recipe: 250ml Water, 24g Coffee, 100ml Milk
+
+# Coffee Machine Resources: (300ml Water, 200ml Milk, 100g Coffee)
