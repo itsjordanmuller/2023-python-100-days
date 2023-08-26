5bea5aa09620f1aa2c596226a68d857e8cdc68ab
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-26 15:40:24 -0700

    Define a function to process the transaction and exchange money and change, modify main loop

--- Day-015/Projects/1-coffee-machine.py
+++ Day-015/Projects/1-coffee-machine.py
@@ -41,6 +41,22 @@ def get_money():
     return total_input
 
 
+def process_transaction(amount_entered, drink_cost):
+    """
+    Process the money transaction.
+    If the user has input enough money, update the money resource, deduct the drink cost, and return the change.
+    If not, let the user know they haven't input enough money.
+    """
+    if amount_entered >= drink_cost:
+        change = amount_entered - drink_cost
+        resources["money"] += drink_cost
+        print(f"Transaction successful! Here is your change: ${change:.2f}")
+        return True
+    else:
+        print(f"Sorry, that's not enough money. Money refunded: ${amount_entered:.2f}")
+        return False
+
+
 def check_resources(drink):
     """
     Returns True if resources are sufficient for the chosen drink. Otherwise, returns False and
@@ -90,8 +106,6 @@ resources = {
 operation = ""
 status = "on"
 
-get_money()
-
 while status == "on":
     operation = input("What would you like? (espresso/latte/cappuccino): ")
     if operation == "off":
@@ -99,7 +113,14 @@ while status == "on":
         status = "off"
     elif operation in ["espresso", "latte", "cappuccino"]:
         if check_resources(operation):
-            print(f"You'd like to make a {operation}")
+            print(f"You have selected a {operation}. Please insert money.")
+            total_money = get_money()
+            if process_transaction(total_money, MENU[operation]["cost"]):
+                for ingredient, amount in MENU[operation]["ingredients"].items():
+                    resources[ingredient] -= amount
+                print(f"Here's your {operation} ☕ Enjoy!")
+            else:
+                print("Please try again with enough money.")
         else:
             print(f"Sorry, we can't make a {operation} right now.")
     elif operation == "report":
