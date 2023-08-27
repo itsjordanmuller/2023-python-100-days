64cd829c98c5d941e5cf81d08158d92960d3dbbc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-27 16:54:12 -0700

    Comment out previous code to create a blank slate for other packages

--- Day-016/Exercises/1-object-oriented.py
+++ Day-016/Exercises/1-object-oriented.py
@@ -1,16 +1,16 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
 # import tkinter as TK
 # import another_module
 
 # print(another_module.another_variable)
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-print(my_screen.canvwidth)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# print(my_screen.canvwidth)
+# my_screen.exitonclick()
