1c6dabb6465996fa5aea5f9900916c846c6f4184
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-27 16:49:42 -0700

    Change the shape of timmy to an actual turtle shape

--- Day-016/Exercises/1-object-oriented.py
+++ Day-016/Exercises/1-object-oriented.py
@@ -6,6 +6,7 @@ from turtle import Turtle, Screen
 # print(another_module.another_variable)
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
 
 my_screen = Screen()
 print(my_screen.canvheight)
