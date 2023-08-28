4054a18b277f28c6c95881332e10030cb62e96fc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-28 13:48:32 -0700

    Create a for loop to create a list of question objects from the question data

--- Day-017/Projects/main.py
+++ Day-017/Projects/main.py
@@ -1,5 +1,15 @@
 from question_model import Question
 from data import question_data
 
-question_1 = Question(question_data[0]["text"], question_data[0]["answer"])
-print(question_1.text, question_1.answer)
+# question_1 = Question(question_data[0]["text"], question_data[0]["answer"])
+# print(question_1.text, question_1.answer)
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
