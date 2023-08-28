adf0dae08b104bdb9e6c73bbe206224165bc9bf7
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-27 17:51:15 -0700

    Add another column with the different types for each Pokemon

--- Day-016/Exercises/1-object-oriented.py
+++ Day-016/Exercises/1-object-oriented.py
@@ -16,6 +16,8 @@
 # my_screen.exitonclick()
 
 from prettytable import PrettyTable
+
 table = PrettyTable()
-table.add_column("Pokemon Name",("Pikachu","Squirtle", "Charmander"))
+table.add_column("Pokemon Name", ("Pikachu", "Squirtle", "Charmander"))
+table.add_column("Type", ("Electric", "Water", "Fire"))
 print(table)
