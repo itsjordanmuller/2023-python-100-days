5d3935ffc0c1a5d9905e8ef4fd58e55cbb942b40
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-28 13:24:55 -0700

    Add an attribute for the total number of followers

--- Day-017/Exercises/classes.py
+++ Day-017/Exercises/classes.py
@@ -6,17 +6,18 @@
 
 class User:
     # pass
-    def __init__(self, user_id, username):
+    def __init__(self, user_id, username, followers):
         print("New user being created...")
         self.id = user_id
         self.username = username
+        self.followers = followers
 
 
-user_1 = User("001", "Jordan")
+user_1 = User("001", "Jordan", 0)
 # user_1.id = "001"
 # user_1.username = "Jordan"
 
-print(user_1.id, user_1.username)
+print(user_1.id, user_1.username, user_1.followers)
 
-user_2 = User("002", "Angela")
-print(user_2.id, user_2.username)
+user_2 = User("002", "Angela", 0)
+print(user_2.id, user_2.username, user_2.followers)
