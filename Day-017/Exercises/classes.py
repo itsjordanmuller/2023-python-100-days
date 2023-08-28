0e7a9eb4213826d605aa2a2f4942e7395ddaeaf5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-28 13:25:36 -0700

    Show how you can set a default value inside of the class definition to avoid needing to input during object creation

--- Day-017/Exercises/classes.py
+++ Day-017/Exercises/classes.py
@@ -6,18 +6,18 @@
 
 class User:
     # pass
-    def __init__(self, user_id, username, followers):
+    def __init__(self, user_id, username):
         print("New user being created...")
         self.id = user_id
         self.username = username
-        self.followers = followers
+        self.followers = 0
 
 
-user_1 = User("001", "Jordan", 0)
+user_1 = User("001", "Jordan")
 # user_1.id = "001"
 # user_1.username = "Jordan"
 
 print(user_1.id, user_1.username, user_1.followers)
 
-user_2 = User("002", "Angela", 0)
+user_2 = User("002", "Angela")
 print(user_2.id, user_2.username, user_2.followers)
