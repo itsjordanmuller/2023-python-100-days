0977a25e5052dbdc127958b346a16197225ae6ee
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 15:20:33 -0700

    Define a function that can calculate interior angles of regular shapes

--- Day-018/Exercises/1-turtle-challenge.py
+++ Day-018/Exercises/1-turtle-challenge.py
@@ -13,11 +13,21 @@ tim.color("firebrick1")
 # tim.forward(100)
 # tim.right(90)
 
-for i in range(50):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+# for i in range(50):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+
+def calc_interior_angles(n):
+    """Calculates the interior angles of regular shapes given a number of sides as an input n"""
+    angle = ((n - 2) * 180) / n
+    print(angle)
+
+
+calc_interior_angles(3)
+
 
 # Leave the screen/exit on click settings at the end of code
 screen = Screen()
