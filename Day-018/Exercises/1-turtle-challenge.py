abde2ffd7d57bbe432ba7fb99086fd796a9e615c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 15:36:24 -0700

    Import random module, generate a random rgb color, use it to change the color each loop

--- Day-018/Exercises/1-turtle-challenge.py
+++ Day-018/Exercises/1-turtle-challenge.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 tim.shape("turtle")
@@ -20,13 +21,27 @@ tim.color("firebrick1")
 #     tim.pendown()
 
 
+def random_color():
+    """Generates a random color using RGB color format"""
+    r = random.randint(1, 255)
+    g = random.randint(1, 255)
+    b = random.randint(1, 255)
+    return r, g, b
+
+
 def calc_exterior_angles(n):
     """Calculates the exterior angles of regular shapes given a number of sides as an input n"""
     angle = 360 / n
     return angle
 
 
+# Leave the screen/exit on click settings at the end of code
+screen = Screen()
+
+screen.colormode(255)
+
 for sides in range(3, 11):
+    tim.color(random_color())
     current_angle = calc_exterior_angles(sides)
 
     for step in range(0, sides):
@@ -34,6 +49,4 @@ for sides in range(3, 11):
         tim.right(current_angle)
 
 
-# Leave the screen/exit on click settings at the end of code
-screen = Screen()
 screen.exitonclick()
