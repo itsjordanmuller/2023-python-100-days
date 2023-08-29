bc548d1823e01c29e5b98243f213c22b69c1469c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 15:03:25 -0700

    Change the color of the turtle object to firebrick1 named color

--- Day-018/Exercises/1-turtle-challenge.py
+++ Day-018/Exercises/1-turtle-challenge.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("firebrick1")
 
 # Leave the screen/exit on click settings at the end of code
 screen = Screen()
