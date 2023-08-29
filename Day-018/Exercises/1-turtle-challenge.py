6f1ba6268c5a780e1bb598400273cb93659e273e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 15:28:51 -0700

    Modify interior angles function to calculate exterior angles instead
    Test out drawing using nested loops

--- Day-018/Exercises/1-turtle-challenge.py
+++ Day-018/Exercises/1-turtle-challenge.py
@@ -20,13 +20,18 @@ tim.color("firebrick1")
 #     tim.pendown()
 
 
-def calc_interior_angles(n):
-    """Calculates the interior angles of regular shapes given a number of sides as an input n"""
-    angle = ((n - 2) * 180) / n
-    print(angle)
+def calc_exterior_angles(n):
+    """Calculates the exterior angles of regular shapes given a number of sides as an input n"""
+    angle = 360 / n
+    return angle
 
 
-calc_interior_angles(3)
+for sides in range(3, 11):
+    current_angle = calc_exterior_angles(sides)
+
+    for step in range(0, sides):
+        tim.forward(100)
+        tim.right(current_angle)
 
 
 # Leave the screen/exit on click settings at the end of code
