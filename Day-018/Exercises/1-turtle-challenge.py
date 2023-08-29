e17cde39015b248ed831c8478d033a426515e1b5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 15:14:01 -0700

    Make tim the turtle draw a dashed line using a for loop and penup pendown

--- Day-018/Exercises/1-turtle-challenge.py
+++ Day-018/Exercises/1-turtle-challenge.py
@@ -3,14 +3,21 @@ from turtle import Turtle, Screen
 tim = Turtle()
 tim.shape("turtle")
 tim.color("firebrick1")
-tim.forward(100)
-tim.right(90)
-tim.forward(100)
-tim.right(90)
-tim.forward(100)
-tim.right(90)
-tim.forward(100)
-tim.right(90)
+
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+
+for i in range(50):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 # Leave the screen/exit on click settings at the end of code
 screen = Screen()
