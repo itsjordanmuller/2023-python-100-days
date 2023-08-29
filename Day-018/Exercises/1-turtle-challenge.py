143780a1c2ee593ddf784cf2a8cb8f37fe3c2aa8
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 15:00:31 -0700

    Change the shape of the turtle to an actual turtle

--- Day-018/Exercises/1-turtle-challenge.py
+++ Day-018/Exercises/1-turtle-challenge.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
 
+# Leave the screen/exit on click settings at the end of code
 screen = Screen()
 screen.exitonclick()
