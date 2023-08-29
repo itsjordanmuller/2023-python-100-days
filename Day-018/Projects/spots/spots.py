38c6f9c0d089a71d27904b3afc8c755dc990386e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 16:38:25 -0700

    Define a function to draw small spots, and use a loop to draw them in a set grid
    Each spot will have a random color picked from the list of extracted colors

--- Day-018/Projects/spots/spots.py
+++ Day-018/Projects/spots/spots.py
@@ -2,6 +2,17 @@ import colorgram
 import random
 from turtle import Turtle, Screen
 
+
+def draw_spot(position, color, size=5):
+    painter.penup()
+    painter.color(color)
+    painter.goto(position)
+    painter.pendown()
+    painter.begin_fill()
+    painter.circle(size)
+    painter.end_fill()
+
+
 colors = colorgram.extract("damien-hirst-spots.jpg", 30)
 color_list = [(color.rgb.r, color.rgb.g, color.rgb.b) for color in colors]
 
@@ -11,8 +22,21 @@ color_list = [(color.rgb.r, color.rgb.g, color.rgb.b) for color in colors]
 
 screen = Screen()
 screen.bgcolor("white")
+screen.colormode(255)
 
 painter = Turtle()
 painter.speed(10)
+
+grid_spacing = 30
+grid_size = 5
+
+for x in range(
+    -(grid_spacing * grid_size) // 2, (grid_spacing * grid_size) // 2, grid_spacing
+):
+    for y in range(
+        -(grid_spacing * grid_size) // 2, (grid_spacing * grid_size) // 2, grid_spacing
+    ):
+        color = random.choice(color_list)
+        draw_spot((x, y), color)
 
 screen.exitonclick()
