57d5bd6c3ae0335c7aa75ab277335e29166c9dcb
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 15:06:22 -0700

    Use the rename symbol feature in VS Code to rename timmy_the_turtle to shorter tim

--- Day-018/Exercises/1-turtle-challenge.py
+++ Day-018/Exercises/1-turtle-challenge.py
@@ -1,15 +1,15 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("firebrick1")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
+tim = Turtle()
+tim.shape("turtle")
+tim.color("firebrick1")
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
 
 # Leave the screen/exit on click settings at the end of code
 screen = Screen()
