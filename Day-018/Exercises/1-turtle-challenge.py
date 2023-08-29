46978866c4ec33f1e1a33b916882f1225530cb1d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 15:05:18 -0700

    Use forward and right methods to make the turtle draw a square

--- Day-018/Exercises/1-turtle-challenge.py
+++ Day-018/Exercises/1-turtle-challenge.py
@@ -3,6 +3,13 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("firebrick1")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
 
 # Leave the screen/exit on click settings at the end of code
 screen = Screen()
