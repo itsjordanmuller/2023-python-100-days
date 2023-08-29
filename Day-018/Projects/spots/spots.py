5c4e47a6cc21070561db6c33b1e27a9a768b78aa
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-29 16:31:11 -0700

    Store the extracted colors in a list, and setup the screen and turtle to paint

--- Day-018/Projects/spots/spots.py
+++ Day-018/Projects/spots/spots.py
@@ -1,2 +1,18 @@
 import colorgram
-from turtle import Turtle
+import random
+from turtle import Turtle, Screen
+
+colors = colorgram.extract("damien-hirst-spots.jpg", 30)
+color_list = [(color.rgb.r, color.rgb.g, color.rgb.b) for color in colors]
+
+# for color in colors:
+#     r, g, b = color.rgb
+#     print(f"({r}, {g}, {b})")
+
+screen = Screen()
+screen.bgcolor("white")
+
+painter = Turtle()
+painter.speed(10)
+
+screen.exitonclick()
