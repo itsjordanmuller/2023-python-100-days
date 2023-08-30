ddd4e655f1caec823f6cd976a22dfb2fcfc15e44
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-30 13:05:15 -0700

    Add the ability to turn clockwise and counter-clockwise

--- Day-019/Projects/1-etch-a-sketch.py
+++ Day-019/Projects/1-etch-a-sketch.py
@@ -12,9 +12,20 @@ def move_backwards():
     tim.backward(10)
 
 
+def turn_clockwise():
+    tim.right(5)
+
+
+def turn_counter_clockwise():
+    tim.left(5)
+
+
 screen.listen()
 
 screen.onkey(fun=move_forwards, key="w")
 screen.onkey(fun=move_backwards, key="s")
+screen.onkey(fun=turn_clockwise, key="d")
+screen.onkey(fun=turn_counter_clockwise, key="a")
+
 
 screen.exitonclick()
