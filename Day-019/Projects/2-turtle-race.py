c2f10c13cd797f62d073dc1a40341f005ad03926
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-30 13:16:28 -0700

    Use the textinput method from turtle graphics to prompt the user for their bet

--- Day-019/Projects/2-turtle-race.py
+++ Day-019/Projects/2-turtle-race.py
@@ -4,5 +4,10 @@ tim = Turtle()
 screen = Screen()
 
 screen.setup(width=500, height=400)
+user_bet = screen.textinput(
+    title="Make your bet!",
+    prompt="Type a color to place your bet on which turtle will win the race: ",
+)
+print(f"You bet on {user_bet}!")
 
 screen.exitonclick()
