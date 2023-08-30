49e762692ea527458d96d93e12c57802cabbbc60
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-30 13:08:39 -0700

    Add the ability to do a full reset, returning the turtle to center

--- Day-019/Projects/1-etch-a-sketch.py
+++ Day-019/Projects/1-etch-a-sketch.py
@@ -24,6 +24,13 @@ def clear_screen():
     tim.clear()
 
 
+def full_reset():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
 screen.listen()
 
 screen.onkey(fun=move_forwards, key="w")
@@ -31,5 +38,6 @@ screen.onkey(fun=move_backwards, key="s")
 screen.onkey(fun=turn_clockwise, key="d")
 screen.onkey(fun=turn_counter_clockwise, key="a")
 screen.onkey(fun=clear_screen, key="c")
+screen.onkey(fun=full_reset, key="r")
 
 screen.exitonclick()
