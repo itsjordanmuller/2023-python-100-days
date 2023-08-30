0ae38d8ad15935a0e1458a0721d1f9318b73547a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-30 12:57:58 -0700

    Create a function to make the turtle move forwards, call it when the user presses space, and set an exit on click

--- Day-019/Exercises/event-listeners.py
+++ Day-019/Exercises/event-listeners.py
@@ -3,4 +3,11 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
+
+def move_forwards():
+    tim.forward(10)
+
+
 screen.listen()
+screen.onkey(fun=move_forwards, key="space")
+screen.exitonclick()
