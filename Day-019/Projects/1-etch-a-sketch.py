af03cc8547f64a0ccf9cba92b5db844881383267
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-30 13:06:57 -0700

    Add the ability to clear the screen, and change how fast the turtle turns

--- Day-019/Projects/1-etch-a-sketch.py
+++ Day-019/Projects/1-etch-a-sketch.py
@@ -13,11 +13,15 @@ def move_backwards():
 
 
 def turn_clockwise():
-    tim.right(5)
+    tim.right(10)
 
 
 def turn_counter_clockwise():
-    tim.left(5)
+    tim.left(10)
+
+
+def clear_screen():
+    tim.clear()
 
 
 screen.listen()
@@ -26,6 +30,6 @@ screen.onkey(fun=move_forwards, key="w")
 screen.onkey(fun=move_backwards, key="s")
 screen.onkey(fun=turn_clockwise, key="d")
 screen.onkey(fun=turn_counter_clockwise, key="a")
-
+screen.onkey(fun=clear_screen, key="c")
 
 screen.exitonclick()
