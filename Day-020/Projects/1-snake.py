aa98b9e4e0c1237ba255435930a2ed75e85a0078
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-31 14:22:50 -0700

    Create a for loop to automatically create a set amount of segments moving left from the origin

--- Day-020/Projects/1-snake.py
+++ Day-020/Projects/1-snake.py
@@ -14,18 +14,24 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-segment_1 = Turtle("square")
-segment_1.color("white")
-segment_1.penup()
+segments = []
+for i in range(3):
+    x_position = i * -20
+    segment = create_segment(x_position, 0)
+    segments.append(segment)
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.penup()
-segment_2.goto(x=-20, y=0)
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+# segment_1.penup()
 
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.penup()
-segment_3.goto(x=-40, y=0)
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.penup()
+# segment_2.goto(x=-20, y=0)
+
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.penup()
+# segment_3.goto(x=-40, y=0)
 
 screen.exitonclick()
