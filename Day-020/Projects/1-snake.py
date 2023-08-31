c5f54fefb2ea203706865ee1d80d7ddd1ec9c2e0
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-31 14:27:32 -0700

    Create a nested loop to move the snake forward while the game is still on

--- Day-020/Projects/1-snake.py
+++ Day-020/Projects/1-snake.py
@@ -20,18 +20,9 @@ for i in range(3):
     segment = create_segment(x_position, 0)
     segments.append(segment)
 
-# segment_1 = Turtle("square")
-# segment_1.color("white")
-# segment_1.penup()
-
-# segment_2 = Turtle("square")
-# segment_2.color("white")
-# segment_2.penup()
-# segment_2.goto(x=-20, y=0)
-
-# segment_3 = Turtle("square")
-# segment_3.color("white")
-# segment_3.penup()
-# segment_3.goto(x=-40, y=0)
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 screen.exitonclick()
