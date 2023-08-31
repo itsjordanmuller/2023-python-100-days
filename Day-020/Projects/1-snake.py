85d1214adbc943070d5d3062f8b14a11891dcd0a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-08-31 14:21:22 -0700

    Manually create the first 3 segments of the snake

--- Day-020/Projects/1-snake.py
+++ Day-020/Projects/1-snake.py
@@ -5,5 +5,18 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+segment_1 = Turtle("square")
+segment_1.color("white")
+segment_1.penup()
+
+segment_2 = Turtle("square")
+segment_2.color("white")
+segment_2.penup()
+segment_2.goto(x=-20, y=0)
+
+segment_3 = Turtle("square")
+segment_3.color("white")
+segment_3.penup()
+segment_3.goto(x=-40, y=0)
 
 screen.exitonclick()
