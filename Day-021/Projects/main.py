e71d189209ce7cdbb13ec767d700d10c9360c069
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-01 15:24:57 -0700

    Copy over the snake game files from the first day/half of the project

---
+++ Day-021/Projects/main.py
@@ -0,0 +1,26 @@
+from turtle import Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.move_left, "a")
+screen.onkey(snake.move_right, "d")
+screen.onkey(snake.move_up, "w")
+screen.onkey(snake.move_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
