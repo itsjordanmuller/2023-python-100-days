fa046d405d5f9837c938234b114cdd09fadf99d4
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-01 15:48:24 -0700

    Begin to create scoreboard turtle, move it into position and check it by creating an object in main.py

---
+++ Day-021/Projects/scoreboard.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 280)
--- Day-021/Projects/tempCodeRunnerFile.py
+++ Day-021/Projects/tempCodeRunnerFile.py
@@ -2,7 +2,6 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
-from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,7 +11,6 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.move_left, "a")
