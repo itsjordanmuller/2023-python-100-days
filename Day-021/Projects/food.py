04883c569437003b1279aca67835860ad55ff828
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-01 15:29:38 -0700

    Set the color of the food to yellow, and make it appear as fast as possible when called

--- Day-021/Projects/food.py
+++ Day-021/Projects/food.py
@@ -7,3 +7,5 @@ class Food(Turtle):
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("yellow")
+        self.speed("fastest")
