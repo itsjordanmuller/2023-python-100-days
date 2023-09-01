16744db7c3837aa5f7b9607859191324e44284c6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-01 15:50:35 -0700

    Hide the turtle shape, goto top center, and write a test score

--- Day-021/Projects/scoreboard.py
+++ Day-021/Projects/scoreboard.py
@@ -7,4 +7,6 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 280)
+        self.hideturtle()
+        self.goto(0, 265)
+        self.write("Score: 0", align="center", font=("Arial", 20, "normal"))
