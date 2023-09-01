34b0592c8228efc1dc0cc66a7fb1c263e4ade0d8
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-01 15:28:52 -0700

    Begin to modify the food class using turtle methods and inheritance

--- Day-021/Projects/food.py
+++ Day-021/Projects/food.py
@@ -1,6 +1,9 @@
 from turtle import Turtle
 
 
-class Food:
+class Food(Turtle):
     def __init__(self):
-        pass
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
