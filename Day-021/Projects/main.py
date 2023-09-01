6fe6539149a92876e48b596469e9b0988936519e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-01 16:18:54 -0700

    Detect potential collisions between the head and the rest of the segments of the snake
    Call the game over method if the snake collides with it's own body

--- Day-021/Projects/main.py
+++ Day-021/Projects/main.py
@@ -43,4 +43,12 @@ while game_is_on:
         scoreboard.game_over()
         print(f"Game over. You got a final score of: {scoreboard.score}")
 
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+            print(f"Game over. You got a final score of: {scoreboard.score}")
+
 screen.exitonclick()
