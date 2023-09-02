18caf0ae3088c36197129a1ecceef4a7826cc6ff
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-02 14:24:38 -0700

    Create a screen based on the resolution of the original Pong game

---
+++ Day-022/Projects/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=858, height=525)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+
+screen.exitonclick()
