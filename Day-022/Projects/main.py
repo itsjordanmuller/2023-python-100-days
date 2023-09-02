4eb7212cd90d59c98dca4ed244c84747d806ae54
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-02 14:31:58 -0700

    Create a basic paddle class, and create a new paddle object for the right paddle

--- Day-022/Projects/main.py
+++ Day-022/Projects/main.py
@@ -1,10 +1,11 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=858, height=525)
 screen.bgcolor("black")
 screen.title("Pong Game")
-screen.tracer(0)
 
+right_paddle = Paddle((410, 0))
 
 screen.exitonclick()
---
+++ Day-022/Projects/paddle.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.goto(position)
