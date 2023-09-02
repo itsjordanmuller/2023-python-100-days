535c6d16793ebd8e9ca2ffda6de73a5b769802ba
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-02 14:50:11 -0700

    Create a second paddle on the left with different controls

--- Day-022/Projects/main.py
+++ Day-022/Projects/main.py
@@ -7,9 +7,12 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 
 right_paddle = Paddle((410, 0))
+left_paddle = Paddle((-415, 0))
 
 screen.listen()
 screen.onkey(fun=right_paddle.move_up, key="Up")
 screen.onkey(fun=right_paddle.move_down, key="Down")
+screen.onkey(fun=left_paddle.move_up, key="w")
+screen.onkey(fun=left_paddle.move_down, key="s")
 
 screen.exitonclick()
