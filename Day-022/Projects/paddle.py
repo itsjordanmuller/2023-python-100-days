648a97cc44972393bebc4eb1fd3f5b691b2cdfd8
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-02 14:46:59 -0700

    Stretch the paddle shape to make it roughly 1/3 the size of the screen

--- Day-022/Projects/paddle.py
+++ Day-022/Projects/paddle.py
@@ -6,6 +6,7 @@ class Paddle(Turtle):
         super().__init__()
         self.shape("square")
         self.color("white")
+        self.shapesize(stretch_wid=9, stretch_len=1)
         self.penup()
         self.goto(position)
 
