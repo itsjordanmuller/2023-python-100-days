811b717205c6cfb17b614111741a4e591de6d7c1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-03 15:46:41 -0700

    Import starting files and draw the turtle on the screen to start Frogger game clone

---
+++ Day-023/Projects/player.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.setheading(90)
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        move_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), move_y)
