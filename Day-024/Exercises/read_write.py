59f435933c4eeb82d9aa54bec8ba6bbd6e6d92ec
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-04 14:19:57 -0700

    Use the write method to create a file that doesn't already exist

--- Day-024/Exercises/read_write.py
+++ Day-024/Exercises/read_write.py
@@ -4,6 +4,9 @@ with open("data.txt", mode="w") as file:
 with open("data.txt", mode="a") as file:
     file.write("\nAnother line of text!")
 
+with open("new_data.txt", mode="w") as file:
+    file.write("Here's a new file!\nIt got created because it didn't already exist!")
+
 # file = open("data.txt")
 with open("data.txt") as file:
     contents = file.read()
