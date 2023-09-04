112947524e6f7f6616b32dba73dd4605dbfb861e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-04 14:45:09 -0700

    Open and read the starting letter to a list, then join the contents of that list into a single string before writing to a test file

--- Day-024/Projects/Mail Merge/main.py
+++ Day-024/Projects/Mail Merge/main.py
@@ -1,8 +1,15 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Input/Letters/starting_letter.txt") as file:
+    contents = file.readlines()
+    print(contents)
+
+with open("Output/ReadyToSend/test.txt", mode="w") as file:
+    file.write("".join(contents))
