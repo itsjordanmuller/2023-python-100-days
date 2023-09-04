495ba436ecdae4bc9e052a6e723b810b05e920c6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-04 13:50:47 -0700

    Import previous snake game final project to add high score functionality

---
+++ Day-024/Projects/scoreboard.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+BIG_FONT = ("Courier", 32, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 265)
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
+    def game_over(self):
+        self.clear()
+        self.goto(0, 10)
+        self.color("red")
+        self.write(f"GAME OVER!", align=ALIGNMENT, font=BIG_FONT)
+        self.goto(0, -30)
+        self.write(f"Final Score: {self.score}", align=ALIGNMENT, font=FONT)
