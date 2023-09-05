57ce527387a45cadaee2db10770d4fe251fa3cc7
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 13:16:55 -0700

    Use does not equal to avoid unnecessary pass and else lines

--- Day-025/Exercises/Weather/read_csv.py
+++ Day-025/Exercises/Weather/read_csv.py
@@ -8,8 +8,6 @@ with open("weather_data.csv") as file:
     data = csv.reader(file)
     temperatures = []
     for row in data:
-        if row[1] == "temp":
-            pass
-        else:
+        if row[1] != "temp":
             temperatures.append(int(row[1]))
     print(temperatures)
