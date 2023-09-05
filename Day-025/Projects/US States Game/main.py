ea75dfa9fd3f59aeeb29d20a1135eb295bff2e7a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 14:38:03 -0700

    Get a guess and convert it to title case, prepare a dataframe with the states data

--- Day-025/Projects/US States Game/main.py
+++ Day-025/Projects/US States Game/main.py
@@ -1,4 +1,5 @@
 import turtle
+
 import pandas
 
 screen = turtle.Screen()
@@ -6,12 +7,22 @@ screen.title("US States Game")
 screen.setup(width=725, height=491)
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-answer_state = screen.textinput(
-    title="Guess Another State", prompt="What's another name of a state in the US?"
-)
+data = pandas.read_csv("50_states.csv")
+# print(data["state"])
+
+df = pandas.DataFrame(data)
+print(df)
+
+# 1. Get a Guess and Convert to Title Case
+answer_state = (
+    screen.textinput(
+        title="Guess Another State", prompt="What's another name of a state in the US?"
+    )
+).title()
+
+# 2. Check if Guess is Among the 50 States
 
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
