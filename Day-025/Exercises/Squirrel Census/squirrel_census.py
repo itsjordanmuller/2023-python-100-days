0a1597053a4ad640f43edea7b210d56b4ba38f53
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 14:09:24 -0700

    Get a count of each primary fur color, and export that data to CSV

--- Day-025/Exercises/Squirrel Census/squirrel_census.py
+++ Day-025/Exercises/Squirrel Census/squirrel_census.py
@@ -1,4 +1,16 @@
 import pandas
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census.csv")
-print(data)
+# print(data)
+
+relevant_data = data.groupby("Primary Fur Color").size()
+
+df = pandas.DataFrame(
+    {"Primary Fur Color": relevant_data.index, "Count": relevant_data.values}
+)
+
+df = df.sort_values(by="Count", ascending=False)
+
+df = df.reset_index(drop=True)
+
+df.to_csv("squirrel_count.csv")
