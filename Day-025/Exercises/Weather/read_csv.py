2223cdbd3b8b7449c3cedb23c95a29849bb21cf1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 13:25:59 -0700

    Create a new virtual environment for weather exercises, import and use pandas to read the CSV

--- Day-025/Exercises/Weather/read_csv.py
+++ Day-025/Exercises/Weather/read_csv.py
@@ -2,12 +2,17 @@
 #     data = file.readlines()
 #     print(data)
 
-import csv
+# import csv
 
-with open("weather_data.csv") as file:
-    data = csv.reader(file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+# with open("weather_data.csv") as file:
+#     data = csv.reader(file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
