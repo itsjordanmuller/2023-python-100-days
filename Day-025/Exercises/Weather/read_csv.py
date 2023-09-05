1a563a57bca96356a72df83222155968af2067f0
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 13:30:42 -0700

    Convert the entire dataframe to a dictionary, then convert the temperature series to a list

--- Day-025/Exercises/Weather/read_csv.py
+++ Day-025/Exercises/Weather/read_csv.py
@@ -15,5 +15,11 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(type(data))
-print(type(data["temp"]))
+# print(type(data))
+# print(type(data["temp"]))
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
