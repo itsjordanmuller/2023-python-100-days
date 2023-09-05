4ae120956a84560f42d0b9c3f4098161bdeba9f3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 14:48:46 -0700

    Modify the code to create a loop so the user can keep inputting states

--- Day-025/Projects/US States Game/main.py
+++ Day-025/Projects/US States Game/main.py
@@ -1,5 +1,4 @@
 import turtle
-
 import pandas
 
 screen = turtle.Screen()
@@ -20,21 +19,26 @@ df = pandas.DataFrame(data)
 # print(df)
 
 # 1. Get a Guess and Convert to Title Case
-answer_state = (
-    screen.textinput(
-        title="Guess Another State", prompt="What's another name of a state in the US?"
-    )
-).title()
-
 # 2. Check if Guess is Among the 50 States
 # 3. Write the Correct Guesses onto the Map
-if answer_state in df["state"].values:
-    print(f"{answer_state} is a valid US state!")
-    state_row = df[df["state"] == answer_state].iloc[0]
-    state_turtle.goto(int(state_row["x"]), int(state_row["y"]))
-    state_turtle.write(answer_state, align="center", font=("Arial", 8, "normal"))
-else:
-    print(f"{answer_state} is not a valid US state!")
+# 4. Create a Loop to Allow the User to Keep Guessing
+game_is_on = True
+
+while game_is_on:
+    answer_state = (
+        screen.textinput(
+            title="Guess Another State",
+            prompt="What's another name of a state in the US?",
+        )
+    ).title()
+
+    if answer_state in df["state"].values:
+        print(f"{answer_state} is a valid US state!")
+        state_row = df[df["state"] == answer_state].iloc[0]
+        state_turtle.goto(int(state_row["x"]), int(state_row["y"]))
+        state_turtle.write(answer_state, align="center", font=("Arial", 8, "normal"))
+    else:
+        print(f"{answer_state} is not a valid US state!")
 
 
 # def get_mouse_click_coor(x, y):
