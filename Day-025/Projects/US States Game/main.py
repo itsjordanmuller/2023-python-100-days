a2c6eaa01108da774a2c22931478aa844a216afa
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-05 14:44:45 -0700

    Write a correct guess onto the map using x and y coordinates

--- Day-025/Projects/US States Game/main.py
+++ Day-025/Projects/US States Game/main.py
@@ -9,6 +9,10 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+state_turtle = turtle.Turtle()
+state_turtle.hideturtle()
+state_turtle.penup()
+
 data = pandas.read_csv("50_states.csv")
 # print(data["state"])
 
@@ -23,10 +27,15 @@ answer_state = (
 ).title()
 
 # 2. Check if Guess is Among the 50 States
+# 3. Write the Correct Guesses onto the Map
 if answer_state in df["state"].values:
     print(f"{answer_state} is a valid US state!")
+    state_row = df[df["state"] == answer_state].iloc[0]
+    state_turtle.goto(int(state_row["x"]), int(state_row["y"]))
+    state_turtle.write(answer_state, align="center", font=("Arial", 8, "normal"))
 else:
     print(f"{answer_state} is not a valid US state!")
+
 
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
