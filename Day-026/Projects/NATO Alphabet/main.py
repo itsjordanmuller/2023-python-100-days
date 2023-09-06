174f0d9df072f9a906503a0918b4fca793e2ebd1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 15:36:35 -0700

    Get a user input word string and convert it to a list of letters

--- Day-026/Projects/NATO Alphabet/main.py
+++ Day-026/Projects/NATO Alphabet/main.py
@@ -10,6 +10,11 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 df = pandas.DataFrame(data)
 # print(df)
 phonetic_dict = {row["letter"]: row["code"] for (index, row) in df.iterrows()}
-print(phonetic_dict)
+# print(phonetic_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+print("Welcome to the NATO Phonetic Conversion Tool")
+user_input = input("What word would you like to convert to the NATO Alphabet?\n")
+letter_list = [letter for letter in user_input]
+print(letter_list)
