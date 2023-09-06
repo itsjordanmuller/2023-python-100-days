70f667a10ab853262adca8ae436e0b6f0d987f89
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 15:08:33 -0700

    Import starting instructions and C to F temperature conversion function for weather exercise

---
+++ Day-026/Exercises/6-weather-dict.py
@@ -0,0 +1,20 @@
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+
+def CelsiusToFahrenheit(temp):
+    return (temp * 9 / 5) + 32
+
+
+# Write your code 👇 below:
+
+
+# print(weather_f)
