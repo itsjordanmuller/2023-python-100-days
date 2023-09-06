6eaa51bdab43e2b626b8926f05aab36d692fe7fd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 15:18:57 -0700

    Create a pandas DataFrame from the student dictionary

--- Day-026/Exercises/Pandas DataFrame Iterate/main.py
+++ Day-026/Exercises/Pandas DataFrame Iterate/main.py
@@ -2,6 +2,9 @@ import pandas
 
 student_dict = {"student": ["Jordan", "James", "Lily"], "score": [56, 76, 98]}
 
-for key, value in student_dict.items():
-    print(key)
-    print(value)
+# for key, value in student_dict.items():
+#     print(key)
+#     print(value)
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
