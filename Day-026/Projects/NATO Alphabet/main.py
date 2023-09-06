7ae506003d38f968bfb13306e0f2fe6f1cd0f402
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 15:34:20 -0700

    Create a phonetic dictionary by using dictionary comprehension to loop through a pandas DataFrame

--- Day-026/Projects/NATO Alphabet/main.py
+++ Day-026/Projects/NATO Alphabet/main.py
@@ -8,6 +8,8 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 df = pandas.DataFrame(data)
-print(df)
+# print(df)
+phonetic_dict = {row["letter"]: row["code"] for (index, row) in df.iterrows()}
+print(phonetic_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
