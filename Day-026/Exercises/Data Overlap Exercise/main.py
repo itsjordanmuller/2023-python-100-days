4e371cf68cd7ac995515f806bd72d1458bff5e31
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 14:50:45 -0700

    Use set, split, int, sorted, and list comprehension to get the list of matches

--- Day-026/Exercises/Data Overlap Exercise/main.py
+++ Day-026/Exercises/Data Overlap Exercise/main.py
@@ -1,11 +1,18 @@
 with open("file1.txt") as file:
     file_1_contents = file.read()
-    print(file_1_contents)
+    # print(file_1_contents)
 
 with open("file2.txt") as file:
     file_2_contents = file.read()
-    print(file_2_contents)
+    # print(file_2_contents)
+
+numbers_1 = set(file_1_contents.split())
+numbers_2 = set(file_2_contents.split())
+
+result = [int(num) for num in numbers_1 if num in numbers_2]
+
+result = sorted(result, reverse=False)
 
 # Write your code above 👆
 
-# print(result)
+print(result)
