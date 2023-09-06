12209301ad95e68560379a077fe45e981f80d825
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 15:01:05 -0700

    Create a list of only the students who passed with a score of 60 or more using dictionary comprehension

--- Day-026/Exercises/4-dict-comprehension.py
+++ Day-026/Exercises/4-dict-comprehension.py
@@ -10,3 +10,8 @@ def RandomScore():
 
 student_scores = {name: RandomScore() for name in names}
 print(student_scores)
+
+passed_scores = {
+    student: score for (student, score) in student_scores.items() if score >= 60
+}
+print(passed_scores)
