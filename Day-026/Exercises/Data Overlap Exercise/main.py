24d5e96e5fda961a040e4d5e42ed4be72d2987eb
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 14:45:18 -0700

    Open and read both of the text files to get and check their contents

--- Day-026/Exercises/Data Overlap Exercise/main.py
+++ Day-026/Exercises/Data Overlap Exercise/main.py
@@ -1,7 +1,11 @@
+with open("file1.txt") as file:
+    file_1_contents = file.read()
+    print(file_1_contents)
 
+with open("file2.txt") as file:
+    file_2_contents = file.read()
+    print(file_2_contents)
 
 # Write your code above 👆
 
-print(result)
-
-
+# print(result)
