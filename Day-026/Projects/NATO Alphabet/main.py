5d7134dee86782158e92feb6fe2b7c4580cfc429
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 15:31:25 -0700

    Create a new project folder with a virtual environment and pandas to read a CSV and create a DataFrame of the phonetic alphabet

---
+++ Day-026/Projects/NATO Alphabet/main.py
@@ -0,0 +1,13 @@
+import pandas
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+df = pandas.DataFrame(data)
+print(df)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
