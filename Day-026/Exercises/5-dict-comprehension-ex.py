008962b56d14ab4ecbb7d5d95d6c928e0d0393a2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-06 15:02:56 -0700

    Import the starting instructions, get a list of words by splitting the string

---
+++ Day-026/Exercises/5-dict-comprehension-ex.py
@@ -0,0 +1,9 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+
+word_list = sentence.split()
+print(word_list)
+
+# print(result)
