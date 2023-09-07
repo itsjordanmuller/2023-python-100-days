f314066f02fd49641b9e60dd628242db943d24dc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 13:48:40 -0700

    Show a couple ways to modify labels after they have been defined

--- Day-027/Exercises/1-windows-labels.py
+++ Day-027/Exercises/1-windows-labels.py
@@ -7,5 +7,7 @@ window.minsize(width=500, height=400)
 my_label = tkinter.Label(text="I am a label!", font=("Arial", 24, "bold"))
 my_label.pack(expand=True)
 
+my_label["text"] = "New Label Text!"
+my_label.config(text="Yet Another Label!")
 
 window.mainloop()
