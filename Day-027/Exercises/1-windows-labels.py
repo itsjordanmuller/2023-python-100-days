67b86aaf6619976cc652f0f561a0e0c5aa7411fa
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 13:09:44 -0700

    Set expand to true to make the label show in the middle of the window

--- Day-027/Exercises/1-windows-labels.py
+++ Day-027/Exercises/1-windows-labels.py
@@ -5,7 +5,7 @@ window.title("Python GUI Program")
 window.minsize(width=500, height=400)
 
 my_label = tkinter.Label(text="I am a label!", font=("Arial", 24, "bold"))
-my_label.pack(side="bottom")
+my_label.pack(expand=True)
 
 
 window.mainloop()
