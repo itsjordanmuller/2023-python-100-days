70893f0beca02f993cf92dd97598a49202c79745
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 13:07:07 -0700

    Create a tkinter label and center it in the window using pack method

--- Day-027/Exercises/1-windows-labels.py
+++ Day-027/Exercises/1-windows-labels.py
@@ -4,5 +4,8 @@ window = tkinter.Tk()
 window.title("Python GUI Program")
 window.minsize(width=500, height=400)
 
+my_label = tkinter.Label(text="I am a label!")
+my_label.pack()
+
 
 window.mainloop()
