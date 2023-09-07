7fadb677ecfff3e77e57cd1ad2d2c90d28923157
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 14:13:05 -0700

    Bring in the original window with label, button, and entry, and reorder to make it easier to work with pack and other layout managers

---
+++ Day-027/Exercises/5-layout-managers.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+
+i = 0
+
+
+def button_clicked():
+    global i
+    print("Button was clicked!")
+    i += 1
+    my_label["text"] = f"Button Clicked {i} Times!"
+
+
+def input_label():
+    input_value = input.get()
+    my_label["text"] = input_value
+
+
+# Window
+window = Tk()
+window.title("Python GUI Program")
+window.minsize(width=500, height=400)
+
+# Label
+my_label = Label(text="I am a label!", font=("Arial", 24, "bold"))
+my_label["text"] = "New Label Text!"
+my_label.pack()
+
+# Button
+button = Button(text="Button!", command=input_label)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
+
+
+window.mainloop()
