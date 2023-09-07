cd4765f57fa17febeec95ebb99d8a9fee41d524f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 13:33:09 -0700

    Use square brackets to print the values of a specified key in the dictionary of unlimited keyword arguments

--- Day-027/Exercises/3-many-keyword-args.py
+++ Day-027/Exercises/3-many-keyword-args.py
@@ -1,9 +1,10 @@
 def calculate(**kwargs):
     # print(kwargs)
     # print(type(kwargs))
-    for key, value in kwargs.items():
-        print(key)
-        print(value)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    print(kwargs["add"], kwargs["multiply"])
 
 
 calculate(add=3, multiply=5)
