1d8442567fe73358c85d4e5326bf99f02d1d1b74
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 13:35:46 -0700

    Add another argument before the unlimited keyword arguments to calculate based on those keywords, print the result of the calculations

--- Day-027/Exercises/3-many-keyword-args.py
+++ Day-027/Exercises/3-many-keyword-args.py
@@ -1,10 +1,13 @@
-def calculate(**kwargs):
-    # print(kwargs)
+def calculate(n, **kwargs):
+    print(kwargs)
     # print(type(kwargs))
     # for key, value in kwargs.items():
     #     print(key)
     #     print(value)
-    print(kwargs["add"], kwargs["multiply"])
+    # print(kwargs["add"], kwargs["multiply"])
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
 
 
-calculate(add=3, multiply=5)
+calculate(2, add=3, multiply=5)
