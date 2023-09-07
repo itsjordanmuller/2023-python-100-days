30c4a054c465876eb86ee5a8a64ebc56974a4998
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 14:18:48 -0700

    Use grid to place the widgets on a defined grid inside of the window

--- Day-027/Exercises/5-layout-managers.py
+++ Day-027/Exercises/5-layout-managers.py
@@ -25,18 +25,21 @@ window.minsize(width=500, height=400)
 my_label = Label(text="I am a label!", font=("Arial", 24, "bold"))
 my_label["text"] = "New Label Text!"
 # my_label.pack(side="left")
-my_label.place(
-    x=10,
-    y=10,
-)
+# my_label.place(
+#     x=10,
+#     y=10,
+# )
+my_label.grid(column=0, row=0)
 
 # Button
 button = Button(text="Button!", command=input_label)
 # button.pack(side="left")
+button.grid(column=1, row=1)
 
 # Entry
 input = Entry(width=10)
 # input.pack(side="left")
+input.grid(column=2, row=2)
 
 
 window.mainloop()
