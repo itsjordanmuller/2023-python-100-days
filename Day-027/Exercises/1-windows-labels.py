2bd8b0805ce46047dbf3f06a1521540f9c98e5f6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 13:07:56 -0700

    Customize the label font to make it use Arial, a larger size, and bold text

--- Day-027/Exercises/1-windows-labels.py
+++ Day-027/Exercises/1-windows-labels.py
@@ -4,7 +4,7 @@ window = tkinter.Tk()
 window.title("Python GUI Program")
 window.minsize(width=500, height=400)
 
-my_label = tkinter.Label(text="I am a label!")
+my_label = tkinter.Label(text="I am a label!", font=("Arial", 24, "bold"))
 my_label.pack()
 
 
