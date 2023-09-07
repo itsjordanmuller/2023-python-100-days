1dc493b3529b1fb82992f74a83c755d813c0e448
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-07 13:32:02 -0700

    Use a loop and the items method to print the keys and values of the kwargs dictionary

--- Day-027/Exercises/3-many-keyword-args.py
+++ Day-027/Exercises/3-many-keyword-args.py
@@ -1,6 +1,9 @@
 def calculate(**kwargs):
-    print(kwargs)
-    print(type(kwargs))
+    # print(kwargs)
+    # print(type(kwargs))
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
 
 
 calculate(add=3, multiply=5)
