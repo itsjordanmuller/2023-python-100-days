4b3ca74431a1bdcdb0f40399fad28dc11bf7196e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-08 15:01:55 -0700

    Simplify the canvas config, and add some text to the center of the tomato

--- Day-028/Projects/Pomodoro Timer/main.py
+++ Day-028/Projects/Pomodoro Timer/main.py
@@ -21,10 +21,10 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg="#ffebcd")
 
-canvas = Canvas(width=202, height=224)
-canvas.config(bg="#ffebcd", highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg="#ffebcd", highlightthickness=0)
 img = PhotoImage(file="tomato.png")
-canvas.create_image(101, 112, image=img)
+canvas.create_image(100, 112, image=img)
+canvas.create_text(100, 140, text="00:00", font=(FONT_NAME, 26, "bold"), fill="white")
 canvas.pack(expand=True)
 
 window.mainloop()
