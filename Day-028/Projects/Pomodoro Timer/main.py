b0c4ba318f62609fc61eba7857405f43685ac010
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-08 15:10:20 -0700

    Add the rest of the widgets to the UI, space everything out using grid, modify color constants

--- Day-028/Projects/Pomodoro Timer/main.py
+++ Day-028/Projects/Pomodoro Timer/main.py
@@ -3,8 +3,8 @@ from tkinter import *
 # CONSTANTS
 PINK = "#e2979c"
 RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+GREEN = "#097969"
+TAN = "#ffebcd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -19,12 +19,24 @@ LONG_BREAK_MIN = 20
 # UI SETUP
 window = Tk()
 window.title("Pomodoro Timer")
-window.config(padx=100, pady=50, bg="#ffebcd")
+window.config(padx=100, pady=50, bg=TAN)
 
-canvas = Canvas(width=200, height=224, bg="#ffebcd", highlightthickness=0)
+timer_label = Label(text="Timer")
+timer_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=TAN, highlightthickness=0)
 img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=img)
 canvas.create_text(100, 140, text="00:00", font=(FONT_NAME, 26, "bold"), fill="white")
-canvas.pack(expand=True)
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Start")
+reset_button.grid(column=2, row=2)
+
+check_label = Label(text="✓")
+check_label.grid(column=1, row=3)
 
 window.mainloop()
