bac7822b40f572361cd9f268f34e0e72b77db7af
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-08 15:18:51 -0700

    Make adjustments to spacing, color, and style to match the example layout, set reset button to have text of reset

--- Day-028/Projects/Pomodoro Timer/main.py
+++ Day-028/Projects/Pomodoro Timer/main.py
@@ -3,7 +3,7 @@ from tkinter import *
 # CONSTANTS
 PINK = "#e2979c"
 RED = "#e7305b"
-GREEN = "#097969"
+GREEN = "#379b46"
 TAN = "#ffebcd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
@@ -21,22 +21,22 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=TAN)
 
-timer_label = Label(text="Timer")
+timer_label = Label(text="Timer", bg=TAN, fg=GREEN, font=(FONT_NAME, 40, "bold"))
 timer_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=TAN, highlightthickness=0)
 img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=img)
 canvas.create_text(100, 140, text="00:00", font=(FONT_NAME, 26, "bold"), fill="white")
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=1, padx=20, pady=10)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", bg="white")
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Start")
+reset_button = Button(text="Reset", bg="white")
 reset_button.grid(column=2, row=2)
 
-check_label = Label(text="✓")
+check_label = Label(text="✓", bg=TAN, fg=GREEN, font=(FONT_NAME, 18, "bold"))
 check_label.grid(column=1, row=3)
 
 window.mainloop()
