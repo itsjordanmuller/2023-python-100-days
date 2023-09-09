3073b2705e793b1989cbbbe70d1d632ca5dc5a16
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-09 16:55:11 -0700

    Create the layout of all the labels, entries, and buttons to match the example layout provided

--- Day-029/Projects/Password Manager/main.py
+++ Day-029/Projects/Password Manager/main.py
@@ -12,7 +12,31 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=img)
-canvas.pack(expand=True, padx=20, pady=20)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1, sticky="E")
+
+website_entry = Entry()
+website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+
+user_email_label = Label(text="Email/Username:")
+user_email_label.grid(column=0, row=2, sticky="E")
+
+user_email_entry = Entry()
+user_email_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3, sticky="E")
+
+password_entry = Entry()
+password_entry.grid(column=1, row=3, sticky="EW")
+
+gen_password_button = Button(text="Generate Password", pady=0)
+gen_password_button.grid(column=2, row=3, sticky="EW")
+
+add_password_button = Button(text="Add", width=32)
+add_password_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 
 window.mainloop()
