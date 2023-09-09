0c4bdd34c46238e0a5dd598651202ebff861cbe1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-09 16:24:45 -0700

    Create the initial window with the logo image on the canvas to start the program

--- Day-029/Projects/Password Manager/main.py
+++ Day-029/Projects/Password Manager/main.py
@@ -1,5 +1,18 @@
+from tkinter import *
+
 # PASSWORD GENERATOR
 
 # SAVE PASSWORD
 
 # UI SETUP
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+img = PhotoImage(file="logo.png")
+canvas.create_image(100, 95, image=img)
+canvas.pack(expand=True, padx=20, pady=20)
+
+
+window.mainloop()
