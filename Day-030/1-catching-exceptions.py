2340cba91fde8b87127bfde07edf1fc7b4228683
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-10 13:43:33 -0700

    Raise an exception when a value is out of range

--- Day-030/1-catching-exceptions.py
+++ Day-030/1-catching-exceptions.py
@@ -19,19 +19,28 @@
 # Else: Do this if there were no exceptions
 # Finally: Do this no matter what happens
 
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    # print(a_dictionary["non-existent-key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed")
-    raise TypeError("This is an error that I made up")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     # print(a_dictionary["non-existent-key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed")
+#     raise TypeError("This is an error that I made up")
+
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Height should not be over 3 meters")
+
+bmi = weight / height**2
+print(bmi)
