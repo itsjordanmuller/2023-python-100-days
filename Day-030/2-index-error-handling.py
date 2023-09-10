f271754e669b69f3192147be6967d318f21857a5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-10 13:52:04 -0700

    Catch the exceptions caused by index errors

--- Day-030/2-index-error-handling.py
+++ Day-030/2-index-error-handling.py
@@ -3,8 +3,16 @@ fruits = ["Apple", "Pear", "Orange"]
 
 # TODO: Catch the exception and make sure the code runs without crashing.
 def make_pie(index):
-    fruit = fruits[index]
-    print(fruit + " pie")
+    try:
+        fruit = fruits[index]
+        print(fruit + " pie")
+        return fruit + " pie"
+    except IndexError:
+        print("Fruit pie")
+        return "Fruit pie"
 
 
-make_pie(4)
+try:
+    make_pie(4)
+except IndexError:
+    print("Fruit pie")
