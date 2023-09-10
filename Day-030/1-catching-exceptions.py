ce3d9fe455f1c4c181d52f0175d4d03aaad2ef22
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-10 13:34:53 -0700

    Handle another type of error, and pass specific information about the error into a print statement

--- Day-030/1-catching-exceptions.py
+++ Day-030/1-catching-exceptions.py
@@ -26,3 +26,5 @@ try:
 except FileNotFoundError:
     file = open("a_file.txt", "w")
     file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
