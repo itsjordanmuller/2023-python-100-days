4b21b72e0a7aa66740660c0eb59d3cf8423e4870
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-14 13:25:11 -0700

    Use the data from the API instead of using hardcoded questions, unescape the HTML symbols using html module

--- Day-034/Projects/Quizzler/data.py
+++ Day-034/Projects/Quizzler/data.py
@@ -6,7 +6,7 @@ response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 
-print(data)
+question_data = data["results"]
 
 # question_data = [
 #     {
