d00813e5325fe684b5baf2091e273604059fe7d9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-14 14:26:32 -0700

    Update the score as the user goes through, give a message at the end

--- Day-034/Projects/Quizzler/ui.py
+++ Day-034/Projects/Quizzler/ui.py
@@ -55,8 +55,14 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(
+                self.question_text, text="You've reached the end of the quiz!"
+            )
 
     def true_pressed(self):
         is_right = self.quiz.check_answer("True")
