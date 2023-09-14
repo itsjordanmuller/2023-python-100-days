bf659951f157fba2444c5c981470a92d0b5614f3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-14 14:34:03 -0700

    Update the score text after each question, show a fraction at the end

--- Day-034/Projects/Quizzler/ui.py
+++ Day-034/Projects/Quizzler/ui.py
@@ -55,14 +55,17 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
+        self.score_label.config(text=f"Score: {self.quiz.score}")
         if self.quiz.still_has_questions():
-            self.score_label.config(text=f"Score: {self.quiz.score}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
         else:
             self.canvas.itemconfig(
-                self.question_text, text="You've reached the end of the quiz!"
+                self.question_text,
+                text=f"You've reached the end of the quiz!\n\nYou got: {self.quiz.score}/{self.quiz.question_number} correct.",
             )
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_pressed(self):
         is_right = self.quiz.check_answer("True")
