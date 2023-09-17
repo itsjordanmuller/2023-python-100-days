9c5b3da8347f6f6777e5092f225c16855c175b1c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-16 17:56:57 -0700

    Use subsample to resize images to fit canvas size

--- Day-036/Projects/Tic-Tac-Toe/ui.py
+++ Day-036/Projects/Tic-Tac-Toe/ui.py
@@ -9,8 +9,8 @@ class UserInterface:
         self.canvas_size = 128
         self.canvases = []
 
-        self.x_img = PhotoImage(file="./images/x.png")
-        self.o_img = PhotoImage(file="./images/o.png")
+        self.x_img = PhotoImage(file="./images/x.png").subsample(5, 5)
+        self.o_img = PhotoImage(file="./images/o.png").subsample(5, 5)
 
         for i in range(3):
             for j in range(3):
@@ -21,6 +21,26 @@ class UserInterface:
                     bg="white",
                 )
                 canvas.grid(row=i, column=j)
+                canvas.bind("<Button-1>", self.place_x)
+                canvas.bind("<Button-3>", self.place_o)
                 self.canvases.append(canvas)
 
         self.window.mainloop()
+
+    def place_x(self, event):
+        canvas = event.widget
+        canvas_width = canvas.winfo_width()
+        canvas_height = canvas.winfo_height()
+
+        canvas.delete("all")
+
+        canvas.create_image(canvas_width // 2, canvas_height // 2, image=self.x_img)
+
+    def place_o(self, event):
+        canvas = event.widget
+        canvas_width = canvas.winfo_width()
+        canvas_height = canvas.winfo_height()
+
+        canvas.delete("all")
+
+        canvas.create_image(canvas_width // 2, canvas_height // 2, image=self.o_img)
