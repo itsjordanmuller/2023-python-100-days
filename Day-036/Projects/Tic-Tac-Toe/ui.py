1fb46cd81de4dcedf9799d2fe15c00195d81e7e6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-16 18:18:59 -0700

    Add a turn system with color and to force the order of play

--- Day-036/Projects/Tic-Tac-Toe/ui.py
+++ Day-036/Projects/Tic-Tac-Toe/ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 
 class UserInterface:
@@ -10,6 +11,12 @@ class UserInterface:
         self.canvases = []
 
         self.canvas_states = {}
+
+        self.turn_label = Label(text="X Goes", bg="red", font=("TkFixedFont", 24))
+        self.turn_label.grid(row=3, column=0, columnspan=3)
+
+        self.current_turn = random.choice(["X", "O"])
+        self.update_turn()
 
         self.x_img = PhotoImage(file="./images/x.png").subsample(5, 5)
         self.o_img = PhotoImage(file="./images/o.png").subsample(5, 5)
@@ -34,25 +41,35 @@ class UserInterface:
     def place_x(self, event):
         canvas = event.widget
 
-        if self.canvas_states[canvas] == "Empty":
+        if self.current_turn == "X" and self.canvas_states[canvas] == "Empty":
             canvas_width = canvas.winfo_width()
             canvas_height = canvas.winfo_height()
 
             canvas.create_image(canvas_width // 2, canvas_height // 2, image=self.x_img)
-
             canvas.configure(bg="red")
 
             self.canvas_states[canvas] = "X"
+            self.current_turn = "O"
+            self.update_turn()
 
     def place_o(self, event):
         canvas = event.widget
 
-        if self.canvas_states[canvas] == "Empty":
+        if self.current_turn == "O" and self.canvas_states[canvas] == "Empty":
             canvas_width = canvas.winfo_width()
             canvas_height = canvas.winfo_height()
 
             canvas.create_image(canvas_width // 2, canvas_height // 2, image=self.o_img)
-
             canvas.configure(bg="blue")
 
             self.canvas_states[canvas] = "O"
+            self.current_turn = "X"
+            self.update_turn()
+
+    def update_turn(self):
+        if self.current_turn == "X":
+            self.turn_label.config(text="X Goes", bg="red")
+            self.window.config(bg="red")
+        else:
+            self.turn_label.config(text="O Goes", bg="blue")
+            self.window.config(bg="blue")
