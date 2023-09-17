794516221c9d420b376009db8db7379cd66b08b1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-17 15:43:46 -0700

    MStore username in a constant, do the same for token key, create a graph endpoint
    Storing these as constants/variables makes them easier to access

--- Day-037/Projects/Habit Tracker App/main.py
+++ Day-037/Projects/Habit Tracker App/main.py
@@ -6,17 +6,20 @@ load_dotenv()
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
-token_key = os.getenv("PIXELA_TOKEN_KEY")
+USERNAME = "itsjordanmuller"
+TOKEN_KEY = os.getenv("PIXELA_TOKEN_KEY")
 
 user_params = {
-    "token": token_key,
-    "username": "itsjordanmuller",
+    "token": TOKEN_KEY,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 # API Key Storage Test
 # api_key = os.getenv("MY_TEST_KEY")
