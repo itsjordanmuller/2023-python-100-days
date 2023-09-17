13045c9505142143ca6a23d515c40b3668abc1a0
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-17 15:33:28 -0700

    Create a secret token key for creating a user on Pixela API and store in env

--- Day-037/Projects/Habit Tracker App/main.py
+++ Day-037/Projects/Habit Tracker App/main.py
@@ -1,7 +1,15 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv("MY_TEST_KEY")
 
-print(api_key)
+pixela_endpoint = "https://pixe.la/v1/users"
+
+token_key = os.getenv("PIXELA_TOKEN_KEY")
+
+# API Key Storage Test
+# api_key = os.getenv("MY_TEST_KEY")
+# print(api_key)
+
+# requests.post()
