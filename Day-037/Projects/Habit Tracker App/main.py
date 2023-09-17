520c4c17e1a39f6dcd4a20f81f71963ddd0d1174
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-17 15:37:22 -0700

    Create  a dictionary full of user parameters for use in post request

--- Day-037/Projects/Habit Tracker App/main.py
+++ Day-037/Projects/Habit Tracker App/main.py
@@ -8,8 +8,15 @@ pixela_endpoint = "https://pixe.la/v1/users"
 
 token_key = os.getenv("PIXELA_TOKEN_KEY")
 
+user_params = {
+    "token": token_key,
+    "username": "itsjordanmuller",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# requests.post()
+
 # API Key Storage Test
 # api_key = os.getenv("MY_TEST_KEY")
 # print(api_key)
-
-# requests.post()
