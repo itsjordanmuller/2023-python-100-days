da98ced8ea6fa8a951b0eea08a49100a1fd6ae9b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-17 15:58:41 -0700

    Add aaan example pixel to the graph by setting the date and quantity, sending post request to new endpoint

--- Day-037/Projects/Habit Tracker App/main.py
+++ Day-037/Projects/Habit Tracker App/main.py
@@ -8,6 +8,7 @@ pixela_endpoint = "https://pixe.la/v1/users"
 
 USERNAME = "itsjordanmuller"
 TOKEN_KEY = os.getenv("PIXELA_TOKEN_KEY")
+TEST_GRAPH_ID = "testhabittracker"
 
 user_params = {
     "token": TOKEN_KEY,
@@ -21,7 +22,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "testhabittracker",
+    "id": TEST_GRAPH_ID,
     "name": "Test Habit Tracker",
     "unit": "successes",
     "type": "int",
@@ -30,7 +31,16 @@ graph_config = {
 
 headers = {"X-USER-TOKEN": TOKEN_KEY}
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+test_graph_endpoint = f"{graph_endpoint}/{TEST_GRAPH_ID}"
+
+pixel_config = {
+    "date": "20230917",
+    "quantity": "1",
+}
+
+response = requests.post(url=test_graph_endpoint, json=pixel_config, headers=headers)
 print(response.text)
 
 # API Key Storage Test
