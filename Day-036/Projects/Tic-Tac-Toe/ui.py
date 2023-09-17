2d121af0ef10ec90743138bef009f29bf8ad4f2e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-16 17:47:01 -0700

    Use nested for loops to create a 3x3 grid of canvases for the ui

--- Day-036/Projects/Tic-Tac-Toe/ui.py
+++ Day-036/Projects/Tic-Tac-Toe/ui.py
@@ -6,4 +6,18 @@ class UserInterface:
         self.window = Tk()
         self.window.title("Tic-Tac-Toe")
 
+        self.canvas_size = 128
+        self.canvases = []
+
+        for i in range(3):
+            for j in range(3):
+                canvas = Canvas(
+                    self.window,
+                    width=self.canvas_size,
+                    height=self.canvas_size,
+                    bg="white",
+                )
+                canvas.grid(row=i, column=j)
+                self.canvases.append(canvas)
+
         self.window.mainloop()
