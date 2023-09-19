38dd886e2457db0fcfc1b743502d1c644a60ffc4
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-19 13:08:10 -0700

    Import images with credit, create PhotoImages from each for easy access later

--- Day-039/Projects/Rock Paper Scissors App/ui.py
+++ Day-039/Projects/Rock Paper Scissors App/ui.py
@@ -6,4 +6,16 @@ class UserInterface:
         self.window = Tk()
         self.window.title("Rock Paper Scissors")
 
+        # Paper icons created by Freepik - Flaticon
+        # https://www.flaticon.com/free-icons/paper
+        self.paper_img = PhotoImage(file="./images/scroll.png")
+
+        # Rock icons created by Freepik - Flaticon
+        # https://www.flaticon.com/free-icons/rock
+        self.rock_img = PhotoImage(file="./images/stone.png")
+
+        # Scissors icons created by Freepik - Flaticon
+        # https://www.flaticon.com/free-icons/scissors
+        self.scissors_img = PhotoImage(file="./images/scissors.png")
+
         self.window.mainloop()
