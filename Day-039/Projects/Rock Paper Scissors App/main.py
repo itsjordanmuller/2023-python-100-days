bb6e77aed1b388ead900e38e19ef86b8142a3c0b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-19 13:07:21 -0700

    Create a project folder with images for rock paper scissors app, setup basic UI

---
+++ Day-039/Projects/Rock Paper Scissors App/main.py
@@ -0,0 +1,3 @@
+from ui import UserInterface
+
+game_ui = UserInterface()
---
+++ Day-039/Projects/Rock Paper Scissors App/ui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+
+class UserInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Rock Paper Scissors")
+
+        self.window.mainloop()
