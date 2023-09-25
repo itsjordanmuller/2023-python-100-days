b26c9171fef4325d7124ad69e3d0683c6460dd14
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-25 13:15:00 -0700

    Find a specific heading by targeting element name and ID

--- Day-045/Exercises/Parsing HTML and Making Soup/main.py
+++ Day-045/Exercises/Parsing HTML and Making Soup/main.py
@@ -19,3 +19,6 @@ all_anchor_tags = soup.find_all(name="a")
 for tag in all_anchor_tags:
     # print(tag.getText())
     print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
