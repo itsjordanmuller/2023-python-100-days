4d209c1a22cb9b3f69d2abb61e79fd39ce8b42fc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-25 13:47:55 -0700

    Scrape a live website to create two lists containing text and links from a specific element on a site

--- Day-045/Exercises/Scraping a Live Site/main.py
+++ Day-045/Exercises/Scraping a Live Site/main.py
@@ -3,4 +3,20 @@ import requests
 
 response = requests.get("https://news.ycombinator.com/news")
 
-print(response.text)
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+all_anchor_tags = soup.select(selector=".titleline > a")
+
+article_texts = []
+article_links = []
+
+for tag in all_anchor_tags:
+    article_text = tag.getText()
+    article_texts.append(article_text)
+    article_link = tag.get("href")
+    article_links.append(article_link)
+
+print(article_texts)
+print(article_links)
