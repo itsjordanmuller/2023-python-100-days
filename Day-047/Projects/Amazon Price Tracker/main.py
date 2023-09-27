d55297a9e703101320ea46f42faa9d794ce5320a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-27 11:34:46 -0700

    Create the basic CLI menu for getting user inputs and choices

---
+++ Day-047/Projects/Amazon Price Tracker/main.py
@@ -0,0 +1,13 @@
+instructions = "Welcome to the Amazon Price Tracker\n-----------------------------------\nHere are your options:\n1. Check prices of existing items\n2. Add a new item to track\n3. Remove or modify an item\n4. Exit the program\n-----------------------------------"
+
+print(instructions)
+selection = int(input("To choose, enter a number (1-4): "))
+
+# if(selection == 1):
+#     check_prices()
+# elif(selection == 2):
+#     add_item()
+# elif(selection == 3):
+#     modify_item()
+# elif(selection == 4):
+#     exit_program()
