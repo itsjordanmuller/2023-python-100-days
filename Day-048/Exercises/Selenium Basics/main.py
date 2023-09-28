0f34ac62a3fec9df307171ac76c2c1cfd334a92e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-28 16:52:56 -0700

    Switchh over to pypi packages, to get around Google Store page structure problems

--- Day-048/Exercises/Selenium Basics/main.py
+++ Day-048/Exercises/Selenium Basics/main.py
@@ -5,7 +5,11 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://store.google.com/us/product/pixel_buds_pro?hl=en-US")
+driver.get("https://pypi.org/project/selenium/")
+
+pip_install_cmd = driver.find_element(By.CSS_SELECTOR, "span#pip-command")
+
+print(pip_install_cmd, type(pip_install_cmd))
 
 # Close a Tab
 # driver.close()
