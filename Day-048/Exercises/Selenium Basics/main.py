a8b27fe84920026c1a0faf3ecd4f0f955ab542dd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-28 16:58:19 -0700

    Print the text of the pip install command and package description

--- Day-048/Exercises/Selenium Basics/main.py
+++ Day-048/Exercises/Selenium Basics/main.py
@@ -8,8 +8,10 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://pypi.org/project/selenium/")
 
 pip_install_cmd = driver.find_element(By.CSS_SELECTOR, "span#pip-command")
+package_desc = driver.find_element(By.CSS_SELECTOR, "section#introduction p")
 
-print(pip_install_cmd, type(pip_install_cmd))
+print(pip_install_cmd.text)
+print(package_desc.text)
 
 # Close a Tab
 # driver.close()
