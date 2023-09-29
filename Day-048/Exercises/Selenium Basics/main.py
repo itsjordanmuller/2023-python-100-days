b6033c5e0ca4b9d21856b1f374d7dc01ea38edc7
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-28 17:13:02 -0700

    Get an input from the user to check a PyPI package, print out info about that package

--- Day-048/Exercises/Selenium Basics/main.py
+++ Day-048/Exercises/Selenium Basics/main.py
@@ -4,15 +4,18 @@ from selenium.webdriver.common.by import By
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://pypi.org/project/selenium/")
+pkg = input("Enter a project name from PyPI.org:\n")
+print(pkg)
 
-pip_install_cmd = driver.find_element(By.CSS_SELECTOR, "span#pip-command")
-package_desc = driver.find_element(By.CSS_SELECTOR, "section#introduction p")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://pypi.org/project/" + pkg + "/")
+
+pip_install_cmd = driver.find_element(By.CSS_SELECTOR, "#pip-command")
+package_name = driver.find_element(By.CSS_SELECTOR, ".package-header__name")
 release_date = driver.find_element(By.CSS_SELECTOR, ".package-header__date")
 
+print(f"NAME\t{package_name.text}")
 print(f"CMD\t{pip_install_cmd.text}")
-print(f"DESC\t{package_desc.text}")
 print(f"DATE\t{release_date.text}")
 
 # Close a Tab
