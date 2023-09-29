68a9dfe0e78750dc041ed043e8c66fd50ab14860
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-28 18:05:19 -0700

    Create a way to time clicks, and stop after a goal is hit

--- Day-048/Projects/Cookie Clicker/main.py
+++ Day-048/Projects/Cookie Clicker/main.py
@@ -1,6 +1,6 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -8,13 +8,22 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
-money = driver.find_element(By.CSS_SELECTOR, "div#money").text
-cookie = driver.find_element(By.CSS_SELECTOR, "div#cookie")
+cookie = driver.find_element(By.ID, "cookie")
+money = 0
+goal = 100
 
-print(f"MONEY\t{money}")
+milliseconds_between_clicks = 10
 
-# Close a Tab
-# driver.close()
+while True:
+    cookie.click()
 
-# Close Entire Instance
-# driver.quit()
+    time.sleep(milliseconds_between_clicks / 1000)
+
+    money_element = driver.find_element(By.CSS_SELECTOR, "div#money")
+    money = float(money_element.text.replace(",", "").split()[0])
+
+    if money >= (goal - 1):
+        print("Money reached 100 or more. Stopping the clicks.")
+        break
+
+print(f"Final Money: {money}")
