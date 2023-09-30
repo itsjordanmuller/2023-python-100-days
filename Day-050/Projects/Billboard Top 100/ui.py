6f49e2233adba7880bb11b30384f28033fa0d8d8
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-30 16:03:14 -0700

    Create a calendar date picker on the left half of the program

--- Day-050/Projects/Billboard Top 100/ui.py
+++ Day-050/Projects/Billboard Top 100/ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkcalendar import Calendar
 import json
 import random
 
@@ -12,7 +13,7 @@ class UserInterface:
         self.header_canvas = Canvas(
             width=700, height=100, bg="#E20A17", highlightthickness=0
         )
-        self.header_canvas.grid(column=0, row=0)
+        self.header_canvas.grid(column=0, row=0, columnspan=2)
 
         self.logo = PhotoImage(file="./images/BillboardHot100Logo.png").subsample(
             x=4, y=4
@@ -22,9 +23,28 @@ class UserInterface:
             420, 50, text="Billboard Hot 100", font=("TkFixedFont", 40, "bold")
         )
 
-        self.control_canvas = Canvas(
-            width=700, height=400, bg="#0082D6", highlightthickness=0
+        self.date_canvas = Canvas(
+            width=350, height=400, bg="#FFFFFF", highlightthickness=0
         )
-        self.control_canvas.grid(column=0, row=1)
+        self.date_canvas.grid(column=1, row=1)
+
+        self.calendar_canvas = Canvas(
+            width=350, height=400, bg="#0082D6", highlightthickness=0
+        )
+        self.calendar_canvas.grid(column=0, row=1)
+
+        self.calendar = Calendar(
+            self.window,
+            selectmode="day",
+            year=2023,
+            month=9,
+            day=30,
+            width=350,
+            height=400,
+        )
+
+        self.calendar_window = self.calendar_canvas.create_window(
+            50, 50, anchor=NW, window=self.calendar
+        )
 
         self.window.mainloop()
