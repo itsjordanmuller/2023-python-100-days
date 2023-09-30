35e1ab2e7821643362c659bab34ced2e0c0181d1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-30 15:41:22 -0700

    Begin to layout the control canvas area, set colors on canvases to match logo

--- Day-050/Projects/Billboard Top 100/ui.py
+++ Day-050/Projects/Billboard Top 100/ui.py
@@ -9,7 +9,9 @@ class UserInterface:
         self.window.title("Billboard Top 100 Playlist Creator")
         self.window.config(width=700, height=500)
 
-        self.header_canvas = Canvas(width=700, height=100)
+        self.header_canvas = Canvas(
+            width=700, height=100, bg="#E20A17", highlightthickness=0
+        )
         self.header_canvas.grid(column=0, row=0)
 
         self.logo = PhotoImage(file="./images/BillboardHot100Logo.png").subsample(
@@ -17,7 +19,12 @@ class UserInterface:
         )
         self.header_canvas.create_image(10, 50, anchor=W, image=self.logo)
         self.header_canvas.create_text(
-            420, 50, text="Billboard Hot 100", font=("TkFixedFont", 32, "bold")
+            420, 50, text="Billboard Hot 100", font=("TkFixedFont", 40, "bold")
         )
+
+        self.control_canvas = Canvas(
+            width=700, height=400, bg="#0082D6", highlightthickness=0
+        )
+        self.control_canvas.grid(column=0, row=1)
 
         self.window.mainloop()
