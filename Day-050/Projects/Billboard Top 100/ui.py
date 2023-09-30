770c8e0450fb07e3165d217b033a936b25bf22da
itsjordanmuller <itsjordanmuller@gmail.com>
2023-09-30 15:36:18 -0700

    Add a logo and title text to the header of the GUI app

--- Day-050/Projects/Billboard Top 100/ui.py
+++ Day-050/Projects/Billboard Top 100/ui.py
@@ -9,4 +9,15 @@ class UserInterface:
         self.window.title("Billboard Top 100 Playlist Creator")
         self.window.config(width=700, height=500)
 
+        self.header_canvas = Canvas(width=700, height=100)
+        self.header_canvas.grid(column=0, row=0)
+
+        self.logo = PhotoImage(file="./images/BillboardHot100Logo.png").subsample(
+            x=4, y=4
+        )
+        self.header_canvas.create_image(10, 50, anchor=W, image=self.logo)
+        self.header_canvas.create_text(
+            420, 50, text="Billboard Hot 100", font=("TkFixedFont", 32, "bold")
+        )
+
         self.window.mainloop()
