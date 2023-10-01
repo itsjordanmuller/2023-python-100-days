90e1dc180013f130a6460da9ce2c51325a4df018
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-01 15:15:22 -0700

    Begin to define some basic UI functions, and set labels based on these button actions

--- Day-051/Projects/Billboard Hot 100/gui.py
+++ Day-051/Projects/Billboard Hot 100/gui.py
@@ -27,22 +27,35 @@ class GUI:
         self.day_entry = Entry()
         self.day_entry.grid(column=1, row=3)
 
-        self.date_select_button = Button(text="Search Date")
+        self.date_select_button = Button(text="Search Date", command=self.search_date)
         self.date_select_button.grid(column=0, row=4, columnspan=2)
 
         self.date_select_status = Label(text="")
         self.date_select_status.grid(column=0, row=5, columnspan=2)
 
-        self.search_songs_button = Button(text="Search Songs")
+        self.search_songs_button = Button(
+            text="Search Songs", command=self.search_songs
+        )
         self.search_songs_button.grid(column=0, row=6, columnspan=2)
 
         self.search_songs_status = Label(text="")
         self.search_songs_status.grid(column=0, row=7, columnspan=2)
 
-        self.create_playlist_button = Button(text="Create Playlist")
+        self.create_playlist_button = Button(
+            text="Create Playlist", command=self.create_playlist
+        )
         self.create_playlist_button.grid(column=0, row=8, columnspan=2)
 
         self.create_playlist_status = Label(text="")
         self.create_playlist_status.grid(column=0, row=9, columnspan=2)
 
         self.window.mainloop()
+
+    def search_date(self):
+        self.date_select_status.config(text="Date Set", fg="green")
+
+    def search_songs(self):
+        self.search_songs_status.config(text="Searching...", fg="orange")
+
+    def create_playlist(self):
+        self.create_playlist_status.config(text="Playlist Created", fg="green")
