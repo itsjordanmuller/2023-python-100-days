2b38db1e04e08c990ee38d394e57bce0c421f4de
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-01 15:10:36 -0700

    Create buttons and labels for the status of those buttons

--- Day-051/Projects/Billboard Hot 100/gui.py
+++ Day-051/Projects/Billboard Hot 100/gui.py
@@ -27,4 +27,22 @@ class GUI:
         self.day_entry = Entry()
         self.day_entry.grid(column=1, row=3)
 
+        self.date_select_button = Button(text="Search Date")
+        self.date_select_button.grid(column=0, row=4, columnspan=2)
+
+        self.date_select_status = Label(text="")
+        self.date_select_status.grid(column=0, row=5, columnspan=2)
+
+        self.search_songs_button = Button(text="Search Songs")
+        self.search_songs_button.grid(column=0, row=6, columnspan=2)
+
+        self.search_songs_status = Label(text="")
+        self.search_songs_status.grid(column=0, row=7, columnspan=2)
+
+        self.create_playlist_button = Button(text="Create Playlist")
+        self.create_playlist_button.grid(column=0, row=8, columnspan=2)
+
+        self.create_playlist_status = Label(text="")
+        self.create_playlist_status.grid(column=0, row=9, columnspan=2)
+
         self.window.mainloop()
