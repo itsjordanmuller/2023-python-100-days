4f640cf0f0a3c862ab844086ee59faf9553c0183
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 11:31:42 -0700

    Show how functions can be passed as arguments, which allows for using functions in functions

--- Day-054/Exercises/Python Decorators/main.py
+++ Day-054/Exercises/Python Decorators/main.py
@@ -13,3 +13,12 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+
+##Functions are first-class objects, can be passed around as arguments e.g. int/string/float etc.
+def calculate(calc_function, n1, n2):
+    return calc_function(n1, n2)
+
+
+result = calculate(add, 2, 3)
+print(result)
