cafa1ba6e19d68b778a43180d92c71679747699c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 11:52:22 -0700

    Create a decorator function to calculate the time elapsed during a function execution

--- Day-054/Projects/Create Your Own Python Decorator/main.py
+++ Day-054/Projects/Create Your Own Python Decorator/main.py
@@ -6,15 +6,28 @@ print(current_time)  # seconds since Jan 1st, 1970
 # Write your code below 👇
 
 
-def speed_calc_decorator():
-    pass
+def speed_calc_decorator(function):
+    def time_difference():
+        time1 = time.time()
+        function()
+        time2 = time.time()
+        diff = time2 - time1
+        print(f"{function.__name__} run speed: {diff}s")
+
+    return time_difference
 
 
+@speed_calc_decorator
 def fast_function():
     for i in range(1000000):
         i * i
 
 
+@speed_calc_decorator
 def slow_function():
     for i in range(10000000):
         i * i
+
+
+fast_function()
+slow_function()
