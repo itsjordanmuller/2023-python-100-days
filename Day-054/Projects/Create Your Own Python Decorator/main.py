7e4c7b246f0a2cfa2da7fea20d947d33c64738bf
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 11:51:40 -0700

    Import Python decorator challenge project

---
+++ Day-054/Projects/Create Your Own Python Decorator/main.py
@@ -0,0 +1,20 @@
+import time
+
+current_time = time.time()
+print(current_time)  # seconds since Jan 1st, 1970
+
+# Write your code below 👇
+
+
+def speed_calc_decorator():
+    pass
+
+
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+
+def slow_function():
+    for i in range(10000000):
+        i * i
