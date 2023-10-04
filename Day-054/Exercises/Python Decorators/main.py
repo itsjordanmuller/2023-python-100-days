00334996f3cbfc9c6939bb50f4790fc5e8d497d3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 11:32:24 -0700

    Demonstrate how functions can be nested and returned from other functions

--- Day-054/Exercises/Python Decorators/main.py
+++ Day-054/Exercises/Python Decorators/main.py
@@ -22,3 +22,30 @@ def calculate(calc_function, n1, n2):
 
 result = calculate(add, 2, 3)
 print(result)
+
+
+##Functions can be nested in other functions
+def outer_function():
+    print("I'm outer")
+
+    def nested_function():
+        print("I'm inner")
+
+    nested_function()
+
+
+outer_function()
+
+
+## Functions can be returned from other functions
+def outer_function():
+    print("I'm outer")
+
+    def nested_function():
+        print("I'm inner")
+
+    return nested_function
+
+
+inner_function = outer_function()
+inner_function
