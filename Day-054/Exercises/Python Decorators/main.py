3b2aa0682026417f914944bf47403cf619ceb069
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 11:30:55 -0700

    Show how functions can have inputs, functionality, and outputs

---
+++ Day-054/Exercises/Python Decorators/main.py
@@ -0,0 +1,15 @@
+## Functions can have inputs/functionality/output
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
