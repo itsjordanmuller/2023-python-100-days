70e23c01be6f9e092c6295e96396c1ae32377693
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 13:10:53 -0700

    Implement the ability to dynamically change the year in the copyright footer based on current year

--- Day-057/Exercises/Jinja Template Introduction/server.py
+++ Day-057/Exercises/Jinja Template Introduction/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import random
+import datetime
 
 app = Flask(__name__)
 
@@ -7,7 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     random_number = random.randint(1, 10)
-    return render_template("index.html", num=random_number)
+    today = datetime.date.today()
+    year = today.year
+    return render_template("index.html", num=random_number, year=year)
 
 
 if __name__ == "__main__":
