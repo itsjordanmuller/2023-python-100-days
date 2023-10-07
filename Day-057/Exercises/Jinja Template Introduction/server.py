99edf14a76b695b262bc4a43a93efa6f0dd205b1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 13:44:35 -0700

    Create a blog template, fetch some placeholder blog post data, use it to populate blog posts using Jinja & Flask

--- Day-057/Exercises/Jinja Template Introduction/server.py
+++ Day-057/Exercises/Jinja Template Introduction/server.py
@@ -62,5 +62,13 @@ def guess(name):
     )
 
 
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/2f41322a9185c7a94a7a"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
