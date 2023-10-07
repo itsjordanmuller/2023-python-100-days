a47d8e607ed066891d512bff2e1af633049a6d3c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 13:05:55 -0700

    Create a random number in the server Python file, bring it into the HTML template

--- Day-057/Exercises/Jinja Template Introduction/server.py
+++ Day-057/Exercises/Jinja Template Introduction/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    random_number = random.randint(1, 10)
+    return render_template("index.html", num=random_number)
 
 
 if __name__ == "__main__":
