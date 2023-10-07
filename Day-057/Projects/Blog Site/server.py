ee73ee5da745863f4f3b86bc98998ebfa2a4fda0
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 14:19:10 -0700

    Use a loop to create the correct divisions and HTML elements for each blog post on the home page

--- Day-057/Projects/Blog Site/server.py
+++ Day-057/Projects/Blog Site/server.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/2f41322a9185c7a94a7a"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
 
 
 if __name__ == "__main__":
