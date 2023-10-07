7f32f1e83e389c47ac081f8a8ccf8a1a80d5ac83
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 14:25:54 -0700

    Make each post on the home page accessible when the link is clicked, show content for specific post based on URL number

--- Day-057/Projects/Blog Site/server.py
+++ Day-057/Projects/Blog Site/server.py
@@ -4,13 +4,20 @@ import json
 
 app = Flask(__name__)
 
+blog_url = "https://api.npoint.io/2f41322a9185c7a94a7a"
+response = requests.get(blog_url)
+all_posts = response.json()
+
 
 @app.route("/")
 def hello_world():
-    blog_url = "https://api.npoint.io/2f41322a9185c7a94a7a"
-    response = requests.get(blog_url)
-    all_posts = response.json()
     return render_template("index.html", posts=all_posts)
+
+
+@app.route("/posts/<id>")
+def blog_post(id):
+    postID = int(id) - 1
+    return render_template("post.html", posts=all_posts, postID=postID)
 
 
 if __name__ == "__main__":
