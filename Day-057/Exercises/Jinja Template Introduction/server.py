a87e2f2f278f6f3d93bbd051685788f94917b1ba
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 13:25:08 -0700

    Create a new template for guessing demographics of names, request data from two APIs to check gender and age guesses

--- Day-057/Exercises/Jinja Template Introduction/server.py
+++ Day-057/Exercises/Jinja Template Introduction/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -11,6 +13,25 @@ def hello_world():
     today = datetime.date.today()
     year = today.year
     return render_template("index.html", num=random_number, year=year)
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    nameData = name
+
+    ageResponse = requests.get(f"https://api.agify.io?name={nameData}")
+    if ageResponse.status_code == 200:
+        ageAPIData = ageResponse.json()
+        ageData = ageAPIData["age"]
+
+    genderResponse = requests.get(f"https://api.genderize.io?name={nameData}")
+    if genderResponse.status_code == 200:
+        genderAPIData = genderResponse.json()
+        genderData = genderAPIData["gender"]
+
+    return render_template(
+        "demographics.html", name=nameData, gender=genderData, age=ageData
+    )
 
 
 if __name__ == "__main__":
