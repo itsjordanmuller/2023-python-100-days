e68e9ea0daea660c33696de6225254407d6b90ce
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-09 10:22:05 -0700

    Replace the header in all pages with the single header template with updated navigation links

--- Day-059/Projects/Upgraded Blog/main.py
+++ Day-059/Projects/Upgraded Blog/main.py
@@ -8,5 +8,20 @@ def home_page():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
+
+@app.route("/post/<num>")
+def post_page(num):
+    return render_template("post.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
