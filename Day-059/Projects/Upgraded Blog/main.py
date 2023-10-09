271fb759df4b1b33685e29784afe74a6fc25b298
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-09 10:44:03 -0700

    Make an API request to a set of placeholder blog posts, fill recent posts area with data from the API

--- Day-059/Projects/Upgraded Blog/main.py
+++ Day-059/Projects/Upgraded Blog/main.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+blog_url = "https://api.npoint.io/5e6104dab7d489a76a57"
+response = requests.get(blog_url)
+all_posts = response.json()
+
+# print(all_posts[0]["id"])
+# print(all_posts[0]["body"])
+# print(all_posts[0]["date"])
+# print(all_posts[0]["title"])
+# print(all_posts[0]["author"])
+# print(all_posts[0]["subtitle"])
+# print(all_posts[0]["image_alt"])
+# print(all_posts[0]["image_url"])
 
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route("/about")
