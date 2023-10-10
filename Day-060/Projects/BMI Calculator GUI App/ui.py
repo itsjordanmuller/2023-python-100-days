9c42981aaa2cd5cd9dcdae7212abbc25b49f1c77
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-10 11:15:48 -0700

    Remove gender and age from GUI, this could  be used for other features but has no bearing on BMI calculations in our case
    For young people, BMI is calculated based on age and gender, but we will be using the simple calculation here in all cases by default

--- Day-060/Projects/BMI Calculator GUI App/ui.py
+++ Day-060/Projects/BMI Calculator GUI App/ui.py
@@ -37,23 +37,23 @@ class UI:
             fill="#353b48",
         )
 
-        self.age_canvas = Canvas(
-            self.window, width=200, height=200, bg="#718093", highlightthickness=0
-        )
-        self.age_canvas.grid(column=0, row=2)
-        self.age_canvas.create_text(
-            100,
-            30,
-            text="AGE (IN YEARS)",
-            font=("TkFixedFont", 12, "bold"),
-            fill="#353b48",
-        )
-        self.age_canvas.create_rectangle(40, 55, 160, 125, fill="#dcdde1", width=0)
-        self.age_canvas.create_text(
-            100, 90, text="100", font=("TkFixedFont", 40, "bold")
-        )
-        self.age_canvas.create_oval(50, 140, 90, 180, fill="#273c75")
-        self.age_canvas.create_oval(150, 140, 110, 180, fill="#273c75")
+        # self.age_canvas = Canvas(
+        #     self.window, width=200, height=200, bg="#718093", highlightthickness=0
+        # )
+        # self.age_canvas.grid(column=0, row=3)
+        # self.age_canvas.create_text(
+        #     100,
+        #     30,
+        #     text="AGE (IN YEARS)",
+        #     font=("TkFixedFont", 12, "bold"),
+        #     fill="#353b48",
+        # )
+        # self.age_canvas.create_rectangle(40, 55, 160, 125, fill="#dcdde1", width=0)
+        # self.age_canvas.create_text(
+        #     100, 90, text="100", font=("TkFixedFont", 40, "bold")
+        # )
+        # self.age_canvas.create_oval(50, 140, 90, 180, fill="#273c75")
+        # self.age_canvas.create_oval(150, 140, 110, 180, fill="#273c75")
 
         self.weight_canvas = Canvas(
             self.window, width=200, height=200, bg="#718093", highlightthickness=0
@@ -76,7 +76,7 @@ class UI:
         self.height_canvas = Canvas(
             self.window, width=200, height=200, bg="#718093", highlightthickness=0
         )
-        self.height_canvas.grid(column=0, row=3)
+        self.height_canvas.grid(column=0, row=2)
         self.height_canvas.create_text(
             100,
             30,
@@ -99,33 +99,33 @@ class UI:
         self.height_canvas.create_oval(120, 155, 150, 185, fill="#273c75")
         self.height_canvas.create_oval(120, 55, 150, 85, fill="#273c75")
 
-        self.gender_canvas = Canvas(
-            self.window, width=200, height=200, bg="#718093", highlightthickness=0
-        )
-        self.gender_canvas.grid(column=1, row=3)
-        self.gender_canvas.create_text(
-            100,
-            30,
-            text="GENDER",
-            font=("TkFixedFont", 12, "bold"),
-            fill="#353b48",
-        )
-        self.gender_canvas.create_rectangle(40, 55, 160, 130, fill="#dcdde1", width=0)
-        self.gender_canvas.create_text(
-            100,
-            75,
-            text="I am",
-            font=("TkFixedFont", 12, "bold"),
-            fill="#353b48",
-        )
-        self.gender_canvas.create_text(
-            100,
-            100,
-            text="FEMALE",
-            font=("TkFixedFont", 18, "bold"),
-            fill="#353b48",
-        )
-        self.gender_canvas.create_oval(50, 140, 90, 180, fill="#273c75")
-        self.gender_canvas.create_oval(150, 140, 110, 180, fill="#273c75")
+        # self.gender_canvas = Canvas(
+        #     self.window, width=200, height=200, bg="#718093", highlightthickness=0
+        # )
+        # self.gender_canvas.grid(column=1, row=3)
+        # self.gender_canvas.create_text(
+        #     100,
+        #     30,
+        #     text="GENDER",
+        #     font=("TkFixedFont", 12, "bold"),
+        #     fill="#353b48",
+        # )
+        # self.gender_canvas.create_rectangle(40, 55, 160, 130, fill="#dcdde1", width=0)
+        # self.gender_canvas.create_text(
+        #     100,
+        #     75,
+        #     text="I am",
+        #     font=("TkFixedFont", 12, "bold"),
+        #     fill="#353b48",
+        # )
+        # self.gender_canvas.create_text(
+        #     100,
+        #     100,
+        #     text="FEMALE",
+        #     font=("TkFixedFont", 18, "bold"),
+        #     fill="#353b48",
+        # )
+        # self.gender_canvas.create_oval(50, 140, 90, 180, fill="#273c75")
+        # self.gender_canvas.create_oval(150, 140, 110, 180, fill="#273c75")
 
         self.window.mainloop()
