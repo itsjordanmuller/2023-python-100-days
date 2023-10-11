00a557e43e53b074f376ddbd9b325a547bd52e26
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-11 10:34:13 -0700

    Improve the login form by adding validation, swtiching password from string to password field
    Added a submit field button instead of input button using HTML

--- Day-061/Projects/Flask WTForms/main.py
+++ Day-061/Projects/Flask WTForms/main.py
@@ -1,16 +1,17 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 load_dotenv()
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email:", validators=[DataRequired()])
-    password = StringField("Password:", validators=[DataRequired()])
+    email = StringField(label="Email:", validators=[DataRequired()])
+    password = PasswordField(label="Password:", validators=[DataRequired()])
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
@@ -22,10 +23,25 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "test@example.com" and form.password.data == "password":
+            return redirect(url_for("success"))
+        else:
+            return redirect(url_for("denied"))
     return render_template("login.html", form=form)
+
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+
+@app.route("/denied")
+def denied():
+    return render_template("denied.html")
 
 
 if __name__ == "__main__":
