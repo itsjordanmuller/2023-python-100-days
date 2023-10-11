5d6d599447c0c2a3bbfcd13431d7faa717b1ab04
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-11 10:22:58 -0700

    Create a basic login form with email and password fields, length 30 each

--- Day-061/Projects/Flask WTForms/main.py
+++ Day-061/Projects/Flask WTForms/main.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+load_dotenv()
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email:", validators=[DataRequired()])
+    password = StringField("Password:", validators=[DataRequired()])
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
 @app.route("/")
@@ -11,7 +24,8 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", form=form)
 
 
 if __name__ == "__main__":
