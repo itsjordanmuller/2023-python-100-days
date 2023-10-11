c7018ff40841420c9943e5165d686ecb652cbe8b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-11 09:57:54 -0700

    Add a new route to the login page template so that the home page renders correctly

--- Day-061/Projects/Flask WTForms/main.py
+++ Day-061/Projects/Flask WTForms/main.py
@@ -9,5 +9,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
