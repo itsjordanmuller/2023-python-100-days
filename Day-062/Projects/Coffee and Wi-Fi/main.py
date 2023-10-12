ceb21efaba9dd76e08804018a85fab7dae029e90
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-12 09:21:25 -0700

    Remove comments for tasks that have already been completed for the Coffee and Wi-Fi website project

--- Day-062/Projects/Coffee and Wi-Fi/main.py
+++ Day-062/Projects/Coffee and Wi-Fi/main.py
@@ -71,15 +71,6 @@ class cafeForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-# e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
-
 # all Flask routes below
 @app.route("/")
 def home():
