375fcb0c2005d5aea7543a8fd7994227c49b3482
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-12 08:34:12 -0700

    Render cafe-data CSV file data in a Bootstrap table on cafes page using Jinja templating

--- Day-062/Projects/Coffee and Wi-Fi/main.py
+++ Day-062/Projects/Coffee and Wi-Fi/main.py
@@ -52,9 +52,9 @@ def cafes():
     with open("cafe-data.csv", newline="", encoding="utf-8") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=",")
         list_of_rows = []
-        for row in csv_data:
-            list_of_rows.append(row)
-    return render_template("cafes.html", cafes=list_of_rows)
+        headers = next(csv_data, None)
+        rows = [row for row in csv_data]
+    return render_template("cafes.html", headers=headers, rows=rows)
 
 
 if __name__ == "__main__":
