a4a3743bc4f71881dde704b86c9a411aba42161c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 12:03:55 -0700

    Insert a book with ID, title, author, and rating into database

--- Day-063/Exercises/SQLite Practice/main.py
+++ Day-063/Exercises/SQLite Practice/main.py
@@ -4,6 +4,11 @@ db = sqlite3.connect("books-collection.db")
 
 cursor = db.cursor()
 
+# cursor.execute(
+#     "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
+# )
+
 cursor.execute(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
+    "INSERT INTO books VALUES(1, 'The Sixth Extinction', 'Elizabeth Kolbert', '9.8')"
 )
+db.commit()
