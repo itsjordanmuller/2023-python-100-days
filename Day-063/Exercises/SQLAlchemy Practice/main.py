5962720880b19657ae7d3d1405d5e20bedfa4223
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 12:11:28 -0700

    Create a new folder for practicing SQLAlchemy using Flask-SQLAlchemy, create new books collection database with book table

---
+++ Day-063/Exercises/SQLAlchemy Practice/main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# create the extension
+db = SQLAlchemy()
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-colleciton.db"
+# initialize the app with the extension
+db.init_app(app)
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
