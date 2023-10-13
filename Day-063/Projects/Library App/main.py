e36e5970ff253d565455874f168201556c5f8a6d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 12:31:05 -0700

    Create an SQLite database using Flask-SQLAlchemy in Library App project
    Add the ability to add a record using the form by pulling book details

--- Day-063/Projects/Library App/main.py
+++ Day-063/Projects/Library App/main.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 all_books = []
+
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+db.init_app(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
@@ -19,8 +34,16 @@ def add():
             "author": request.form.get("author"),
             "rating": request.form.get("rating"),
         }
-        all_books.append(book_details)
-        print(all_books)
+        # all_books.append(book_details)
+        # print(all_books)
+        with app.app_context():
+            new_book = Book(
+                title=book_details["name"],
+                author=book_details["author"],
+                rating=book_details["rating"],
+            )
+            db.session.add(new_book)
+            db.session.commit()
         return redirect(url_for("home"))
     return render_template("add.html")
 
