a1623a1b9d997dacdffdfdf7ef7c7bb43d3202e6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 12:59:04 -0700

    Implement the ability to modify a record's rating using an an app route and anchor tags to select a specific ID

--- Day-063/Projects/Library App/main.py
+++ Day-063/Projects/Library App/main.py
@@ -47,5 +47,18 @@ def add():
     return render_template("add.html")
 
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = Book.query.get_or_404(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for("home"))
+    book_id = request.args.get("id")
+    book_selected = Book.query.get_or_404(book_id)
+    return render_template("edit.html", book=book_selected)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
