495e2ffd3e33d9406ba5825da4bcc2f2627de358
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-14 11:54:03 -0700

    Import starting files for favorite movies website using Flask and SQLite

---
+++ Day-064/Projects/Favorite Movies Website/main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+Bootstrap5(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
