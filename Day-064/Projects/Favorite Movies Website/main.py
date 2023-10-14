d0be92222f137a51243d786408717effa45ea356
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-14 12:40:13 -0700

    Modify CSS to be more responsive and render content correctly, loop through data in database to render the list of movies on the home page

--- Day-064/Projects/Favorite Movies Website/main.py
+++ Day-064/Projects/Favorite Movies Website/main.py
@@ -37,7 +37,8 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = Movie.query.all()
+    return render_template("index.html", movies=movies)
 
 
 @app.route("/add", methods=["GET", "POST"])
