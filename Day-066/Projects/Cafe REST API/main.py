f57c03ef6648e14f1ac42bd15f000d4cc806f32e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-16 12:33:31 -0700

    Create an add app route to add a new cafe to the database using a form, test with Postman POST request

--- Day-066/Projects/Cafe REST API/main.py
+++ Day-066/Projects/Cafe REST API/main.py
@@ -138,6 +138,28 @@ def search_cafes():
 
 ## HTTP POST - Create Record
 
+
+@app.route("/add", methods=["POST"])
+def add_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(
+        response={"status": "success", "message": "Successfully added a new cafe."}
+    )
+
+
 ## HTTP PUT/PATCH - Update Record
 
 ## HTTP DELETE - Delete Record
