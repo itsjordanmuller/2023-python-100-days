9c9a05ba7065981de9e090d7de68cdfd1981a831
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-16 12:00:49 -0700

    Create a random route and method to return a random cafe from the database in JSON format to serve as an API

--- Day-066/Projects/Cafe REST API/main.py
+++ Day-066/Projects/Cafe REST API/main.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -32,6 +33,32 @@ with app.app_context():
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+# @app.route("/random", methods=["GET"])
+# GET is allowed by Default on All Routes
+@app.route("/random")
+def random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(
+        cafe={
+            "id": random_cafe.id,
+            "name": random_cafe.name,
+            "map_url": random_cafe.map_url,
+            "img_url": random_cafe.img_url,
+            "location": random_cafe.location,
+            "amenities": {
+                "seats": random_cafe.seats,
+                "has_toilet": random_cafe.has_toilet,
+                "has_wifi": random_cafe.has_wifi,
+                "has_sockets": random_cafe.has_sockets,
+                "can_take_calls": random_cafe.can_take_calls,
+                "coffee_price": random_cafe.coffee_price,
+            },
+        }
+    )
 
 
 ## HTTP GET - Read Record
