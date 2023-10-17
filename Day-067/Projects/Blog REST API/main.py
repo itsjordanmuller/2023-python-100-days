7fce27d87c0415b646a7fe0d8dbfd0d739326694
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 10:55:00 -0700

    Add a route and method to edit an existing post by clicking it's edit link in the post view

--- Day-067/Projects/Blog REST API/main.py
+++ Day-067/Projects/Blog REST API/main.py
@@ -37,13 +37,13 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-class NewPost(FlaskForm):
+class PostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired()])
     body = CKEditorField("Body Content", validators=[DataRequired()])
-    submit = SubmitField("Add Post")
+    submit = SubmitField("Submit Post")
 
 
 with app.app_context():
@@ -68,7 +68,7 @@ def show_post(post_id):
 # Route to add_new_post() to create a new blog post
 @app.route("/new-post", methods=["GET", "POST"])
 def new_post():
-    form = NewPost()
+    form = PostForm()
     if form.validate_on_submit():
         new_blog_post = BlogPost(
             title=form.title.data,
@@ -81,10 +81,28 @@ def new_post():
         db.session.add(new_blog_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
+    return render_template("make-post.html", form=form, type="new")
 
 
 # TODO: edit_post() to change an existing blog post
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    if post is None:
+        return redirect(url_for("get_all_posts"))
+
+    form = PostForm(obj=post)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.subtitle = form.subtitle.data
+        post.author = form.author.data
+        post.img_url = form.img_url.data
+        post.body = form.body.data
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+
+    return render_template("make-post.html", form=form, type="edit")
+
 
 # TODO: delete_post() to remove a blog post from the database
 
