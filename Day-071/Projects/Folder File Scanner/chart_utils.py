caccc1f819fc8ba351821ac020e6f93304e579b4
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-21 15:39:12 -0700

    Modify pie chart to show pie slice colors as the logo colors for those languages
    Starting with HTML/CSS & JavaScript files set to orange, blue and yellow respectively

--- Day-071/Projects/Folder File Scanner/chart_utils.py
+++ Day-071/Projects/Folder File Scanner/chart_utils.py
@@ -2,6 +2,10 @@ from collections import Counter
 import matplotlib.pyplot as plt
 
 
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
 def update_chart(folder_path, ax, canvas, scan_folder):
     file_types, lines_count = scan_folder(folder_path)
     type_lines_counter = Counter()
@@ -12,14 +16,34 @@ def update_chart(folder_path, ax, canvas, scan_folder):
     labels = list(type_lines_counter.keys())
     sizes = list(type_lines_counter.values())
 
+    colors = [
+        "#F06529"
+        if label == ".html"
+        else "#2965f1"
+        if label == ".css"
+        else "#f7df1e"
+        if label == ".js"
+        else None
+        for label in labels
+    ]
+
     ax.clear()
     ax.axis("off")
-    ax.pie(
+
+    patches, texts, autotexts = ax.pie(
         sizes,
         labels=labels,
         autopct=lambda p: f"{p:.1f}%\n({int(p*sum(sizes)/100)} lines)",
         startangle=90,
+        colors=colors,
     )
-    ax.axis("equal")
 
+    color_cycle = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+    color_iter = iter(color_cycle)
+    for color, patch in zip(colors, patches):
+        if color is None:
+            patch.set_facecolor(next(color_iter))
+
+    ax.axis("equal")
     canvas.draw()
