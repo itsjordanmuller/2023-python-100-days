a967dc6b422d5df248f9690760ccc4125c7088ed
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-21 15:09:46 -0700

    Add finishing touches with matplotlib chart
    Add a pie chart to display distribution of file types and lines of code

--- Day-071/Projects/Folder File Scanner/main.py
+++ Day-071/Projects/Folder File Scanner/main.py
@@ -1,22 +1,9 @@
 import os
 import tkinter as tk
 from tkinter import ttk, filedialog
+import matplotlib.pyplot as plt
 from collections import Counter
-
-
-def on_closing():
-    root.destroy()
-
-
-def select_all(event):
-    folder_path_entry.selection_range(0, tk.END)
-    return "break"
-
-
-def browse_folder():
-    folder_selected = filedialog.askdirectory()
-    folder_path_entry.delete(0, tk.END)
-    folder_path_entry.insert(0, folder_selected)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 def count_lines(file_path):
@@ -32,19 +19,62 @@ def scan_folder(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
             file_extension = os.path.splitext(file)[-1].lower()
-            lines = count_lines(file_path)
-            file_types.append(file_extension)
-            lines_count.append(lines)
+
+            try:
+                lines = count_lines(file_path)
+                file_types.append(file_extension)
+                lines_count.append(lines)
+            except Exception as e:
+                print(f"Could not read file {file_path}: {e}")
 
     return file_types, lines_count
+
+
+def update_chart(folder_path):
+    file_types, lines_count = scan_folder(folder_path)
+    type_lines_counter = Counter()
+
+    for file_type, lines in zip(file_types, lines_count):
+        type_lines_counter[file_type] += lines
+
+    labels = list(type_lines_counter.keys())
+    sizes = list(type_lines_counter.values())
+
+    ax.clear()
+    ax.axis("off")
+    ax.pie(
+        sizes,
+        labels=labels,
+        autopct=lambda p: f"{p:.1f}%\n({int(p*sum(sizes)/100)} lines)",
+        startangle=90,
+    )
+    ax.axis("equal")
+
+    canvas.draw()
+
+
+def on_closing():
+    plt.close()
+    root.destroy()
 
 
 def on_button_click():
     folder_path = folder_path_entry.get()
     if os.path.isdir(folder_path):
-        print(scan_folder(folder_path))
+        update_chart(folder_path)
     else:
         print("Please enter a valid folder path")
+
+
+def select_all(event):
+    folder_path_entry.selection_range(0, tk.END)
+    return "break"
+
+
+def browse_folder():
+    folder_selected = filedialog.askdirectory()
+    folder_path_entry.delete(0, tk.END)
+    folder_path_entry.insert(0, folder_selected)
 
 
 root = tk.Tk()
@@ -58,13 +88,24 @@ folder_path_label.grid(column=0, row=0, sticky=tk.W, columnspan=3)
 
 folder_path_entry = ttk.Entry(frame1, width=40)
 folder_path_entry.grid(column=0, row=1, sticky=(tk.W, tk.E), columnspan=3)
+
+frame1.grid_columnconfigure(0, weight=1)
+
 folder_path_entry.bind("<Control-a>", select_all)
 
 browse_button = ttk.Button(frame1, text="Browse", command=browse_folder)
 browse_button.grid(column=1, row=0)
-scan_button = ttk.Button(frame1, text="Analyze", command=lambda: None)
+scan_button = ttk.Button(frame1, text="Analyze", command=on_button_click)
 scan_button.grid(column=2, row=0, sticky=tk.W)
-scan_button.config(command=on_button_click)
+
+frame2 = ttk.Frame(root)
+frame2.pack(padx=20, pady=15, fill=tk.BOTH, expand=True)
+
+fig, ax = plt.subplots()
+ax.axis("off")
+canvas = FigureCanvasTkAgg(fig, master=frame2)
+canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
+
 root.mainloop()
