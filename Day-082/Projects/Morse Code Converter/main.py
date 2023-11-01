8dbab755a2f218daddc8abac635097ce583e84d3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-01 12:37:11 -0700

    Create a basic while loop with a text interface to translate text to morse and vice versa, handle invalid choices in input

--- Day-082/Projects/Morse Code Converter/main.py
+++ Day-082/Projects/Morse Code Converter/main.py
@@ -71,3 +71,28 @@ def decrypt(morse_code):
             else:
                 text += " "
     return text.strip()
+
+
+print("\n----- Morse Code Translator -----")
+print("(WILL CONVERT TEXT TO UPPERCASE)")
+
+while True:
+    choice = input(
+        "\nWhat would you like to translate?\n1. Text to Morse\n2. Morse to Text\n\n0. Exit\n\nEnter a #: "
+    )
+
+    if choice == "1":
+        text = input("\nInput text to translate to Morse Code:\n")
+        morse = encrypt(text)
+        print(f"\n{text.upper()} in Morse: {morse}")
+        print(f"{morse} in Text: {decrypt(morse)}")
+    elif choice == "2":
+        morse = input("\nInput Morse Code to translate to Text:\n")
+        text = decrypt(morse)
+        print(f"\n{morse} in Text: {text.upper()}")
+        print(f"{text.upper()} in Morse: {encrypt(text)}")
+    elif choice == "0":
+        print("Goodbye!")
+        break
+    else:
+        print("\nInvalid Choice. Please select 1, 2, or 0 to exit.")
