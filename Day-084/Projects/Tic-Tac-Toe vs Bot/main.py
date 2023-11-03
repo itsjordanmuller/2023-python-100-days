73e50984b60c4ae0f556d306c1cfa155888233b6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 08:49:14 -0700

    Define a method to print the game board out using ASCII/text characters

--- Day-084/Projects/Tic-Tac-Toe vs Bot/main.py
+++ Day-084/Projects/Tic-Tac-Toe vs Bot/main.py
@@ -9,3 +9,17 @@ class TicTacToeCLI:
         self.x_score = 0
         self.o_score = 0
         self.round = 1
+
+    def print_board(self):
+        for i in range(3):
+            row = ""
+            for j in range(3):
+                if self.board[i * 3 + j] == " ":
+                    row += str(i * 3 + j + 1)
+                else:
+                    row += self.board[i * 3 + j]
+                if j < 2:
+                    row += " | "
+            print(row)
+            if i < 2:
+                print("---------")
