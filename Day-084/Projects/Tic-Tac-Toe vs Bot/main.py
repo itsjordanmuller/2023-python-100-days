fa01f96877c0211db169d4e75b39c219c6d3dd4a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 08:49:46 -0700

    Define methods to check for draw and win conditions

--- Day-084/Projects/Tic-Tac-Toe vs Bot/main.py
+++ Day-084/Projects/Tic-Tac-Toe vs Bot/main.py
@@ -23,3 +23,25 @@ class TicTacToeCLI:
             print(row)
             if i < 2:
                 print("---------")
+
+    def check_win(self, mark):
+        win_conditions = [
+            # rows
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            # columns
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            # diagonals
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        for condition in win_conditions:
+            if all(self.board[i] == mark for i in condition):
+                return True
+        return False
+
+    def check_draw(self):
+        return " " not in self.board
