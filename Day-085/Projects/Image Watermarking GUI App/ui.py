3f44037927d1e57a4b84e7493b92f2b15bbd95ca
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-04 14:15:40 -0700

    Implement upload button to upload and display an image

--- Day-085/Projects/Image Watermarking GUI App/ui.py
+++ Day-085/Projects/Image Watermarking GUI App/ui.py
@@ -1,4 +1,6 @@
 from tkinter import Tk, Canvas
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
 
 class UI:
@@ -18,4 +20,28 @@ class UI:
         self.controls_canvas = Canvas(self.window, width=800, height=100, bg="#7f8c8d")
         self.controls_canvas.grid(column=0, row=2)
 
+        self.first_row_frame = Frame(self.controls_canvas, bg="#7f8c8d")
+        self.first_row_frame.pack(anchor="center")
+
+        self.second_row_frame = Frame(self.controls_canvas, bg="#7f8c8d")
+        self.second_row_frame.pack(anchor="center")
+
+        self.upload_button = Button(
+            self.first_row_frame, text="Upload Image", command=self.upload_image
+        )
+        self.upload_button.pack(side=LEFT, padx=10)
+
         self.window.mainloop()
+
+    def upload_image(self):
+        self.image_path = filedialog.askopenfilename(
+            title="Select an Image",
+            filetypes=[("image files", (".png", ".jpg", ".jpeg", ".gif", ".bmp"))],
+        )
+        if self.image_path:
+            self.original_image = Image.open(self.image_path)
+            self.display_image()
+
+    def display_image(self):
+        self.tk_image = ImageTk.PhotoImage(self.original_image)
+        self.image_canvas.create_image(400, 300, image=self.tk_image)
