df12cc25933285b9825e9506c49efba119d17ec2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-05 11:39:10 -0800

    Add a header canvas with the app title in bold 32pt font

--- Day-086/Projects/Typing Speed Test GUI App/ui.py
+++ Day-086/Projects/Typing Speed Test GUI App/ui.py
@@ -6,4 +6,12 @@ class UI:
         self.window = Tk()
         self.window.title("Typing Speed Test")
 
+        self.header_canvas = Canvas(
+            self.window, width=800, height=100, highlightthickness=0
+        )
+        self.header_canvas.grid(column=0, row=0)
+        self.header_canvas.create_text(
+            400, 50, text="Typing Speed Test", font=("TkFixedFont", 32, "bold")
+        )
+
         self.window.mainloop()
