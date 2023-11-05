63d9077c3989457d35b951b0c2a1d9655b58eefd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-05 12:21:50 -0800

    Modify the timer to display seconds and ms instead of minutes and seconds

--- Day-086/Projects/Typing Speed Test GUI App/timer.py
+++ Day-086/Projects/Typing Speed Test GUI App/timer.py
@@ -10,21 +10,27 @@ class Timer:
         )
         self.timer_canvas.pack(fill="x")
         self.timer_text = self.timer_canvas.create_text(
-            width // 2, height // 2, text="00:00", font=("TkFixedFont", 32, "bold")
+            width // 2, height // 2, text="00.00", font=("TkFixedFont", 32, "bold")
         )
         self.start_time = None
+        self.running = False
 
     def start_timer(self):
-        self.start_time = time.time()
-        self.update_timer()
+        if not self.running:
+            self.start_time = time.time() - (self.start_time or 0)
+            self.running = True
+            self.update_timer()
 
     def update_timer(self):
-        if self.start_time is not None:
+        if self.start_time is not None and self.running:
             elapsed_time = time.time() - self.start_time
-            minutes, seconds = divmod(int(elapsed_time), 60)
-            time_str = f"{minutes:02d}:{seconds:02d}"
+            seconds = int(elapsed_time)
+            milliseconds = int((elapsed_time - seconds) * 100)
+            time_str = f"{seconds:02d}.{milliseconds:02d}"
             self.timer_canvas.itemconfig(self.timer_text, text=time_str)
-            self.window.after(1000, self.update_timer)
+            self.window.after(50, self.update_timer)
 
     def stop_timer(self):
-        self.start_time = None
+        if self.running:
+            self.start_time = time.time() - self.start_time
+            self.running = False
