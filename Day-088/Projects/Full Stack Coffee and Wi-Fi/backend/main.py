2dbd0aef765dfae82811c6c77fd6b95c1baeb4cf
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-07 11:13:10 -0800

    Set frontend port to 3000, backend port to 5000
    Modify the default ports in the front and back of the application so
    that they will always start on 3000 and 5000 to start, that way the
    order of the servers starting won't matter when running both locally

--- Day-088/Projects/Full Stack Coffee and Wi-Fi/backend/main.py
+++ Day-088/Projects/Full Stack Coffee and Wi-Fi/backend/main.py
@@ -230,4 +230,4 @@ def report_closed(cafe_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- Day-088/Projects/Full Stack Coffee and Wi-Fi/frontend/main.py
+++ Day-088/Projects/Full Stack Coffee and Wi-Fi/frontend/main.py
@@ -113,4 +113,4 @@ def cafes():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
