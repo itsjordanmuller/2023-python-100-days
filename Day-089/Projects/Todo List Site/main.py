29a78def57ce4b0e1842e70d20d9eae39c379f4f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-08 08:02:35 -0800

    Create a basic Flask Form to add new tasks to data
    Create a new add route and render the Flask Form using Bootstrap 5 forms

--- Day-089/Projects/Todo List Site/main.py
+++ Day-089/Projects/Todo List Site/main.py
@@ -1,13 +1,35 @@
 import os
 import csv
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerRangeField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+bootstrap = Bootstrap5(app)
+
+
+class taskForm(FlaskForm):
+    taskName = StringField("Task Name:", validators=[DataRequired()])
+    taskDescription = StringField("Describe Task:", validators=[DataRequired()])
+    taskDifficulty = IntegerRangeField(
+        "Difficulty:",
+        validators=[DataRequired()],
+    )
+    submit = SubmitField("Submit")
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/add")
+def add():
+    form = taskForm()
+    return render_template("add.html", form=form)
 
 
 if __name__ == "__main__":
