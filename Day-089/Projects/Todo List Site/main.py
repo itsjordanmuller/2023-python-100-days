1a7c1dca42b06143ed36d63f99f699bc24287337
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-08 08:09:05 -0800

    Create a CSV with headers, append new data to rows
    Modify the add route to use POST method to get data for CSV

--- Day-089/Projects/Todo List Site/main.py
+++ Day-089/Projects/Todo List Site/main.py
@@ -1,24 +1,40 @@
 import os
 import csv
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerRangeField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 bootstrap = Bootstrap5(app)
+
+csv_file_path = "tasks.csv"
 
 
 class taskForm(FlaskForm):
     taskName = StringField("Task Name:", validators=[DataRequired()])
     taskDescription = StringField("Describe Task:", validators=[DataRequired()])
-    taskDifficulty = IntegerRangeField(
-        "Difficulty:",
+    taskDifficulty = StringField(
+        "Difficulty (1-10):",
         validators=[DataRequired()],
     )
     submit = SubmitField("Submit")
+
+
+def check_and_create_csv():
+    if not os.path.exists(csv_file_path):
+        with open(csv_file_path, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Task Name", "Task Description", "Task Difficulty"])
+
+
+def append_to_csv(task_name, task_description, task_difficulty):
+    with open(csv_file_path, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([task_name, task_description, task_difficulty])
 
 
 @app.route("/")
@@ -26,9 +42,18 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/add")
+check_and_create_csv()
+
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
     form = taskForm()
+    if form.validate_on_submit():
+        append_to_csv(
+            form.taskName.data, form.taskDescription.data, form.taskDifficulty.data
+        )
+        flash("Task added successfully!", "success")
+        return redirect(url_for("add"))
     return render_template("add.html", form=form)
 
 
