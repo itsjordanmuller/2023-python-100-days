397e4f973eb2f037ee4d680e5dab7946363c48fc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-10 12:10:27 -0800

    Print the total number of pages, get user input

--- Day-091/Projects/Convert PDF to Audiobook/main.py
+++ Day-091/Projects/Convert PDF to Audiobook/main.py
@@ -1,13 +1,21 @@
 from PyPDF2 import PdfReader
 from gtts import gTTS
 
-reader = PdfReader("Python (Programming Language).pdf")
+filePath = "Python (Programming Language).pdf"
+reader = PdfReader(filePath)
 
 page = reader.pages[0]
 text = page.extract_text()
-print(text)
+# print(text)
+
+total_pages = len(reader.pages)
+print(f"The document has {total_pages} pages.")
+
+num_pages = int(input("Enter the number of pages to convert to MP3: "))
+num_pages = min(num_pages, total_pages)
+# print(num_pages)
 
 language = "en"
 
 textToSpeech = gTTS(text=text, lang=language, slow=False)
-textToSpeech.save("1.mp3")
+textToSpeech.save("test.mp3")
