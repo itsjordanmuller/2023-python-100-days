d4b8cbbc3bdaaee99422986e4f1266c855e0b295
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-10 12:29:04 -0800

    Complete bare-bones MVP implementation add modes

--- Day-091/Projects/Convert PDF to Audiobook/main.py
+++ Day-091/Projects/Convert PDF to Audiobook/main.py
@@ -9,17 +9,31 @@ print(f"The document has {total_pages} pages.")
 
 num_pages = int(input("Enter the number of pages to convert to MP3: "))
 num_pages = min(num_pages, total_pages)
-# print(num_pages)
 
-full_text = ""
-for i in range(num_pages):
-    page = reader.pages[i]
-    text = page.extract_text()
-    if text:
-        full_text += text + "\n"
-
+print()
+choice = input(
+    "\nChoose an Option:\n1. Create Single MP3 (1 for All Pages)\n2. Create Multiple MP3s (1 for Each Page)\n"
+)
 
 language = "en"
 
-tts = gTTS(text=full_text, lang=language, slow=False)
-tts.save("test.mp3")
+if choice == "1":
+    full_text = ""
+    for i in range(num_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+        if text:
+            full_text += text + "\n"
+
+    tts = gTTS(text=full_text, lang=language, slow=False)
+    tts.save("combined_audio.mp3")
+
+elif choice == "2":
+    for i in range(num_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+        if text:
+            tts = gTTS(text=text, lang=language, slow=False)
+            tts.save(f"page_{i+1}.mp3")
+else:
+    print("Invalid choice. Please enter '1' or '2'.")
