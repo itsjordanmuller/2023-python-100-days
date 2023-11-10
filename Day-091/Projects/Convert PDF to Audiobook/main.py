31639359e3f306ae5a45d7baf7e266ce9f0c2a6e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-10 12:04:40 -0800

    Import a PDF from Wikipedia, import GTTS for TTS

--- Day-091/Projects/Convert PDF to Audiobook/main.py
+++ Day-091/Projects/Convert PDF to Audiobook/main.py
@@ -1,9 +1,13 @@
 from PyPDF2 import PdfReader
-
+from gtts import gTTS
 
 reader = PdfReader("Python (Programming Language).pdf")
 
 page = reader.pages[0]
-
 text = page.extract_text()
 print(text)
+
+language = "en"
+
+textToSpeech = gTTS(text=text, lang=language, slow=False)
+textToSpeech.save("1.mp3")
