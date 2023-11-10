a9283bb3a97f5f1b41a9c32fcb881bb715fce13b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-10 12:19:12 -0800

    Add the ability to loop through pages and make MP3

--- Day-091/Projects/Convert PDF to Audiobook/main.py
+++ Day-091/Projects/Convert PDF to Audiobook/main.py
@@ -4,10 +4,6 @@ from gtts import gTTS
 filePath = "Python (Programming Language).pdf"
 reader = PdfReader(filePath)
 
-page = reader.pages[0]
-text = page.extract_text()
-# print(text)
-
 total_pages = len(reader.pages)
 print(f"The document has {total_pages} pages.")
 
@@ -15,7 +11,15 @@ num_pages = int(input("Enter the number of pages to convert to MP3: "))
 num_pages = min(num_pages, total_pages)
 # print(num_pages)
 
+full_text = ""
+for i in range(num_pages):
+    page = reader.pages[i]
+    text = page.extract_text()
+    if text:
+        full_text += text + "\n"
+
+
 language = "en"
 
-textToSpeech = gTTS(text=text, lang=language, slow=False)
-textToSpeech.save("test.mp3")
+tts = gTTS(text=full_text, lang=language, slow=False)
+tts.save("test.mp3")
