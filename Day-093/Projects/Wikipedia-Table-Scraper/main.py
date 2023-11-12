cf4650f18e56b939ec8abdaa4daa3daf7cb401f2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-12 10:43:49 -0800

    Define functions to extract/save table data to CSV

--- Day-093/Projects/Wikipedia-Table-Scraper/main.py
+++ Day-093/Projects/Wikipedia-Table-Scraper/main.py
@@ -2,6 +2,23 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
+
+def extract_table_data(table):
+    rows = table.find_all("tr")
+    table_data = []
+    for row in rows:
+        cells = row.find_all(["th", "td"])
+        cells_text = [cell.get_text(strip=True) for cell in cells]
+        table_data.append(cells_text)
+    return table_data
+
+
+def save_to_csv(data, filename):
+    with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data)
+
+
 r = requests.get(
     "https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
 )
@@ -13,4 +30,10 @@ soup = BeautifulSoup(r.content, "html.parser")
 
 tables = soup.findAll("table")
 
-print(tables)
+# print(tables)
+
+for index, table in enumerate(tables):
+    table_data = extract_table_data(table)
+    filename = f"table_{index}.csv"
+    save_to_csv(table_data, filename)
+    print(f"Saved {filename}")
