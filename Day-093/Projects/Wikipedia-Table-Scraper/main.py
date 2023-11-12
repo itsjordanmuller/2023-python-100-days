da3c0ef5618143c92328d2cfdb6b88982da757d6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-12 11:00:57 -0800

    Use the URL suffix to create directories for CSVs

--- Day-093/Projects/Wikipedia-Table-Scraper/main.py
+++ Day-093/Projects/Wikipedia-Table-Scraper/main.py
@@ -1,6 +1,8 @@
+import os
 import csv
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, unquote
 
 
 def extract_table_data(table):
@@ -16,21 +18,30 @@ def extract_table_data(table):
     return table_data
 
 
-def save_to_csv(data, filename):
-    with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+def save_to_csv(data, folder_name, filename):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    filepath = os.path.join(folder_name, filename)
+    with open(filepath, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(data)
 
 
 def scrape_and_save_tables(url):
+    parsed_url = urlparse(url)
+
+    folder_name = unquote(parsed_url.path.split("/wiki/")[-1]).lower()
+    if folder_name == "":
+        folder_name = unquote(parsed_url.netloc).lower()
+
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
     tables = soup.find_all("table")
     for index, table in enumerate(tables):
         table_data = extract_table_data(table)
         filename = f"table_{index}.csv"
-        save_to_csv(table_data, filename)
-        print(f"Saved {filename}")
+        save_to_csv(table_data, folder_name, filename)
+        print(f"Saved {filename} in folder {folder_name}")
 
 
 if __name__ == "__main__":
