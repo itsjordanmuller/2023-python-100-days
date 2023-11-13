7b4a5d306a96733416165498b1b242bdb8aad6f7
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-13 11:13:09 -0800

    Refactor and reorganize program for readability

--- Day-094/Projects/Chrome-Dino-Game-Automation/main.py
+++ Day-094/Projects/Chrome-Dino-Game-Automation/main.py
@@ -4,48 +4,46 @@ import pyautogui
 from PIL import ImageGrab, ImageOps
 
 
-class coordinates:
-    replaybutton = (480, 300)
-    dinosaur = (250, 300)
+class GameCoordinates:
+    REPLAY_BUTTON = (480, 300)
+    DINOSAUR = (250, 300)
 
 
-def restart():
-    pyautogui.click(coordinates.replaybutton)
+def restart_game():
+    pyautogui.click(GameCoordinates.REPLAY_BUTTON)
     pyautogui.keyDown("down")
 
 
 def jump():
     pyautogui.keyUp("down")
-
     pyautogui.keyDown("space")
     time.sleep(0.08)
     pyautogui.keyUp("space")
-
     pyautogui.keyDown("down")
 
 
-def checkBox():
+def check_box():
     box = (
-        coordinates.dinosaur[0] + 30,
-        coordinates.dinosaur[1],
-        coordinates.dinosaur[0] + 130,
-        coordinates.dinosaur[1] + 30,
+        GameCoordinates.DINOSAUR[0] + 30,
+        GameCoordinates.DINOSAUR[1],
+        GameCoordinates.DINOSAUR[0] + 130,
+        GameCoordinates.DINOSAUR[1] + 30,
     )
-
     image = ImageGrab.grab(box)
-    grayImage = ImageOps.grayscale(image)
-
-    array = np.array(grayImage.getcolors())
-
+    gray_image = ImageOps.grayscale(image)
+    array = np.array(gray_image.getcolors())
     print(array.sum())
     return array.sum()
 
 
-restart()
+def main():
+    restart_game()
+    check_value = check_box()
+    while True:
+        if check_box() != check_value:
+            jump()
+            time.sleep(0.05)
 
-checkValue = checkBox()
 
-while True:
-    if checkBox() != checkValue:
-        jump()
-        time.sleep(0.05)
+if __name__ == "__main__":
+    main()
