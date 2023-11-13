427dd5c0e60be34553c57342e61d3a17e25a41fd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-13 11:07:39 -0800

    Implement ability to restart and jump

--- Day-094/Projects/Chrome-Dino-Game-Automation/main.py
+++ Day-094/Projects/Chrome-Dino-Game-Automation/main.py
@@ -7,3 +7,18 @@ from PIL import ImageGrab, ImageOps
 class coordinates:
     replaybutton = (480, 300)
     dinosaur = (250, 300)
+
+
+def restart():
+    pyautogui.click(coordinates.replaybutton)
+    pyautogui.keyDown("down")
+
+
+def jump():
+    pyautogui.keyUp("down")
+
+    pyautogui.keyDown("space")
+    time.sleep(0.08)
+    pyautogui.keyUp("space")
+
+    pyautogui.keyDown("down")
