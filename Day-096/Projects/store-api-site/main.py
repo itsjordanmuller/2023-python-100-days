a8ab78340e16eca007e6509166200e53791fe148
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-15 11:08:01 -0800

    Render a list of all product data on the home page

--- Day-096/Projects/store-api-site/main.py
+++ Day-096/Projects/store-api-site/main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 from flask import Flask, render_template
 
@@ -8,9 +9,14 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    return render_template("index.html")
+    response = requests.get("https://fakestoreapi.com/products")
+    if response.status_code == 200:
+        products = response.json()
+        return render_template("index.html", products=products)
+    else:
+        return "Failed to fetch data", response.status_code
 
 
 if __name__ == "__main__":
