ac348fdefb697c3a0ea2224d65f1fa386cbf0324
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-17 15:12:49 -0800

    Define two function options for creating folders

--- Day-098/Projects/automatic-folder-creation/create_folders.py
+++ Day-098/Projects/automatic-folder-creation/create_folders.py
@@ -3,6 +3,30 @@ import csv
 
 csv_file = "folders.csv"
 
+
+def create_folders_with_readmes():
+    """Create folders with README.md files."""
+    with open(csv_file, newline="") as data:
+        reader = csv.DictReader(data)
+        for row in reader:
+            folder_name = row["Folder Name"]
+            if not os.path.exists(folder_name):
+                os.mkdir(folder_name)
+                print(f"Created folder: {folder_name}")
+            # create_readme(folder_name)
+
+
+def create_folders_without_readmes():
+    """Create folders without README.md files."""
+    with open(csv_file, newline="") as data:
+        reader = csv.DictReader(data)
+        for row in reader:
+            folder_name = row["Folder Name"]
+            if not os.path.exists(folder_name):
+                os.mkdir(folder_name)
+                print(f"Created folder: {folder_name}")
+
+
 if os.path.exists(csv_file):
     print(f"Found '{csv_file}' file.")
 else:
