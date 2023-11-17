e671b5cb9d0fbde9017c5c619a07d38fb77966ef
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-17 15:14:56 -0800

    Add two more functions for creating README files

--- Day-098/Projects/automatic-folder-creation/create_folders.py
+++ Day-098/Projects/automatic-folder-creation/create_folders.py
@@ -13,7 +13,7 @@ def create_folders_with_readmes():
             if not os.path.exists(folder_name):
                 os.mkdir(folder_name)
                 print(f"Created folder: {folder_name}")
-            # create_readme(folder_name)
+            create_readme(folder_name)
 
 
 def create_folders_without_readmes():
@@ -25,6 +25,29 @@ def create_folders_without_readmes():
             if not os.path.exists(folder_name):
                 os.mkdir(folder_name)
                 print(f"Created folder: {folder_name}")
+
+
+def create_only_readmes():
+    """Create README.md files in existing folders."""
+    with open(csv_file, newline="") as data:
+        reader = csv.DictReader(data)
+        for row in reader:
+            folder_name = row["Folder Name"]
+            if os.path.exists(folder_name) and os.path.isdir(folder_name):
+                create_readme(folder_name)
+            else:
+                print(f"Folder does not exist: {folder_name}")
+
+
+def create_readme(folder_name):
+    """Create a README.md file in the specified folder."""
+    readme_path = os.path.join(folder_name, "README.md")
+    if not os.path.exists(readme_path):
+        with open(readme_path, "w") as readme_file:
+            readme_file.write(f"# {folder_name}\n")
+            print(f"Created README.md in {folder_name}")
+    else:
+        print(f"README.md already exists in {folder_name}")
 
 
 if os.path.exists(csv_file):
