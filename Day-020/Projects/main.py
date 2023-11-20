1cb341687b9d8d7062aaf1c3bf834feaa82d3524
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 14:48:37 -0800

    Add Edits/Images for Snake Game

--- Day-020/Projects/main.py
+++ Day-020/Projects/main.py
@@ -8,19 +8,21 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+# Initialize Snake object
 snake = Snake()
 
+# Keyboard bindings for snake movement
 screen.listen()
 screen.onkey(snake.move_left, "a")
 screen.onkey(snake.move_right, "d")
 screen.onkey(snake.move_up, "w")
 screen.onkey(snake.move_down, "s")
 
+# Main game loop
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
 screen.exitonclick()
--- Day-020/Projects/snake.py
+++ Day-020/Projects/snake.py
@@ -2,17 +2,22 @@ from turtle import Turtle
 
 
 class Snake:
+    """A class to represent the snake in the Snake game."""
+
     def __init__(self):
+        """Initialize the snake with default properties."""
         self.segments = []
         self.create_snake()
         self.direction = "right"
 
     def create_snake(self):
+        """Create initial snake body segments."""
         for i in range(3):
             x_position = i * -20
             self.add_segment(x_position, 0)
 
     def add_segment(self, x, y):
+        """Add a new segment to the snake at the specified coordinates."""
         segment = Turtle("square")
         segment.color("white")
         segment.penup()
@@ -20,11 +25,17 @@ class Snake:
         self.segments.append(segment)
 
     def move(self):
+        """Move the snake in the current direction."""
         for seg_num in range(len(self.segments) - 1, 0, -1):
             x = self.segments[seg_num - 1].xcor()
             y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(x, y)
 
+        self.update_head_direction()
+        self.segments[0].forward(20)
+
+    def update_head_direction(self):
+        """Update the heading of the snake's head based on current direction."""
         if self.direction == "up":
             self.segments[0].setheading(90)
         elif self.direction == "down":
@@ -34,20 +45,22 @@ class Snake:
         elif self.direction == "right":
             self.segments[0].setheading(0)
 
-        self.segments[0].forward(20)
-
     def move_left(self):
+        """Change the snake's direction to left if not moving right."""
         if self.direction != "right":
             self.direction = "left"
 
     def move_right(self):
+        """Change the snake's direction to right if not moving left."""
         if self.direction != "left":
             self.direction = "right"
 
     def move_up(self):
+        """Change the snake's direction to up if not moving down."""
         if self.direction != "down":
             self.direction = "up"
 
     def move_down(self):
+        """Change the snake's direction to down if not moving up."""
         if self.direction != "up":
             self.direction = "down"
