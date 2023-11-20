69ab71c7634036e12a2b744892453842f0b44493
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 23:17:15 -0800

    Add Edits/Images for Days in Month Calculator

--- Day-010/Exercises/3-days-in-month.py
+++ Day-010/Exercises/3-days-in-month.py
@@ -1,4 +1,5 @@
 def is_leap(year):
+    # Check for leap year
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
@@ -12,13 +13,23 @@ def is_leap(year):
 
 
 def days_in_month(year, month):
+    # Days in each month
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # Adjust for leap year in February
     if is_leap(year) and month == 2:
         return 29
     return month_days[month - 1]
 
 
+print("\nDays in Month Calculator\n")
+print("Check how many days are in a specific month in a specific year:")
+print("\nEnter Year as YYYY, Month as M")
+
+# User input for year and month
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
+
+# Calculate and print the number of days in the given month
 days = days_in_month(year, month)
-print(days)
+print(f"\n{days}")
