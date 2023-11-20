c6923eccc041866b2f6c19d9435f19e618bedfe9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 12:27:51 -0800

    Add Edits/Images for Coffee Machine Simulator V2

--- Day-016/Projects/main.py
+++ Day-016/Projects/main.py
@@ -2,23 +2,39 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# Initialize objects for money handling, coffee making, and menu
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
 
+# Flag to keep the machine running
 is_on = True
 
+print("\nCoffee Machine Simulator")
+print("Type 'report' to view resources, 'off' to exit")
+
+# Main loop for coffee machine operations
 while is_on:
+    # Retrieve menu items
     options = menu.get_items()
-    choice = input(f"What would you like? ({options}) ")
+    # Get user's choice
+    choice = input(f"\nWhat would you like? ({options}): ")
+
     if choice == "off":
+        # Turn off the machine
+        print("\nShutting down machine.")
         is_on = False
     elif choice == "report":
+        # Display reports for coffee maker and money machine
+        print("--- Resource Report ---")
         coffee_maker.report()
         money_machine.report()
     else:
+        # Find the drink object based on user's choice
         drink = menu.find_drink(choice)
+        # Check resource sufficiency and process payment
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(
             drink.cost
         ):
+            # Make and serve coffee
             coffee_maker.make_coffee(drink)
--- Day-016/Projects/money_machine.py
+++ Day-016/Projects/money_machine.py
@@ -1,13 +1,7 @@
 class MoneyMachine:
-
     CURRENCY = "$"
 
-    COIN_VALUES = {
-        "quarters": 0.25,
-        "dimes": 0.10,
-        "nickles": 0.05,
-        "pennies": 0.01
-    }
+    COIN_VALUES = {"quarters": 0.25, "dimes": 0.10, "nickles": 0.05, "pennies": 0.01}
 
     def __init__(self):
         self.profit = 0
@@ -19,9 +13,11 @@ class MoneyMachine:
 
     def process_coins(self):
         """Returns the total calculated from coins inserted."""
-        print("Please insert coins.")
+        print("\nPlease insert coins.\n")
         for coin in self.COIN_VALUES:
-            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+            self.money_received += (
+                int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+            )
         return self.money_received
 
     def make_payment(self, cost):
@@ -29,12 +25,15 @@ class MoneyMachine:
         self.process_coins()
         if self.money_received >= cost:
             change = round(self.money_received - cost, 2)
-            print(f"Here is {self.CURRENCY}{change} in change.")
+            print(
+                f"\nTransaction successful! Here is your change: {self.CURRENCY}{change}"
+            )
             self.profit += cost
             self.money_received = 0
             return True
         else:
-            print("Sorry that's not enough money. Money refunded.")
+            print(
+                "\nSorry that's not enough money. Money refunded.\n\nPlease try again with enough money."
+            )
             self.money_received = 0
             return False
-        
