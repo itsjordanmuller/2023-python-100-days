b79c4fad2b39be74e9ba38f6260572e6d1696d32
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 19:40:46 -0800

    Add Edits/Images for High Score

--- Day-005/Exercises/2-high-score.py
+++ Day-005/Exercises/2-high-score.py
@@ -1,15 +1,19 @@
-# 🚨 Don't change the code below 👇
-student_scores = input("Input a list of student scores ").split()
-for n in range(0, len(student_scores)):
-    student_scores[n] = int(student_scores[n])
-print(student_scores)
-# 🚨 Don't change the code above 👆
+print("\nHigh Score Finder\n")
 
-# Write your code below this row 👇
+# Get comma-separated list of student scores
+student_scores = input("Input a list of student scores (comma-separated):\n").split(",")
+for n in range(len(student_scores)):
+    # Strip spaces and convert to int
+    student_scores[n] = int(student_scores[n].strip())
+print(f"\nScores: {student_scores}")
+
+# Initialize variable for tracking the highest score
 high_score = 0
 
+# Iterate through the scores to find the highest one
 for score in student_scores:
     if score > high_score:
         high_score = score
 
-print(f"The highest score in the class is: {high_score}")
+# Print the highest score found
+print(f"\nThe highest score in the class is: {high_score}")
