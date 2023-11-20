bc35c8ee5b1f9060cb000f72b8f1c6865294e040
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 21:30:22 -0800

    Add Edits/Images for Paint Area Calculator

--- Day-008/Exercises/2-paint-area-calculator.py
+++ Day-008/Exercises/2-paint-area-calculator.py
@@ -1,14 +1,27 @@
 import math
 
 
+# Function to calculate the number of paint cans required
 def paint_calc(height, width, cover):
+    # Calculate total & round up to the nearest whole number
     num_cans = (height * width) / cover
     round_up_cans = math.ceil(num_cans)
-    print(f"You'll need {round_up_cans} cans of paint.")
+
+    # Print the number of cans needed
+    print(f"\nYou'll need {round_up_cans} cans of paint.")
 
 
+print("\nPaint Area Calculator\n")
+print(
+    "Determines how many cans of paint is appropriate for your job.\n(Not tested and specific to a hypothetical can size.)\n"
+)
+
+# Coverage per can of paint
+COVERAGE = 5
+
+# Get wall height and width from user
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
-coverage = 5
 
-paint_calc(height=test_h, width=test_w, cover=coverage)
+# Call function to calculate and display required paint cans
+paint_calc(height=test_h, width=test_w, cover=COVERAGE)
