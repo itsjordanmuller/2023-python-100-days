99d596df8cb58440b83ace4b9204c59d37fee36b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 21:45:22 -0800

    Add Edits/Images for Dictionary exercise

--- Day-009/Exercises/1-dictionary.py
+++ Day-009/Exercises/1-dictionary.py
@@ -1,18 +1,26 @@
+# Dictionary with programming terms and definitions
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
 }
 
+# Print definition of 'Bug'
 print(programming_dictionary["Bug"])
+# Print definition of 'Function'
 print(programming_dictionary["Function"])
 
+# Print entire dictionary
 print(programming_dictionary)
 
+# Add 'Loop' definition to dictionary
 programming_dictionary["Loop"] = "The action of doing something over and over again."
 
+# Print definition of 'Loop'
 print(programming_dictionary["Loop"])
+# Print updated dictionary
 print(programming_dictionary)
 
+# Iterate and print each key-value pair in the dictionary
 for key in programming_dictionary:
     print(key)
     print(programming_dictionary[key])
