d23e1e173cc26269a4325eaf6abd495bccc4f144
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 14:41:32 -0800

    Add Edits/Images for Event Listeners exercise

--- Day-019/Exercises/1-event-listeners.py
+++ Day-019/Exercises/1-event-listeners.py
@@ -1,13 +1,20 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
-screen = Screen()
 
-
+# Define function to move turtle forward by 10 units
 def move_forwards():
     tim.forward(10)
 
 
+# Initialize turtle object and create screen
+tim = Turtle()
+screen = Screen()
+
+# Listen for events on screen
 screen.listen()
+
+# Bind space key to move_forwards function
 screen.onkey(fun=move_forwards, key="space")
+
+# Exit program on screen click
 screen.exitonclick()
