634e9b9cfb043599d74991df79fc8fe6d5cd878c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 23:17:27 -0800

    Add Edits/Images for Docstrings exercise

--- Day-010/Exercises/4-docstrings.py
+++ Day-010/Exercises/4-docstrings.py
@@ -1,18 +1,24 @@
 def format_name(f_name, l_name):
-    """Take a first and last name and format it
-    to return the title case version of the name."""
+    """Format first and last name to title case.
+    Return error message if either name is empty"""
     if f_name == "" or l_name == "":
         return "Empty Name, Please Provide a Valid Name"
+
+    # Convert names to title case
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
-    return f"{formatted_f_name} {formatted_l_name}"
+
+    # Return the formatted full name
+    return f"\n{formatted_f_name} {formatted_l_name}"
 
 
+print("\nDocstrings Tutorial\n")
+print("Name Formatter\n")
+
+# Prompt user for first and last name, and print formatted name
 print(
     format_name(
         input("What is your first name? "),
         input("What is your last name? "),
     )
 )
-
-format_name()
