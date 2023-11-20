dfc6c6a4a935a56e24dbb45ff30a5c5a7bbb3128
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 15:14:27 -0800

    Add Edits/Images for Slicing Exercise

--- Day-021/Exercises/2-slicing.py
+++ Day-021/Exercises/2-slicing.py
@@ -1,8 +1,22 @@
+print("\nSlicing Exercise\n")
+
+# Define a list of piano keys
 piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
 
+# Output keys c, d, e
 print(piano_keys[2:5])
+
+# Output keys from c to g
 print(piano_keys[2:])
+
+# Output keys from a to e
 print(piano_keys[:5])
+
+# Output every second key from b to f
 print(piano_keys[1:6:2])
+
+# Output every second key in the entire list
 print(piano_keys[::2])
+
+# Output all keys in reverse order
 print(piano_keys[::-1])
