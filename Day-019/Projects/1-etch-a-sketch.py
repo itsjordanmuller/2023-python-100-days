9f7ab12848e5105b09657ba59389972516fa6098
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 14:41:54 -0800

    Add Edits/Images for Etch-a-Sketch Project

--- Day-019/Projects/1-etch-a-sketch.py
+++ Day-019/Projects/1-etch-a-sketch.py
@@ -1,29 +1,36 @@
 from turtle import Turtle, Screen
 
+# Create turtle and screen objects
 tim = Turtle()
 screen = Screen()
 
 
+# Move turtle forward by 10 units
 def move_forwards():
     tim.forward(10)
 
 
+# Move turtle backward by 10 units
 def move_backwards():
     tim.backward(10)
 
 
+# Rotate turtle clockwise by 10 degrees
 def turn_clockwise():
     tim.right(10)
 
 
+# Rotate turtle counterclockwise by 10 degrees
 def turn_counter_clockwise():
     tim.left(10)
 
 
+# Clear turtle's drawings from screen
 def clear_screen():
     tim.clear()
 
 
+# Reset turtle's position and clear drawings
 def full_reset():
     tim.clear()
     tim.penup()
@@ -31,8 +38,10 @@ def full_reset():
     tim.pendown()
 
 
+# Listen for key presses
 screen.listen()
 
+# Bind keys to corresponding functions
 screen.onkey(fun=move_forwards, key="w")
 screen.onkey(fun=move_backwards, key="s")
 screen.onkey(fun=turn_clockwise, key="d")
@@ -40,4 +49,5 @@ screen.onkey(fun=turn_counter_clockwise, key="a")
 screen.onkey(fun=clear_screen, key="c")
 screen.onkey(fun=full_reset, key="r")
 
+# Exit on screen click
 screen.exitonclick()
