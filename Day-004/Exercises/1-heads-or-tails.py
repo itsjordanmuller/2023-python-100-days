a68ababa3f06431d6d845a81afad4058b868bd68
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 19:20:13 -0800

    Add Edits/Images for Heads or Tails

--- Day-004/Exercises/1-heads-or-tails.py
+++ Day-004/Exercises/1-heads-or-tails.py
@@ -1,8 +1,32 @@
 import random
 
+print("\nFlip Coin")
+
+heads = """
+  ╔════════╗  
+ ╔╝ ░░░▒▒▓ ╚╗ 
+╔╝ ░░░░▒▒▓▓ ╚╗
+║ ░░░HEAD▒▓▓ ║
+╚╗ ░░░▒▒▒▓▓ ╔╝
+ ╚╗ ░░░▒▒▓ ╔╝ 
+  ╚════════╝  
+"""
+
+tails = """
+  ╔════════╗  
+ ╔╝ ░░░▒▒▓ ╚╗ 
+╔╝ ░░░░▒▒▓▓ ╚╗
+║ ░░░TAIL▒▓▓ ║
+╚╗ ░░░▒▒▒▓▓ ╔╝
+ ╚╗ ░░░▒▒▓ ╔╝ 
+  ╚════════╝  
+"""
+
+# Generate a random integer, 0 or 1, to simulate a coin toss
 face_value = random.randint(0, 1)
 
+# Print ASCII Art, Tails for 0, Heads for 1
 if face_value == 0:
-    print("Tails")
+    print(tails)
 else:
-    print("Heads")
+    print(heads)
