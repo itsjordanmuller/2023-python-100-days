dfde4dad8ac3eeccd27d7a455bbac0533070a2a5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 14:20:03 -0800

    Add Edits/Images for Damien Hirst Spots paint app

--- Day-018/Projects/spots/spots.py
+++ Day-018/Projects/spots/spots.py
@@ -4,6 +4,14 @@ from turtle import Turtle, Screen
 
 
 def draw_spot(position, color, size=5):
+    """
+    Draw a colored spot at a given position using Turtle graphics.
+
+    Args:
+        position (tuple): The (x, y) coordinates for the spot's position.
+        color (tuple): The RGB color value for the spot, e.g., (255, 255, 255) for white.
+        size (int, optional): The radius of the spot in pixels. Defaults to 5.
+    """
     painter.penup()
     painter.color(color)
     painter.goto(position)
@@ -12,31 +20,31 @@ def draw_spot(position, color, size=5):
     painter.circle(size)
     painter.end_fill()
 
-
+# Extract colors from image and create color list
 colors = colorgram.extract("damien-hirst-spots.jpg", 30)
 color_list = [(color.rgb.r, color.rgb.g, color.rgb.b) for color in colors]
 
-# for color in colors:
-#     r, g, b = color.rgb
-#     print(f"({r}, {g}, {b})")
-
+# Screen setup
 screen = Screen()
 screen.bgcolor("white")
 screen.colormode(255)
 
+# Turtle setup for drawing
 painter = Turtle()
 painter.speed(10)
 
+# Grid configuration
 grid_spacing = 30
-grid_size = 5
+grid_size = 10
 
-for x in range(
-    -(grid_spacing * grid_size) // 2, (grid_spacing * grid_size) // 2, grid_spacing
-):
-    for y in range(
-        -(grid_spacing * grid_size) // 2, (grid_spacing * grid_size) // 2, grid_spacing
-    ):
+# Drawing spots in a grid pattern
+for x in range(-(grid_spacing * grid_size) // 2, (grid_spacing * grid_size) // 2, grid_spacing):
+    for y in range(-(grid_spacing * grid_size) // 2, (grid_spacing * grid_size) // 2, grid_spacing):
         color = random.choice(color_list)
         draw_spot((x, y), color)
 
+# Hide the turtle icon after drawing is complete
+painter.hideturtle()
+
+# Wait for a mouse click to exit
 screen.exitonclick()
