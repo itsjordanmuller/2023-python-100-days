218506a3bb09302a99bba3cbcaa2ab329c605ddc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 17:27:28 -0800

    Add Edits/Images for Leap Year

--- Day-003/Exercises/3-leap-year.py
+++ Day-003/Exercises/3-leap-year.py
@@ -1,15 +1,20 @@
-# 🚨 Don't change the code below 👇
-year = int(input("Which year do you want to check? "))
-# 🚨 Don't change the code above 👆
+print("\nLeap Year Checker\n")
 
-# Write your code below this line 👇
+# Get user input for the year
+year = int(input("Which year do you want to check? "))
+
+# Check if year is a leap year
 if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
-            print("Leap year.")
+            # Divisible by 400: Leap year
+            print(f"\n{year} is a leap year.")
         else:
-            print("Not leap year.")
+            # Divisible by 100 but not 400: Not a leap year
+            print(f"\n{year} is not a leap year.")
     else:
-        print("Leap year.")
+        # Divisible by 4 but not 100: Leap year
+        print(f"\n{year} is a leap year.")
 else:
-    print("Not leap year.")
+    # Not divisible by 4: Not a leap year
+    print(f"\n{year} is not a leap year.")
