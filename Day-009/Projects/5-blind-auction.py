9499151e275cd0f937a53791caff9d48be61fa65
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 21:45:11 -0800

    Add Edits/Images for Blind Auction Program

--- Day-009/Projects/5-blind-auction.py
+++ Day-009/Projects/5-blind-auction.py
@@ -2,23 +2,39 @@ from art import logo
 import os
 
 
+# Function to clear the screen
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
 
+# Display logo and introduction message
 print(logo)
-print("Welcome to the blind auction program.")
+print("\nBlind Auction Service\n")
 
+# Initialize list to store bidders
 bidders = []
+
+# Flag to control bidding loop
 bidding = "y"
 
+# Loop to collect bids
 while bidding == "y":
+    # Get bidder's name and bid amount
     name = input("What is your name?: ")
     bid = int(input("What is your bid?: $"))
+
+    # Add bidder's details to the list
     bidders.append({"name": name, "bid": bid})
+
+    # Check if there are more bidders
     bidding = input("Are there any other bidders? Type 'y' or 'n': ")
+
+    # Clear screen if more bidders
     if bidding == "y":
         clear()
 
+# Determine the winner with the highest bid
 winner = max(bidders, key=lambda x: x["bid"])
-print("The winner is", winner["name"], "with a bid of $" + str(winner["bid"]))
+
+# Display winner's details
+print("\nThe winner is", winner["name"], "with a bid of $" + str(winner["bid"]))
