ebd57a6e53d4b45b773c78e84964c7932596960e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 12:45:53 -0800

    Add Edits/Images for Classes exercise

--- Day-017/Exercises/classes.py
+++ Day-017/Exercises/classes.py
@@ -1,12 +1,14 @@
-# Classes should use PascalCase
-# Most other Python code use snake_case
-# For constants, we can use UPPERCASE
-# Sometimes, we also use camelCase
+# Naming conventions in Python
+# Classes: PascalCase, Variables: snake_case, Constants: UPPERCASE
+# Occasionally: camelCase
 
 
 class User:
-    # pass
     def __init__(self, user_id, username):
+        """
+        Initialize a new user with user ID and username.
+        Sets followers and following counts to 0.
+        """
         print("New user being created...")
         self.id = user_id
         self.username = username
@@ -14,16 +16,20 @@ class User:
         self.following = 0
 
     def follow(self, user):
+        """
+        Increment the follower count of the user being followed and the following count of the current user.
+        """
         user.followers += 1
         self.following += 1
 
 
+# Creating two User instances
 user_1 = User("001", "Jordan")
 user_2 = User("002", "Angela")
-# user_1.id = "001"
-# user_1.username = "Jordan"
 
+# User 1 follows User 2
 user_1.follow(user_2)
 
+# Displaying user details
 print(user_1.id, user_1.username, user_1.followers, user_1.following)
 print(user_2.id, user_2.username, user_2.followers, user_2.following)
