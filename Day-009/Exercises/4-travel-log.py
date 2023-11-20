aa552f41852b54767fe1c344cc7310096b733a96
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-19 21:48:15 -0800

    Add Edits/Images for Travel Log

--- Day-009/Exercises/4-travel-log.py
+++ Day-009/Exercises/4-travel-log.py
@@ -1,9 +1,11 @@
+# List to store travel logs
 travel_log = [
     {"country": "France", "visits": 12, "cities": ["Paris", "Lille", "Dijon"]},
     {"country": "Germany", "visits": 5, "cities": ["Berlin", "Hamburg", "Stuttgart"]},
 ]
 
 
+# Function to add a new country to the travel log
 def add_new_country(country_visited, times_visited, cities_visited):
     new_country = {}
     new_country["country"] = country_visited
@@ -12,5 +14,10 @@ def add_new_country(country_visited, times_visited, cities_visited):
     travel_log.append(new_country)
 
 
-add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# Add Canada to travel log
+add_new_country("Canada", 2, ["Vancouver", "Toronto"])
+
+print("\nTravel Log\n")
+
+# Display updated travel log
 print(travel_log)
