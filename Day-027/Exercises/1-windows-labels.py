d33cba328846b76a9adc78362b0a359339875cd4
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 20:12:06 -0800

    Add Edits/Images for Windows and Labels

--- Day-027/Exercises/1-windows-labels.py
+++ Day-027/Exercises/1-windows-labels.py
@@ -1,18 +1,23 @@
 from tkinter import *
 
+# Create main window
 window = Tk()
 window.title("Python GUI Program")
 window.minsize(width=500, height=400)
 
+# Initialize a label
 my_label = Label(text="I am a label!", font=("Arial", 24, "bold"))
 my_label.pack()
 
+# Update label text
 my_label["text"] = "New Label Text!"
 my_label.config(text="Yet Another Label!")
 
+# Counter for button clicks
 i = 0
 
 
+# Function to handle button click (increments counter)
 def button_clicked():
     global i
     print("Button was clicked!")
@@ -20,16 +25,23 @@ def button_clicked():
     my_label["text"] = f"Button Clicked {i} Times!"
 
 
+# Function to update label with input field text
 def input_label():
     input_value = input.get()
     my_label["text"] = input_value
 
 
-button = Button(text="Button!", command=input_label)
+# Button to trigger increment function
+button = Button(text="Increment!", command=button_clicked)
 button.pack()
 
+# Button to update label with input field text
+button = Button(text="Get Input!", command=input_label)
+button.pack()
+
+# Input field
 input = Entry(width=10)
 input.pack()
 
-
+# Start the main event loop
 window.mainloop()
