f4701e7e6c2e274414a5afdf4de58da8ca20f5b4
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 19:38:34 -0800

    Add Edits/Images for Filter Even Numbers

--- Day-026/Exercises/3-filter-even-nums.py
+++ Day-026/Exercises/3-filter-even-nums.py
@@ -1,10 +1,10 @@
+print("\nFilter Even Numbers\n")
+
+# List of numbers
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above
 
-# Write your 1 line code 👇 below:
-
+# Filter even numbers using list comprehension
 result = [num for num in numbers if num % 2 == 0]
 
-# Write your code 👆 above:
-
+# Print filtered even numbers
 print(result)
