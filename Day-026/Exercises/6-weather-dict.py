38964b8e9e023f9e7aa58268e4fa17c947a3eb8f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 19:39:25 -0800

    Add Edits/Images for Weather Dictionary exercise

--- Day-026/Exercises/6-weather-dict.py
+++ Day-026/Exercises/6-weather-dict.py
@@ -1,3 +1,11 @@
+# Convert temperature from Celsius to Fahrenheit
+def CelsiusToFahrenheit(temp):
+    return (temp * 9 / 5) + 32
+
+
+print("\nWeather Dictionary Exercise\n")
+
+# Dictionary of temperatures in Celsius for each day
 weather_c = {
     "Monday": 12,
     "Tuesday": 14,
@@ -7,16 +15,9 @@ weather_c = {
     "Saturday": 22,
     "Sunday": 24,
 }
-# 🚨 Don't change code above 👆
 
-
-# Write your code 👇 below:
-
-
-def CelsiusToFahrenheit(temp):
-    return (temp * 9 / 5) + 32
-
-
+# Convert each day's temperature to Fahrenheit
 weather_f = {day: CelsiusToFahrenheit(temp) for (day, temp) in weather_c.items()}
 
+# Print the weather forecast in Fahrenheit
 print(weather_f)
