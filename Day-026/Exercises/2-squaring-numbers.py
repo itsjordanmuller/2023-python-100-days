c2e4e493ea490a700baa812445455b310fe790f6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 19:38:19 -0800

    Add Edits/Images for Squaring Numbers

--- Day-026/Exercises/2-squaring-numbers.py
+++ Day-026/Exercises/2-squaring-numbers.py
@@ -1,10 +1,10 @@
+print("\nSquaring Numbers Exercise\n")
+
+# List of Fibonacci sequence numbers
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above 👆
 
-# Write your 1 line code 👇 below:
-
+# Square each number in the list
 squared_numbers = [num**2 for num in numbers]
 
-# Write your code 👆 above:
-
+# Print squared numbers
 print(squared_numbers)
