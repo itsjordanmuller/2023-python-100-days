111056fb5eb0de53477f8e2c54003602a1d4a6fe
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 19:38:57 -0800

    Add Edits/Images for Dictionary Comprehension

--- Day-026/Exercises/4-dict-comprehension.py
+++ Day-026/Exercises/4-dict-comprehension.py
@@ -1,17 +1,23 @@
 import random
 
-names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 
-
+# Function to generate a random score between 1 and 100
 def RandomScore():
     score = random.randint(1, 100)
     return score
 
 
-student_scores = {name: RandomScore() for name in names}
-print(student_scores)
+print("\nDictionary Comprehension Exercise\n")
 
+# List of student names
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+
+# Generate random scores for each student
+student_scores = {name: RandomScore() for name in names}
+print(f"Student Scores: {student_scores}")
+
+# Filter students with scores of 60 or above
 passed_scores = {
     student: score for (student, score) in student_scores.items() if score >= 60
 }
-print(passed_scores)
+print(f"\nPassed Scores {passed_scores}")
