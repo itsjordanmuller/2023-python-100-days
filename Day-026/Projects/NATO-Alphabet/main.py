77b7c910f1067b0aecde48c5a053719fd5206193
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 19:54:34 -0800

    Add Edits/Images/Requirements for NATO Alphabet

--- Day-026/Projects/NATO-Alphabet/main.py
+++ Day-026/Projects/NATO-Alphabet/main.py
@@ -1,32 +1,28 @@
 import pandas
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
+# Load NATO Phonetic Alphabet data
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 df = pandas.DataFrame(data)
-# print(df)
+
+# Create dictionary from data: letters as keys, codes as values
 phonetic_dict = {row["letter"]: row["code"] for (index, row) in df.iterrows()}
-# print(phonetic_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+print("\nNATO Phonetic Conversion Tool\n")
 
-print("Welcome to the NATO Phonetic Conversion Tool")
-
+# Loop to convert user input into NATO phonetic alphabet
 while True:
     user_input = input("What word would you like to convert to the NATO Alphabet?\n")
     letter_list = [letter for letter in user_input]
 
     try:
+        # Convert each letter in user input to its NATO phonetic equivalent
         converted_list = [
             phonetic_dict[letter.upper()]
             for letter in letter_list
             if letter.upper() in phonetic_dict
         ]
 
+        # Check if all characters in user input are valid and display result
         if len(converted_list) == len(letter_list):
             print(" ".join(converted_list))
             break
