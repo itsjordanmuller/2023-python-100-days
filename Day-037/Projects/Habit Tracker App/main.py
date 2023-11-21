837c9a116551ab90205303d2768b91f4da7bb8d7
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-21 12:51:28 -0800

    Add Edits/Images for Habit Tracker App

--- Day-037/Projects/Habit Tracker App/main.py
+++ Day-037/Projects/Habit Tracker App/main.py
@@ -1,17 +1,21 @@
 import os
-from datetime import datetime
-
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 
+
+# Load environment variables from .env file
 load_dotenv()
 
+# Pixela API base endpoint
 pixela_endpoint = "https://pixe.la/v1/users"
 
+# User credentials and graph ID
 USERNAME = "itsjordanmuller"
 TOKEN_KEY = os.getenv("PIXELA_TOKEN_KEY")
 TEST_GRAPH_ID = "testhabittracker"
 
+# User registration parameters
 user_params = {
     "token": TOKEN_KEY,
     "username": USERNAME,
@@ -19,10 +23,10 @@ user_params = {
     "notMinor": "yes",
 }
 
-# response = requests.post(url=pixela_endpoint, json=user_params)
-
+# Graph endpoint URL
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
+# Graph configuration settings
 graph_config = {
     "id": TEST_GRAPH_ID,
     "name": "Test Habit Tracker",
@@ -31,15 +35,19 @@ graph_config = {
     "color": "ajisai",
 }
 
+# Headers for authentication
 headers = {"X-USER-TOKEN": TOKEN_KEY}
 
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 
+# Specific endpoint for the test graph
 test_graph_endpoint = f"{graph_endpoint}/{TEST_GRAPH_ID}"
 
+# Get current date in Pixela format
 today = datetime.now().strftime("%Y%m%d")
 # print(today)
 
+# Configuration for adding a pixel
 pixel_config = {
     "date": today,
     "quantity": "1",
@@ -47,16 +55,14 @@ pixel_config = {
 
 # response = requests.post(url=test_graph_endpoint, json=pixel_config, headers=headers)
 
+# URL for today's pixel data in the test graph
 today_test_graph_pixels = f"{test_graph_endpoint}/{today}"
 
 # response = requests.delete(url=today_test_graph_pixels, headers=headers)
 
+# Configuration for updating a pixel
 update_config = {"date": today, "quantity": "2"}
 
 # response = requests.put(
 #     url=today_test_graph_pixels, json=update_config, headers=headers
 # )
-
-# API Key Storage Test
-# api_key = os.getenv("MY_TEST_KEY")
-# print(api_key)
