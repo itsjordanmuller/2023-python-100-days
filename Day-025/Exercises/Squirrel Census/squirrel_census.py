0c09779bde162e9d1f707adfb4e9be89789dfa51
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-20 17:03:18 -0800

    Add Edits/Images for Squirrel Census exercise

--- Day-025/Exercises/Squirrel Census/squirrel_census.py
+++ Day-025/Exercises/Squirrel Census/squirrel_census.py
@@ -1,16 +1,21 @@
 import pandas
 
+# Load data from CSV file
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census.csv")
-# print(data)
 
+# Group data by 'Primary Fur Color' and count occurrences
 relevant_data = data.groupby("Primary Fur Color").size()
 
+# Create a DataFrame with fur color and corresponding counts
 df = pandas.DataFrame(
     {"Primary Fur Color": relevant_data.index, "Count": relevant_data.values}
 )
 
+# Sort DataFrame by count in descending order
 df = df.sort_values(by="Count", ascending=False)
 
+# Reset index of the DataFrame
 df = df.reset_index(drop=True)
 
+# Save the sorted data to a new CSV file
 df.to_csv("squirrel_count.csv")
